548374260a7128137d0faca91f4a355dd4326f8e
Lix <xiao.liunit@gmail.com>
2017-05-16 14:11:43 +0800

    Constants

--- t/closure.php
+++ t/closure.php
@@ -11,4 +11,29 @@ $new_numbers = array_map($double, $numbers);
 
 print implode(' ', $new_numbers);
 
+define('MIN_VALUE', '0.0');
+define('MAX_VALUE', '10.0');
+
+echo(' '. MIN_VALUE . ' ' . MAX_VALUE);
+
+class Constants
+{
+    public static function getMinValue()
+    {
+        return MIN_VALUE;
+    }
+
+    public static function getMaxValue()
+    {
+        return MAX_VALUE;
+    }
+
+    public static function toString()
+    {
+        echo '<br>min = ', self::getMinValue(), ' max = ', self::getMaxValue();
+    }
+}
+
+Constants::toString();
+
 ?>
