82346e049c6d2696cc54976fde5e75869e1438c1
Lix <xiao.liunit@gmail.com>
2017-05-16 13:28:21 +0800

    static class method

--- t/call.php
+++ t/call.php
@@ -14,6 +14,30 @@ class MyClass
     }
 }
 
-call_user_func('my_callback_function');
+// call_user_func('my_callback_function');
 
-call_user_func(array('MyClass', 'myCallbackMethod'));
+// call_user_func(array('MyClass', 'myCallbackMethod'));
+
+// $obj = new MyClass();
+// call_user_func(array($obj, 'myCallbackMethod'));
+
+// call_user_func('MyClass::myCallbackMethod');
+
+class A
+{
+    public static function who()
+    {
+        echo "A\n";
+    }
+}
+
+class B extends A
+{
+    public static function who()
+    {
+        echo "B\n";
+    }
+}
+
+call_user_func(array('B', 'parent::who'));
+
