31c44486640736231dc94c6fd91b9a23f669a790
Lix <xiao.liunit@gmail.com>
2017-05-16 11:59:51 +0800

    callback

--- t/call.php
+++ t/call.php
@@ -1,6 +1,19 @@
 <?php
 
 //An example callback function
-function my_callback_function() {
+function my_callback_function()
+{
     echo 'hello world!';
 }
+
+class MyClass
+{
+    static function myCallbackMethod()
+    {
+        echo 'Hello World!';
+    }
+}
+
+call_user_func('my_callback_function');
+
+call_user_func(array('MyClass', 'myCallbackMethod'));
