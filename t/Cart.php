09ae96de8a04a90467496dfc77521529d85ddd08
Lix <xiao.liunit@gmail.com>
2017-05-16 15:09:33 +0800

    getTotal function

--- t/Cart.php
+++ t/Cart.php
@@ -18,5 +18,15 @@ class Cart
         return isset($this->products[$product]) ? $this->products[$product] : FALSE;
     }
 
-
+    public function getTotal($tax)
+    {
+        $total = 0.00;
+        $callback = function($quantity, $product) use ($tax, &$total)
+        {
+            $pricePerItem = constant(__CLASS__ . "::PRICE_" .  strtoupper($product));
+            $total += ($pricePerItem * $quantity) * ($tax + 1.0);
+        };
+        array_walk($this->products, $callback);
+        return round($total, 2);
+    }
 }
