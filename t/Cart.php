1b44e457d139695019b878be7cd09eeccf67de28
Lix <xiao.liunit@gmail.com>
2017-05-16 15:05:47 +0800

    carts function

--- t/Cart.php
+++ t/Cart.php
@@ -7,4 +7,16 @@ class Cart
     const PRICE_EGGS = 6.95;
 
     protected $products = array();
+
+    public function add($product, $quantity)
+    {
+        $this->products[$product] = $quantity;
+    }
+
+    public function getQuantity($product)
+    {
+        return isset($this->products[$product]) ? $this->products[$product] : FALSE;
+    }
+
+
 }
