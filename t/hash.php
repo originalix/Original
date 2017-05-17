7fc68e5c37188c40b6af0a4f9f844a574092dea0
Lix <xiao.liunit@gmail.com>
2017-05-17 17:33:20 +0800

    hash_equals

--- t/hash.php
+++ t/hash.php
@@ -10,3 +10,10 @@ echo hash_final($context), "\n";
 hash_update($copy_context, "data");
 
 echo hash_final($copy_context);
+
+$expected  = crypt('12345', '$2a$07$usesomesillystringforsalt$');
+$correct   = crypt('12345', '$2a$07$usesomesillystringforsalt$');
+$incorrect = crypt('apple', '$2a$07$usesomesillystringforsalt$');
+
+var_dump(hash_equals($expected, $incorrect));
+var_dump(hash_equals($expected, $correct));
