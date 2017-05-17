ab0113d196e7a9b5acda10d53f8a05c3ef4cd43a
Lix <xiao.liunit@gmail.com>
2017-05-17 17:54:18 +0800

    pwd hash

--- t/hash.php
+++ t/hash.php
@@ -41,3 +41,11 @@ echo '</br>';
 if (password_verify('123456', $pwd)) {
     echo "验证成功";
 }
+
+$hash = '$2y$07$BCryptRequires22Chrcte/VlQH0piJtjXl.0t1XkA8pw9dMXTpOq';
+
+if (password_verify('rasmuslerdorf', $hash)) {
+    echo 'Password is valid!';
+} else {
+    echo 'Invalid password.';
+}
