d976d8cb1d54e7dcde2982bac8343b7bcc5e70f7
Lix <xiao.liunit@gmail.com>
2017-05-17 16:08:27 +0800

    GLOBALS

--- t/link.php
+++ t/link.php
@@ -6,3 +6,15 @@
 // unset($a);
 // echo $a, $b;
 //
+$_POST['A'] = 'B';
+$nonReferencedPostVar = $_POST;
+$nonReferencedPostVar['A'] = 'C';
+echo 'POST: '.$_POST['A'].', Variable: '.$nonReferencedPostVar['A']."\n\n";
+
+// Testing Globals
+$GLOBALS['A'] = 'B';
+
+$nonReferencedGlobalsVar = $GLOBALS;
+$nonReferencedGlobalsVar['A'] = 'C';
+
+echo 'GLOBALS: '.$GLOBALS['A'].', Variable: '.$nonReferencedGlobalsVar['A']."\n\n";
