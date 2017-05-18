051274fee21fd5a20f7aaff712a578577bacc9e6
Lix <xiao.liunit@gmail.com>
2017-05-18 11:20:53 +0800

    task

--- todo/js/base.js
+++ todo/js/base.js
@@ -4,4 +4,16 @@
 ;(function () {
   'use strict';
 
+  var $form_add_task = $('.add-task')
+    , new_task = {}
+    ;
+
+  $form_add_task.on('submit', function (e) {
+    /*禁用默认行为*/
+    e.preventDefault();
+    /*获取新Task的值*/
+    new_task.content = $(this).find('input[name=content]').val();
+    /*如果新Task的值为空，则直接返回 否则继续执行*/
+    console.log('new_task', new_task);
+  })
 })();
