5c94d108dccc33dd450e8cd898407dcc9744cce7
Lix <xiao.liunit@gmail.com>
2017-05-19 17:42:30 +0800

    Http Authenticate

--- t/server.php
+++ t/server.php
@@ -1,7 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Lix
- * Date: 2017/5/19
- * Time: 17:19
- */
+
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'Text to send if user hits Cancel button';
+    exit;
+} else {
+    echo "<p>Hello {$_SERVER['PHP_AUTH_USER']} . </p>";
+    echo "<p>You entered {$_SERVER['PHP_AUTH_PW']} as your password.</p>";
+}
+
