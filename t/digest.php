1ad348ea5d420480e6994810b130d4ca02372603
Lix <xiao.liunit@gmail.com>
2017-05-19 17:48:47 +0800

    digest http

--- t/digest.php
+++ t/digest.php
@@ -1,1 +1,11 @@
 <?php
+$realm = 'Restricted area';
+
+//user => password
+$users = array('admin' => 'mypass', 'guest' => 'guest');
+if (empty($_SERVER['PHP_AUTH_DIGEST'])) {
+    header('HTTP/1.1 401 Unauthorized');
+    header('WWW-Authenticate: Digest realm="'.$realm. '" qop="auth" nonce="'.uniqid().'" opaque="'.md5($realm).'"');
+    die('Text to send if user hits Cancel button');
+}
+
