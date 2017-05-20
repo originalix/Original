fbaa2c7a868f4164062a9278d4875031d0dc5864
Lix <xiao.liunit@gmail.com>
2017-05-20 14:55:58 +0800

    authenticate

--- t/authenticate.php
+++ t/authenticate.php
@@ -10,3 +10,15 @@ function authenticate()
     exit;
 }
 
+if (!isset($_SERVER['PHP_AUTH_USER']) ||
+    ($_POST['SeenBefore'] == 1 && $_POST['OldAuth'] == $_SERVER['PHP_AUTH_USER'])) {
+    authenticate();
+} else {
+    echo "<p>Welcome : {$_SERVER['PHP_AUTH_USER']} </br>";
+    echo "Old: {$_REQUEST['OldAuth']} </br>";
+    echo "<form action='{$_SERVER['PHP_SELF']}' METHOD='post'>\n";
+    echo "<input type='hidden' name='SeenBefore' value='1' />\n";
+    echo "<input type='hidden' name='OldAuth' value='{$_SERVER['PHP_AUTH_USER']}' />\n";
+    echo "<input type='submit' value='Re Authenticate'>";
+    echo "</form></p>\n";
+}
