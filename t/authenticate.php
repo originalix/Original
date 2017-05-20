a1bea8592fefbf8aaaaddc3d1f7dabd094d5a197
Lix <xiao.liunit@gmail.com>
2017-05-20 14:46:48 +0800

    function authentication

--- t/authenticate.php
+++ t/authenticate.php
@@ -1,3 +1,12 @@
 <?php
 
 //强迫重新输入用户名和密码的HTTP认证的范例
+
+function authenticate()
+{
+    header('WWW-Authenticate: Basic Realm="Test Authentication System"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo "You mast enter a valid login ID and password to access this resource\n";
+    exit;
+}
+
