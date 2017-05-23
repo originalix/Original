68595c8accdb75a80e0f025b6041a98da8f3035e
Lix <xiao.liunit@gmail.com>
2017-05-23 14:23:02 +0800

    事务处理

--- t/pdo.php
+++ t/pdo.php
@@ -13,10 +13,28 @@ echo '<h1>PDO</h1>';
 //     die();
 // }
 
-    $dbh = new PDO('mysql:host=localhost;dbname=blog', 'root', '', array(
-        PDO::ATTR_PERSISTENT => true  //连接持久化
-    ));
+    // $dbh = new PDO('mysql:host=localhost;dbname=blog', 'root', '', array(
+    //     PDO::ATTR_PERSISTENT => true  //连接持久化
+    // ));
     // 在此使用连接
 
     // 现在运行完成,在此关闭连接
     // $dbh = null;
+
+/* 在事务中执行批处理 */
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=blog', 'root', '', array(
+        PDO::ATTR_PERSISTENT => true  //连接持久化
+    ));
+    echo "Connected\n";
+} catch (Exception $e) {
+    die("Unable to connect: " . $e->getMessage());
+}
+
+try {
+    $dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $dbh->beginTransaction();
+
+}
