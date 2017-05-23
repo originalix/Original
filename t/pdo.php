69525cfc060b196699e42d09338d8b5052c523bb
Lix <xiao.liunit@gmail.com>
2017-05-23 15:11:08 +0800

    占位符插入数据

--- t/pdo.php
+++ t/pdo.php
@@ -45,16 +45,46 @@ try {
 // }
 
 //通过用 name 和 value 替代相应的命名占位符来执行一个插入查询
+// try {
+//     $dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+//     $dbh->beginTransaction();
+
+//     /* 用预处理语句进行重复插入 */
+//     $stmt = $dbh->prepare("INSERT INTO test_main (nikename, mobile, password) VALUES (:nikename, :mobile, :password)");
+//     $stmt->bindParam(':nikename', $nickname);
+//     $stmt->bindParam(':mobile', $mobile);
+//     $stmt->bindParam(':password', $password);
+
+//     //插入一行
+//     $nickname = 'wsx';
+//     $mobile = '13566885986';
+//     $password = '2150';
+//     $stmt->execute();
+
+//     //用不同的值 插入另一行
+//     $nickname = 'xxx';
+//     $mobile = '13566994477';
+//     $password = '5021121';
+//     $stmt->execute();
+
+//     $dbh->commit();
+// } catch (Exception $e) {
+//     $dbh->rollback();
+//     echo "Failed: " . $e->getMessage();
+// }
+
+//通过用 name 和 value 取代 ? 占位符的位置来执行一条插入查询。
 try {
     $dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $dbh->beginTransaction();
 
     /* 用预处理语句进行重复插入 */
-    $stmt = $dbh->prepare("INSERT INTO test_main (nikename, mobile, password) VALUES (:nikename, :mobile, :password)");
-    $stmt->bindParam(':nikename', $nickname);
-    $stmt->bindParam(':mobile', $mobile);
-    $stmt->bindParam(':password', $password);
+    $stmt = $dbh->prepare("INSERT INTO test_main (nikename, mobile, password) VALUES (?, ?, ?)");
+    $stmt->bindParam(1, $nickname);
+    $stmt->bindParam(2, $mobile);
+    $stmt->bindParam(3, $password);
 
     //插入一行
     $nickname = 'wsx';
