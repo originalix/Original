7aba7b30c39e23915d82df9e7243cf77c1ccb736
Lix <xiao.liunit@gmail.com>
2017-05-23 11:13:58 +0800

    set cookie

--- t/cookies.php
+++ t/cookies.php
@@ -1,16 +1,20 @@
 <?php
 
+//Setting new cookie
 setcookie("Original", "Wsx21", time()+60);
 
+//Getting Cookie
 echo $_COOKIE["Original"];
 
+//Updating Cookie
 setcookie("color", "red", time()+60);
 
 echo $_COOKIE["color"];
 
 setcookie("color", "blue", time()+60);
 
+//无效unset
 unset($_COOKIE["Original"]);
 
+/*it expired so it's deleted*/
 setcookie("Original","Wsx21",time()-1);
-
