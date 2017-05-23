f50a6e92b370e88f337832ed8cd0573d49573f87
Lix <xiao.liunit@gmail.com>
2017-05-23 14:15:19 +0800

    PDO持久化连接

--- t/pdo.php
+++ t/pdo.php
@@ -13,8 +13,10 @@ echo '<h1>PDO</h1>';
 //     die();
 // }
 
-    $dbh = new PDO('mysql:host=localhost;dbname=blog', 'root', '');
+    $dbh = new PDO('mysql:host=localhost;dbname=blog', 'root', '', array(
+        PDO::ATTR_PERSISTENT => true  //连接持久化
+    ));
     // 在此使用连接
 
     // 现在运行完成,在此关闭连接
-    $dbh = null;
+    // $dbh = null;
