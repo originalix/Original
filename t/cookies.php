bfbbfddc2883f4fa9efc772a43866239b85b95ea
Lix <xiao.liunit@gmail.com>
2017-05-23 11:12:20 +0800

    delete cookie

--- t/cookies.php
+++ t/cookies.php
@@ -1,5 +1,16 @@
 <?php
 
-// setcookie("Original", "Wsx21", time()+60);
+setcookie("Original", "Wsx21", time()+60);
 
 echo $_COOKIE["Original"];
+
+setcookie("color", "red", time()+60);
+
+echo $_COOKIE["color"];
+
+setcookie("color", "blue", time()+60);
+
+unset($_COOKIE["Original"]);
+
+setcookie("Original","Wsx21",time()-1);
+
