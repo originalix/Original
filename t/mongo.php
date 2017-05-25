e13d65f18808dbb2fd2a8dd40128647e0162df22
Lix <xiao.liunit@gmail.com>
2017-05-25 21:38:34 +0800

    update data

--- t/mongo.php
+++ t/mongo.php
@@ -48,3 +48,16 @@ foreach ($cursor as $document) {
 //     [name] => Google
 //     [url] => http://www.google.com
 // )
+
+
+//update data
+$bulk = new MongoDB\Driver\BulkWrite;
+$bulk->update(
+    ['x' => 2],
+    ['$set' => ['name' => '菜鸟工具', 'url' => 'tool.runoob.com']],
+    ['multi' => false, 'upsert' => false]
+);
+
+$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+$writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 1000);
+$result = $manager->executeBulkWrite('test.sites', $bulk, $writeConcern);
