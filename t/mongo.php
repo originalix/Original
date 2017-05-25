23c88d244f2b4949d339ca6129c62e00d6bee9e8
Lix <xiao.liunit@gmail.com>
2017-05-25 21:36:19 +0800

    插入数据

--- t/mongo.php
+++ t/mongo.php
@@ -11,3 +11,19 @@ var_dump($_id);
 $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
 $writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 1000);
 $result = $manager->executeBulkWrite('test.runoob', $bulk, $writeConcern);
+
+//
+$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+
+// 插入数据
+$bulk = new MongoDB\Driver\BulkWrite;
+$bulk->insert(['x' => 1, 'name'=>'菜鸟教程', 'url' => 'http://www.runoob.com']);
+$bulk->insert(['x' => 2, 'name'=>'Google', 'url' => 'http://www.google.com']);
+$bulk->insert(['x' => 3, 'name'=>'taobao', 'url' => 'http://www.taobao.com']);
+$manager->executeBulkWrite('test.sites', $bulk);
+
+$filter = ['x' => ['$gt' => 1]];
+$options = [
+    'projection' => ['_id' => 0],
+    'sort' => ['x' => -1],
+];
