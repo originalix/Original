a6f5a3b5793f416b30a89be6e126005593553ca0
Lix <xiao.liunit@gmail.com>
2017-05-25 21:36:03 +0800

    add data

--- t/mongo.php
+++ t/mongo.php
@@ -1,3 +1,13 @@
 <?php
 $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
 
+$bulk = new MongoDB\Driver\BulkWrite;
+$document = ['_id' => new MongoDB\BSON\ObjectID, 'name' => '菜鸟教程'];
+
+$_id= $bulk->insert($document);
+
+var_dump($_id);
+
+$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+$writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 1000);
+$result = $manager->executeBulkWrite('test.runoob', $bulk, $writeConcern);
