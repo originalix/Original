4e843ba1130fbf5e73fb212dcb133c377f7b0a38
Lix <xiao.liunit@gmail.com>
2017-05-26 10:29:23 +0800

    yield keyword with fibonacci

--- t/yield.php
+++ t/yield.php
@@ -114,3 +114,28 @@ foreach ($gen as $num) {
     echo "$num ";
 }
 echo $gen->getReturn();
+
+function getFibonacci()
+{
+    $i = 0;
+    $k = 1; //first fibonacci value
+    yield $k;
+    while(true)
+    {
+        $k = $i + $k;
+        $i = $k - $i;
+        yield $k;
+    }
+}
+
+$y = 0;
+
+foreach(getFibonacci() as $fibonacci)
+{
+    echo $fibonacci . "\n";
+    $y++;
+    if($y > 30)
+    {
+        break; // infinite loop prevent
+    }
+}
