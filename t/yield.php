2e9876dbf87b1abf8985c42c36c440d2bd5d4406
Lix <xiao.liunit@gmail.com>
2017-05-26 10:27:22 +0800

    use quote

--- t/yield.php
+++ t/yield.php
@@ -56,3 +56,10 @@ function &gen_reference() {
     }
 }
 
+/*
+ * 我们可以在循环中修改$number的值，而生成器是使用的引用值来生成，所以gen_reference()内部的$value值也会跟着变化。
+ */
+foreach (gen_reference() as &$number) {
+    echo (--$number).'... ';
+}
+
