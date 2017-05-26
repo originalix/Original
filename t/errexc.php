03fe5f067e5b7dd85b86b44cb052466869fe74e7
Lix <xiao.liunit@gmail.com>
2017-05-27 07:32:49 +0800

    error exception

--- t/errexc.php
+++ t/errexc.php
@@ -6,3 +6,15 @@ protected int $severity ;
 /* 方法 */
 public __construct ([ string $message = "" [, int $code = 0 [, int $severity = E_ERROR [, string $filename = __FILE__ [, int $lineno = __LINE__ [, Exception $previous = NULL ]]]]]] )
 final public int getSeverity ( void )
+
+/* 继承的方法 */
+final public string Exception::getMessage ( void )
+final public Throwable Exception::getPrevious ( void )
+final public int Exception::getCode ( void )
+final public string Exception::getFile ( void )
+final public int Exception::getLine ( void )
+final public array Exception::getTrace ( void )
+final public string Exception::getTraceAsString ( void )
+public string Exception::__toString ( void )
+final private void Exception::__clone ( void )
+}
