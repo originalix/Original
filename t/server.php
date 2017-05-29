c3097774aaa7c148099bdcb038c65e2623bb728c
Lix <xiao.liunit@gmail.com>
2017-05-29 16:31:59 +0800

    serilzable

--- t/server.php
+++ t/server.php
@@ -115,3 +115,36 @@ $ser = serialize($obj);
 $newobj = unserialize($ser);
 
 var_dump($newobj->getData());
+
+class Example implements \Serializable
+{
+    protected $property1;
+    protected $property2;
+    protected $property3;
+
+    public function __construct($property1, $property2, $property3)
+    {
+        $this->property1 = $property1;
+        $this->property2 = $property2;
+        $this->property3 = $property3;
+    }
+
+    public function serialize()
+    {
+        return serialize([
+            $this->property1,
+            $this->property2,
+            $this->property3,
+        ]);
+    }
+
+    public function unserialize($data)
+    {
+        list(
+            $this->property1,
+            $this->property2,
+            $this->property3
+        ) = unserialize($data);
+    }
+
+}
