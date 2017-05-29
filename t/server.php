df852b58103670f9a89aa51622c4849ccbc5abb7
Lix <xiao.liunit@gmail.com>
2017-05-29 16:29:53 +0800

    array offset

--- t/server.php
+++ t/server.php
@@ -60,3 +60,23 @@ $obj[] = 'Append 1';
 $obj[] = 'Append 2';
 $obj[] = 'Append 3';
 print_r($obj);
+
+class obj implements arrayaccess {
+    public function offsetSet($offset, $value) {
+        var_dump(__METHOD__);
+    }
+    public function offsetExists($var) {
+        var_dump(__METHOD__);
+        if ($var == "foobar") {
+            return true;
+        }
+        return false;
+    }
+    public function offsetUnset($var) {
+        var_dump(__METHOD__);
+    }
+    public function offsetGet($var) {
+        var_dump(__METHOD__);
+        return "value";
+    }
+}
