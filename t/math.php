30aa6d14c60e6ce41b2c63fdd8db6f00937e9177
Lix <xiao.liunit@gmail.com>
2017-06-01 07:40:57 +0800

    add function

--- t/math.php
+++ t/math.php
@@ -20,5 +20,21 @@ function Add($Num1, $Num2, $Scale=null)
     // remove ending zeroes from decimals and remove point
     $Dec1=isset($Tmp1[2])?rtrim(substr($Tmp1[2],1),'0'):'';
     $Dec2=isset($Tmp2[2])?rtrim(substr($Tmp2[2],1),'0'):'';
+    // calculate the longest length of decimals
+    $DLen=max(strlen($Dec1),strlen($Dec2));
+
+    // if $Scale is null, automatically set it to the amount of decimal places for accuracy
+    if($Scale==null) $Scale=$DLen;
+
+    // remove leading zeroes and reverse the whole numbers, then append padded decimals on the end
+    $Num1=strrev(ltrim($Tmp1[1],'0').str_pad($Dec1,$DLen,'0'));
+    $Num2=strrev(ltrim($Tmp2[1],'0').str_pad($Dec2,$DLen,'0'));
+
+    // calculate the longest length we need to process
+    $MLen=max(strlen($Num1),strlen($Num2));
+
+    // pad the two numbers so they are of equal length (both equal to $MLen)
+    $Num1=str_pad($Num1,$MLen,'0');
+    $Num2=str_pad($Num2,$MLen,'0');
 
 }
