b7618b38f83eb97a8fd610fe5262c5fec4cfab9b
Lix <xiao.liunit@gmail.com>
2017-05-31 10:16:08 +0800

    简便判断图像类型

--- t/exif.php
+++ t/exif.php
@@ -10,13 +10,13 @@ list($width, $height, $type, $attr) = getimagesize("image.gif");
 echo "<img src=\"image.gif\" $attr>";
 echo "list($width, $height, $type, $attr)";
 
+/*判断图像类型*/
+  // function is_jpeg(&$pict)
+  // {
+  //   return (bin2hex($pict[0]) == 'ff' && bin2hex($pict[1]) == 'd8');
+  // }
 
-  function is_jpeg(&$pict)
-  {
-    return (bin2hex($pict[0]) == 'ff' && bin2hex($pict[1]) == 'd8');
-  }
-
-  function is_png(&$pict)
-  {
-    return (bin2hex($pict[0]) == '89' && $pict[1] == 'P' && $pict[2] == 'N' && $pict[3] == 'G');
-  }
+  // function is_png(&$pict)
+  // {
+  //   return (bin2hex($pict[0]) == '89' && $pict[1] == 'P' && $pict[2] == 'N' && $pict[3] == 'G');
+  // }
