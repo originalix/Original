18e168b141efbcc3eda59c3583e950d4c6dc3c02
Lix <xiao.liunit@gmail.com>
2017-06-01 07:21:06 +0800

    check valid number

--- t/math.php
+++ t/math.php
@@ -9,5 +9,9 @@ echo bcadd($a, $b, 4);
 
 function Add($Num1, $Num2, $Scale=null)
 {
+    // check if they're valid positive numbers, extract the whole numbers and decimals
+    if(!preg_match("/^\+?(\d+)(\.\d+)?$/", $Num1, $Tmp1) ||
+       !preg_match("/^\+?(\d+)(\.\d+)?$/", $Num2, $Tmp2))
+        return('0');
 
 }
