1ff708d263b359ffaf79a67aff1dddc2cc81c07f
Lix <xiao.liunit@gmail.com>
2017-06-01 07:42:10 +0800

    finish function

--- t/math.php
+++ t/math.php
@@ -37,4 +37,22 @@ function Add($Num1, $Num2, $Scale=null)
     $Num1=str_pad($Num1,$MLen,'0');
     $Num2=str_pad($Num2,$MLen,'0');
 
+    // process each digit, keep the ones, carry the tens (remainders)
+    for($i=0;$i<$MLen;$i++) {
+        $Sum=((int)$Num1{$i}+(int)$Num2{$i});
+        if(isset($Output[$i])) $Sum+=$Output[$i];
+        $Output[$i]=$Sum%10;
+        if($Sum>9) $Output[$i+1]=1;
+    }
+
+    // convert the array to string and reverse it
+    $Output=strrev(implode($Output));
+
+    // substring the decimal digits from the result, pad if necessary (if $Scale > amount of actual decimals)
+    // next, since actual zero values can cause a problem with the substring values,ifso, just simply give '0'
+    // next, append the decimal value, if $Scale is defined, and return result
+    $Decimal=str_pad(substr($Output,-$DLen,$Scale),$Scale,'0');
+    $Output=(($MLen-$DLen<1)?'0':substr($Output,0,-$DLen));
+    $Output.=(($Scale>0)?".{$Decimal}":'');
+    return($Output);
 }
