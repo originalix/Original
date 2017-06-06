a2f8c24f41ec8a7c9253719d0e5616a69cd455f6
Lix <xiao.liunit@gmail.com>
2017-06-06 10:21:03 +0800

    cancel eio_nop

--- t/err.php
+++ t/err.php
@@ -29,4 +29,9 @@ echo "Hello World \n";
         echo "my_nop ", $data, "\n";
     }
 
-?>
+    //This eio_nop() call will be cancelled
+    $req = eio_nop(EIO_PRI_DEFAULT, "my_nop_cb", "1");
+    var_dump($req);
+    eio_cancel($req);
+
+    
