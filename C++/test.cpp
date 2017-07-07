73b478dc3410e279e857f044bd51308b1cc16f48
Lix <xiao.liunit@gmail.com>
2017-07-07 13:51:16 +0800

    c++ enum

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+enum star
+{
+    kobe,
+    curry,
+    durant,
+    klay
+};
+
 int main()
 {
     cout << "Size of char : " << sizeof(char) << endl;
@@ -10,5 +18,13 @@ int main()
     cout << "Size of float : " << sizeof(float) << endl;
     cout << "Size of double : " << sizeof(double) << endl;
     cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;
+
+    enum star somebody = kobe;
+    if (somebody == curry)
+    {
+        cout << "this is curry shot";
+    } else {
+        cout << "this is " << somebody << " shot" <<endl;
+    }
     return 0;
 }
