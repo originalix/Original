931012197c22377d4b1aa3529cae0fdb8ad030f7
Lix <xiao.liunit@gmail.com>
2017-07-08 16:25:54 +0800

    String

--- C++/BinaryCalculate.cpp
+++ C++/BinaryCalculate.cpp
@@ -168,6 +168,21 @@ void charString()
     cout << "strcmp(str1, str2) : " << a << endl;
 }
 
+void String()
+{
+    string str1 = "Hello";
+    string str2 = "World";
+    string str3;
+    int len;
+
+    str3 = str1;
+    cout << "str3 = " << str3 << endl;
+    str3 = str1 + str2;
+    cout << "str3 = " << str3 << endl;
+    len = str3.size();
+    cout << "str3 size = " << len << endl;
+}
+
 int main()
 {
     // BinaryCalculate();
@@ -198,7 +213,9 @@ int main()
         cout << *(p + i) << endl;
     }
 
-    charString();
+    // charString();
+
+    String();
 
     cout << "Hello wsx" << endl;
     return 0;
