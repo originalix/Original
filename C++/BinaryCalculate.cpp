3233b3e80f52cd0183a29bec559cf4d3be10b63e
Lix <xiao.liunit@gmail.com>
2017-07-08 15:45:07 +0800

    遍历多维数组

--- C++/BinaryCalculate.cpp
+++ C++/BinaryCalculate.cpp
@@ -69,6 +69,24 @@ void printArray()
     }
 }
 
+void MoreDimensionArray()
+{
+    int a[3][4] = {
+        {0, 1, 2, 3},
+        {4, 5, 6, 7},
+        {8, 9, 10, 11}
+    };
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << "a[" << i << "][" << j << "] = ";
+            cout << a[i][j] << endl;
+        }
+    }
+}
+
 int main()
 {
     // BinaryCalculate();
@@ -79,6 +97,8 @@ int main()
     cout << "交换后的值 a = " << a << " , b = " << b <<endl;
 
     printArray();
+
+    MoreDimensionArray();
 
     cout << "Hello wsx" << endl;
     return 0;
