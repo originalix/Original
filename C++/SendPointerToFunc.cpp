6c89c711670639f0dbd4966938ea28f4604934f0
Lix <xiao.liunit@gmail.com>
2017-07-10 07:51:35 +0800

    传递指针给函数

--- C++/SendPointerToFunc.cpp
+++ C++/SendPointerToFunc.cpp
@@ -3,12 +3,18 @@
 
 using namespace std;
 void getSeconds(unsigned long *par);
+double getAverage(int *arr, int size);
 
 int main()
 {
     unsigned long sec;
     getSeconds(&sec);
     cout << "Number of seconds = " << sec << endl;
+
+    int balance[5] = {21, 50, 121, 1201, 135};
+    double avg;
+    avg = getAverage(balance, 5);
+    cout << "Average value is : " << avg << endl;
 
     cout << "Hello wxs" << endl;
 }
@@ -17,4 +23,18 @@ void getSeconds(unsigned long *par)
 {
     *par = time( NULL );
     return;
+}
+
+double getAverage(int *arr, int size)
+{
+    int i, sum = 0;
+    double avager;
+
+    for (i = 0; i < size; i++)
+    {
+        sum += arr[i];
+    }
+
+    avager = (double)sum / size;
+    return avager;
 }
