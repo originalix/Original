85b5f7879b64f4c806aed63463aa0fe1de85eaf1
Lix <xiao.liunit@gmail.com>
2017-07-10 07:55:49 +0800

    从函数返回指针

--- C++/SendPointerToFunc.cpp
+++ C++/SendPointerToFunc.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include <ctime>
+#include <cstdlib>
 
 using namespace std;
 void getSeconds(unsigned long *par);
 double getAverage(int *arr, int size);
+
+int * getRandom()
+{
+    static int r[10];
+
+    srand( (unsigned)time( NULL ) );
+    for (int i = 0; i < 10; ++i)
+    {
+        r[i] = rand();
+        cout << r[i] << endl;
+    }
+
+    return r;
+}
 
 int main()
 {
@@ -15,6 +30,14 @@ int main()
     double avg;
     avg = getAverage(balance, 5);
     cout << "Average value is : " << avg << endl;
+
+    int *p;
+    p = getRandom();
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "*(p + " << i << ") = " ;
+        cout << *(p + i) << endl;
+    }
 
     cout << "Hello wxs" << endl;
 }
