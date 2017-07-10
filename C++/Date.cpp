9eb55c391587b89dca54c9b793e08214892004d9
Lix <xiao.liunit@gmail.com>
2017-07-10 11:25:44 +0800

    format cout date

--- C++/Date.cpp
+++ C++/Date.cpp
@@ -17,6 +17,19 @@ void TimeAndUTC()
 
 int main()
 {
+    time_t now = time(0);
+    cout << "Number of sec since January 1, 1970: " << now << endl;
+
+    tm *ltm = localtime(&now);
+
+    //输出tm结构的各个组成部分
+    cout << "Year: " << 1900 + ltm->tm_year << endl;
+    cout << "Month: " << 1 + ltm->tm_mon << endl;
+    cout << "Day: " << ltm->tm_mday << endl;
+    cout << "Time: " <<  ltm->tm_hour << ":";
+    cout << ltm->tm_min << ":";
+    cout << ltm->tm_sec << endl;
+
     cout << "Hello wxs" << endl;
     return 0;
 }
