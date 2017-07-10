20cfbfe193994f5dc179bc3f9f5d71b5e22dbd3b
Lix <xiao.liunit@gmail.com>
2017-07-11 07:56:18 +0800

    类成员函数

--- C++/Box.cpp
+++ C++/Box.cpp
@@ -8,18 +8,42 @@ class Box
         double length;
         double breadth;
         double height;
+
+        double getVolume(void);
+        void setLength(double len);
+        void setBreadth(double bre);
+        void setHeight(double hei);
 };
+
+double Box::getVolume()
+{
+    return length * breadth * height;
+}
+
+void Box::setLength(double len)
+{
+    length = len;
+}
+
+void Box::setBreadth(double bre)
+{
+    breadth = bre;
+}
+
+void Box::setHeight(double hei)
+{
+    height = hei;
+}
 
 int main()
 {
     Box Box1;
-    Box Box2;
     double volume = 0.0;
 
-    Box1.height = 5.0;
-    Box1.length = 7.0;
-    Box1.breadth = 6.0;
+    Box1.setLength(5.0);
+    Box1.setHeight(4.0);
+    Box1.setBreadth(3.0);
 
-    volume = Box1.height * Box1.length * Box1.breadth;
+    volume = Box1.getVolume();
     cout << "Box1体积: " << volume << endl;
 }
