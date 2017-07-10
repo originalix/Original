d21b340b9c1a62b1080a0fe724c72dacacf00816
Lix <xiao.liunit@gmail.com>
2017-07-11 07:22:09 +0800

    输入输出流

--- C++/IO.cpp
+++ C++/IO.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
+void CinFunc()
+{
+    char name[50];
+    int age;
+    cout << "请输入您的名称、 年龄: ";
+    cin >> name >> age;
+    cout << "您的名称是: " << name << "年龄: " << age << endl;
+}
+
 int main()
 {
     char str[] = "Hello wsx";
     cout << "Value of str is " << str << endl;
+
+    CinFunc();
+
+    return 0;
 }
