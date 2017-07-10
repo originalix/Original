3e9a3e435c9decc9be3f6f09099f6513d0d7ba77
Lix <xiao.liunit@gmail.com>
2017-07-11 07:42:36 +0800

    指向结构的指针

--- C++/Struct.cpp
+++ C++/Struct.cpp
@@ -11,12 +11,12 @@ struct Books
     int book_id;
 };
 
-void printBook( struct Books book)
+void printBook( struct Books *book)
 {
-    cout << "书标题: " << book.title << endl;
-    cout << "书作者: " << book.author << endl;
-    cout << "书类目: " << book.subject << endl;
-    cout << "书ID: " << book.book_id << endl;
+    cout << "书标题: " << book->title << endl;
+    cout << "书作者: " << book->author << endl;
+    cout << "书类目: " << book->subject << endl;
+    cout << "书ID: " << book->book_id << endl;
 }
 
 int main()
@@ -34,9 +34,9 @@ int main()
     strcpy( Book2.subject, "前端技术");
     Book2.book_id = 12346;
 
-    printBook( Book1 );
+    printBook( &Book1 );
 
-    printBook( Book2 );
+    printBook( &Book2 );
 
     return 0;
 }
