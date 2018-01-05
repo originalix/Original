799353e0721602ad1896eddb280a717ad03f4771
Lix <xiao.liunit@gmail.com>
2018-01-05 20:44:38 +0800

    create class mydict

---
+++ python/mydict.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Lix'
+
+class Dict(dict):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+    
+    def __getattr__(self, key)
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+    
+    def __setattr__(self, key, value):
+        self[key] = value
