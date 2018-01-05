10898478b9e948028af3ace4bf3576bca6a1be19
Lix <xiao.liunit@gmail.com>
2018-01-06 07:58:33 +0800

    bytesio

--- python/io.py
+++ python/io.py
@@ -33,5 +33,14 @@ while True:
     s = f.readline()
     if s == '':
         break
-    print(s.strip())
+    # print(s.strip())
+    pass
 
+from io import BytesIO
+
+f = BytesIO()
+f.write('中文'.encode('utf-8'))
+print(f.getvalue())
+
+f = BytesIO(b'\xe4\xb8\xad\xe6\x96\x87')
+print(f.read())
