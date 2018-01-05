779109e3ed41c030f4ad7d7a0a423e5688ada427
Lix <xiao.liunit@gmail.com>
2018-01-05 11:31:13 +0800

    add python function

---
+++ python/hello.py
@@ -0,0 +1,22 @@
+import math
+
+def my_abs(x):
+    if not isinstance(x, (int, float)):
+        raise TypeError('bad operand type')
+    if x >= 0:
+        return x
+    else:
+        return -x
+
+# print(my_abs(100))
+
+def nop():
+    pass
+
+def move(x, y, step, angle=0):
+    nx = x + step * math.cos(angle)
+    ny = y - step * math.sin(angle)
+    return nx, ny
+
+x, y= move(100, 100, 60, math.pi / 6)
+print(x, y)
