8acb10e4cb216acdaaeff1688578b8439629ae53
Lix <xiao.liunit@gmail.com>
2018-01-05 14:09:01 +0800

    高级特性 切片

--- python/hello.py
+++ python/hello.py
@@ -21,10 +21,24 @@ def move(x, y, step, angle=0):
 # x, y= move(100, 100, 60, math.pi / 6)
 # print(x, y)
 
+# 可变参数
 def calc(*numbers):
     sum = 0
     for n in numbers:
         sum = sum + n * n
     return sum
 
-print(calc(1, 2))
+nums = [1, 2, 3]
+# print(calc(nums[0], nums[1], nums[2]))
+# print(calc(*nums))
+
+# 关键字参数
+def person(name, age, **kw):
+    print('name: ', name, 'age: ', age, 'other: ', kw)
+
+extra = {'job': 'developer', 'city': 'Hangzhou'}
+# person('Lixxxx', '25', **extra)
+
+# 高级特性 切片
+L = ['Michael', 'Sarah', 'Tracy']
+print(L[0:1])
