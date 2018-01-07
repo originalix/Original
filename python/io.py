c687f9aaf6ff811d142e4186963b16bc11ae319f
Lix <xiao.liunit@gmail.com>
2018-01-07 15:11:05 +0800

    lock

--- python/io.py
+++ python/io.py
@@ -179,15 +179,36 @@ import time, threading
 # 假定这是你的银行存款:
 balance = 0
 
+# 无锁版本代码
 def change_it(n):
     # 先存后取，结果应该为0:
     global balance
     balance = balance + n
     balance = balance - n
 
+# def run_thread(n):
+#     for i in range(10000000):
+#         change_it(n)
+
+# t1 = threading.Thread(target=run_thread, args=(5,))
+# t2 = threading.Thread(target=run_thread, args=(8,))
+# t1.start()
+# t2.start()
+# t1.join()
+# t2.join()
+# print(balance)
+
+balance = 0
+lock = threading.Lock()
+
+# 加锁版本
 def run_thread(n):
-    for i in range(10000000):
-        change_it(n)
+    for i in range(1000000):
+        lock.acquire()
+        try:
+            change_it(n)
+        finally:
+            lock.release()
 
 t1 = threading.Thread(target=run_thread, args=(5,))
 t2 = threading.Thread(target=run_thread, args=(8,))
