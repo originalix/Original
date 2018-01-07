a90a478fbe9842542888a89a227f2492630cf992
Lix <xiao.liunit@gmail.com>
2018-01-07 20:33:07 +0800

    flask app

--- python/server.py
+++ python/server.py
@@ -2,3 +2,10 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Lix'
+
+from wsgiref.simple_server import make_server
+from web import application
+
+httpd = make_server('', 8000, application)
+print('Serving HTTP on port 8000...')
+httpd.serve_forever()
--- python/web.py
+++ python/web.py
@@ -5,4 +5,5 @@ __author__ = 'Lix'
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    return [b'<h1>Hello, web!</h1>']
+    body = '<h1>Hello, %s!</h1>' % (environ['PATH_INFO'][1:] or 'web')
+    return [body.encode('utf-8')]
