b19c8d625099c7f4ff2c9eae591c92bc694d880a
Lix <xiao.liunit@gmail.com>
2018-01-07 09:53:33 +0800

    启用多线程

--- python/io.py
+++ python/io.py
@@ -128,29 +128,47 @@ import subprocess
 # print('Exit code:', p.returncode)
 
 # 进程间通信
-from multiprocessing import Process, Queue
-import os, time, random
+# from multiprocessing import Process, Queue
+# import os, time, random
 
-# 写入
-def write(q):
-    print('Process to write: %s' % os.getpid())
-    for value in ['A', 'B', 'C']:
-        print('Put %s to queue...' % value)
-        q.put(value)
-        time.sleep(random.random())
+# # 写入
+# def write(q):
+#     print('Process to write: %s' % os.getpid())
+#     for value in ['A', 'B', 'C']:
+#         print('Put %s to queue...' % value)
+#         q.put(value)
+#         time.sleep(random.random())
 
-# 读取
-def read(q):
-    print('Process to read: %s' % os.getpid())
-    while True:
-        value = q.get(True)
-        print('Get %s from queue.' % value)
+# # 读取
+# def read(q):
+#     print('Process to read: %s' % os.getpid())
+#     while True:
+#         value = q.get(True)
+#         print('Get %s from queue.' % value)
 
-if __name__ == '__main__':
-    q = Queue()
-    pw = Process(target=write, args=(q,))
-    pr = Process(target=read, args=(q,))
-    pw.start()
-    pr.start()
-    pw.join()
-    pr.terminate()
+# if __name__ == '__main__':
+#     q = Queue()
+#     pw = Process(target=write, args=(q,))
+#     pr = Process(target=read, args=(q,))
+#     pw.start()
+#     pr.start()
+#     pw.join()
+#     pr.terminate()
+
+# 启用多线程
+import time, threading
+
+def loop():
+    print('thread %s is running...' % threading.current_thread().name)
+    n = 0
+    while n < 5:
+        n = n + 1
+        print('thread %s >>> %s' % (threading.current_thread().name, n))
+        time.sleep(1)
+    print('thread %s ended.' % threading.current_thread().name)
+
+print('thread %s is running...' % threading.current_thread().name)
+t = threading.Thread(target=loop, name='LoopThread')
+t.start()
+t.join()
+print('thread %s ended.' % threading.current_thread().name)
