ed13fa8529d182fff97dd92450a132a49d744994
Lix <xiao.liunit@gmail.com>
2018-01-08 10:53:09 +0800

    Flask连接数据库

---
+++ flaskr/flaskr.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*-coding: utf-8 -*-
+
+#all the imports
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+if __name__ == '__main__':
+    app.run()
--- python2.7/hello.py
+++ python2.7/hello.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    return 'Hello world'
 
 @app.route('/user/<username>')
 def show_user_profile(username):
