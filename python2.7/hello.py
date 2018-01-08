9fe3ac5e9f3a93a8cc033ccc39a420c861a8178d
Lix <xiao.liunit@gmail.com>
2018-01-08 10:20:13 +0800

    使用模板

--- python2.7/hello.py
+++ python2.7/hello.py
@@ -2,11 +2,25 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post %d' % post_id
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
