ea70f08bca60abf2f11024fdc2d291f25d5fec6a
Lix <xiao.liunit@gmail.com>
2018-01-08 11:11:28 +0800

    flaskr

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -6,11 +6,39 @@ import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
+app = Flask(__name__)
+
+def create_app(config=None):
+    app = Flask('flaskr')
+
+    app.config.update(dict(
+        DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+        DEBUG=True,
+        SECRET_KEY=b'_5#y2L"F4Q8z\n\xec]/',
+        USERNAME='admin',
+        PASSWORD='default'
+    ))
+    app.config.update(config or {})
+    app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+    # register_blueprints(app)
+    # register_cli(app)
+    # register_teardowns(app)
+
+    return app
+
 def connect_db():
     """Connects to the specific database."""
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 def get_db():
     """Opens a new database connection if there is none yet for the
