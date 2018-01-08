280e5531b2efcd8f0373af3266a6718f072dcfe8
Lix <xiao.liunit@gmail.com>
2018-01-08 10:07:25 +0800

    flask for 2.7

--- python/app.py
+++ python/app.py
@@ -17,7 +17,6 @@ def signin_form():
 
 @app.route('/signin', methods=['POST'])
 def signin():
-    # 需要从request对象读取表单内容：
     if request.form['username']=='admin' and request.form['password']=='password':
         return '<h3>Hello, admin!</h3>'
     return '<h3>Bad username or password.</h3>'
