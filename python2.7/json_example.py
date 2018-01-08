700230a4e9a57b2c58eda8ec8dc3f96b25a55a97
Lix <xiao.liunit@gmail.com>
2018-01-08 11:45:01 +0800

    flask json example

--- python2.7/json_example.py
+++ python2.7/json_example.py
@@ -1,1 +1,21 @@
 from flask import Flask, Response, jsonify
+
+class MyResponse(Response):
+    @classmethod
+    def force_type(cls, response, environ=None):
+        if isinstance(response, (list, dict)):
+            response = jsonify(response)
+        return super(Response, cls).force_type(response, environ)
+
+class MyFlask(Flask):
+    response_class = MyResponse
+
+app = MyFlask(__name__)
+
+@app.route('/')
+def root():
+    return {'a' : 'hello world', 'b' : 2150}
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
