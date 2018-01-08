dc8ec05afefc1172b24a1b537911c232c7cfd93c
Lix <xiao.liunit@gmail.com>
2018-01-08 21:00:31 +0800

    add user agent headers

--- python2.7/qiushibaike.py
+++ python2.7/qiushibaike.py
@@ -8,13 +8,15 @@ import urllib2
 
 page = 1
 url = 'https://www.qiushibaike.com/hot/page/' + str(page)
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36'
+headers = { 'User-Agent' : user_agent }
 
 try:
-    request = urllib2.Request(url)
+    request = urllib2.Request(url, headers=headers)
     response = urllib2.urlopen(request)
     print response.read()
 except urllib2.URLError, e:
     if hasattr(e, "code"):
         print e.code
     if hasattr(e, "reason"):
-        print e.reason
+        print 'reason ---->' + e.reason
