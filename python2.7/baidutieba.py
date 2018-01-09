9c78a665f82d1666a15d574d59a5fef948b20c0a
Lix <xiao.liunit@gmail.com>
2018-01-10 06:58:15 +0800

    添加百度贴吧爬取HTML代码

--- python2.7/baidutieba.py
+++ python2.7/baidutieba.py
@@ -8,5 +8,18 @@ import re
 
 class BDTB:
 
-    def __init__(self, baseUrl, seeLz):
-        pass
+    def __init__(self, baseUrl, seeLZ):
+        self.baseURL = baseUrl
+        self.seeLZ = '?see_lz=' + str(seeLZ)
+
+    def getPage(self, pageNum):
+        try:
+            url = self.baseURL + self.seeLZ + '&pn=' + str(pageNum)
+            request = urllib2.Request(url)
+            response = urllib2.urlopen(request)
+            print response.read()
+            return response 
+        except urllib2.URLError, e:
+            if hasattr(e, 'reason'):
+                print u'连接百度贴吧错误， 错误原因：', e.reason
+                return None
