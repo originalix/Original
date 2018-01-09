0f2b6482b13ce81e717154324127d0d112cb5281
Lix <xiao.liunit@gmail.com>
2018-01-09 21:19:41 +0800

    no message

--- python2.7/spider.py
+++ python2.7/spider.py
@@ -26,9 +26,9 @@ import cookielib
 # cookie.save(ignore_discard=True, ignore_expires=True)
 
 # 从文件中获取Cookie并访问
-cookie = cookielib.MozillaCookieJar()
-cookie.load('cookie.txt', ignore_discard=True, ignore_expires=True)
-req = urllib2.Request("http://www.baidu.com")
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
-response = opener.open(req)
-print response.read()
+# cookie = cookielib.MozillaCookieJar()
+# cookie.load('cookie.txt', ignore_discard=True, ignore_expires=True)
+# req = urllib2.Request("http://www.baidu.com")
+# opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
+# response = opener.open(req)
+# print response.read()
