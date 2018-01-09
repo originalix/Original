3ff984d8dc7d4db4acf7366ccb14d6c29f7db058
Lix <xiao.liunit@gmail.com>
2018-01-10 06:54:42 +0800

    创建百度贴吧爬虫类

---
+++ python2.7/baidutieba.py
@@ -0,0 +1,12 @@
+__author__ = 'Lix'
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import urllib
+import urllib2
+import re
+
+class BDTB:
+
+    def __init__(self, baseUrl, seeLz):
+        pass
