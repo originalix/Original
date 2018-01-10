e0ea53cf42fdbaf5ad84a00a1156889368f3fdc9
Lix <xiao.liunit@gmail.com>
2018-01-10 13:48:30 +0800

    add comment

--- python2.7/baidutieba.py
+++ python2.7/baidutieba.py
@@ -6,31 +6,42 @@ import urllib
 import urllib2
 import re
 
+# 处理页面标签类
 class Tool:
+    # 去除img标签，7位长空格
     removeImg = re.compile(r'<img.*?>| {7}|')
+    # 删除超链接标签
     removeAddr = re.compile(r'<a.*?>|</a>')
+    # 把换行的标签换位\n
     replaceLine = re.compile(r'<tr>|<div>|</div>|</p>')
+    # 把表格制表的<td>替换为\t
     replaceTD = re.compile(r'<td>')
+    # 把段落开头替换为\n加两个空格
     replacePara = re.compile(r'<p.*?>')
+    # 把br标签替换为\n
     replaceBR = re.compile(r'<br><br>|<br>')
+    # 删除其余的标签
     removeExtraTag = re.compile(r'<.*?>')
     def replace(self, x):
         x = re.sub(self.removeImg, "", x)
         x = re.sub(self.removeAddr, "", x)
         x = re.sub(self.replaceLine, "\n", x)
         x = re.sub(self.replaceTD, "\t", x)
-        x = re.sub(self.replacePara, "\n", x)
+        x = re.sub(self.replacePara, "\n  ", x)
         x = re.sub(self.replaceBR, "\n", x)
         x = re.sub(self.removeExtraTag, "", x)
+        # strip()将前后多余内容删除
         return x.strip()
 
+# 百度贴吧爬虫类
 class BDTB:
-
+    # 初始化，传入url地址，是否只看楼主参数
     def __init__(self, baseUrl, seeLZ):
         self.baseURL = baseUrl
         self.seeLZ = '?see_lz=' + str(seeLZ)
         self.tool = Tool()
 
+    # 传入页码， 获取该页帖子的代码
     def getPage(self, pageNum):
         try:
             url = self.baseURL + self.seeLZ + '&pn=' + str(pageNum)
@@ -43,6 +54,7 @@ class BDTB:
                 print u'连接百度贴吧失败，错误原因: ', e.reason
                 return None
 
+    # 获取帖子标题
     def getTitle(self):
         pageCode = self.getPage(1)
         pattern = re.compile(r'<h3 class="core_title_txt.*?>(.*?)</h3>', re.S)
@@ -52,7 +64,8 @@ class BDTB:
             return result.group(1).strip()
         else:
             return None
-
+    
+    # 获取帖子总页数
     def getPageNum(self):
         pageCode = self.getPage(1)
         pattern = re.compile(r'<li class="l_reply_num.*?</span>.*?<span.*?>(.*?)</span>', re.S)
@@ -62,7 +75,8 @@ class BDTB:
             return result.group(1).strip()
         else:
             return None
-
+    
+    # 获取帖子内容
     def getContent(self):
         pageCode = self.getPage(1)
         pattern = re.compile(r'<div id="post_content_.*?>(.*?)</div>', re.S)
