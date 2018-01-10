c068bee7650711e9371389ffa15c1a9516a593b1
Lix <xiao.liunit@gmail.com>
2018-01-10 15:59:06 +0800

    test

--- python2.7/taobaomm.py
+++ python2.7/taobaomm.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 __author__ = 'Lix'
+
+import urllib
+import urllib2
+import re
+import cookielib
+
+class TBMM:
+    def getPage(self):
+        url = 'https://mm.taobao.com/search_tstar_model.htm'
+        request = urllib2.Request(url, headers = { 'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'})
+        response = urllib2.urlopen(request)
+        pageCode = response.read().decode('gbk')
+        print pageCode
+        return pageCode
+
+tbmm = TBMM()
+tbmm.getPage()
