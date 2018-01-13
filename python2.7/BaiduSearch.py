262062786d9c7430a5df83a8154cfc15e882859c
Lix <xiao.liunit@gmail.com>
2018-01-13 19:37:12 +0800

    finish search in baidu test

--- python2.7/BaiduSearch.py
+++ python2.7/BaiduSearch.py
@@ -8,7 +8,20 @@ from selenium.webdriver.common.keys import Keys
 
 
 class BaiduSearch(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+    
+    def test_searh_in_baidu(self):
+        driver = self.driver
+        driver.get("https://wwww.baidu.com")
+        self.assertIn(u'百度', driver.title)
+        elem = driver.find_element_by_name('wd')
+        elem.send_keys('wsxxxxxxxx')
+        elem.send_keys(Keys.RETURN)
+        assert "No Result in found." not in driver.page_source
 
-if __name__ = '__main__':
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == '__main__':
     unittest.main()
