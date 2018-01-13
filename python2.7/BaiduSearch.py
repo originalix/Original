59cf5b92a3efb4153e2380665888fa002610c7d9
Lix <xiao.liunit@gmail.com>
2018-01-13 19:30:12 +0800

    create baidu search unit test

---
+++ python2.7/BaiduSearch.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'Lix'
+
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class BaiduSearch(unittest.TestCase):
+    pass
+
+if __name__ = '__main__':
+    unittest.main()
