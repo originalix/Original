89c826f3ab0a8b70198d9fb6670deed40ae5af57
Lix <xiao.liunit@gmail.com>
2018-01-14 20:19:55 +0800

    格式化输出淘女郎姓名 城市

--- python2.7/taobaomm.py
+++ python2.7/taobaomm.py
@@ -9,13 +9,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import re
 
 class TBMM:
     def getPage(self):
         url = 'https://mm.taobao.com/search_tstar_model.htm'
         driver = webdriver.PhantomJS()
         driver.get(url)
-        time.sleep(5)
+        time.sleep(3)
         content = driver.page_source.encode('utf-8')
         # with open('tbmm.html', 'w') as f:
             # f.write(content)
@@ -23,7 +24,19 @@ class TBMM:
         soup = BeautifulSoup(content, 'html.parser')
         items = soup.find_all(class_='item')
         for item in items:
-            print item.find(class_='name')
+            print item
+            print u'----分割线---'
+            page_code = str(item)
+            name = item.find(class_='name').text
+            city = item.find(class_='city').text
+            pattern = re.compile(r'data-ks-lazyload="(.*?)"', re.S)
+            result = re.search(pattern, page_code)
+            if result:
+                print 'http:' + result.group(1).strip()
+                print name
+                print city
+            else:
+                print 'not found img src'            
             print '----------------------------------'
         
 
