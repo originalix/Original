bbb5f90dcc3161316afd436bc86db3a13b9839f3
Lix <xiao.liunit@gmail.com>
2018-01-20 21:52:58 +0800

    [函数形式改写git脚本]

--- git.sh
+++ git.sh
@@ -1,18 +1,25 @@
- git diff
- echo "请查看本次提交差异"
- echo "确认提交请输入commit信息"
- echo "如需要取消本次提交，请按ctrl+c"
- read -p "Enter commit message: " commitString
+readCommitMessage() {
+    read -p "Enter commit message: " commitString
+    echo "$commitString"
+}
  
- 
- if [ "$commitString" =  "" ]
- then
-     echo "请不要包含换行符，请重新输入commit信息"
-     read commitString
- else
-     echo "$commitString" 
-     git add .
-     git commit -m "[$commitString]"
-     git push -u origin
-     git push -u os
- fi
+pushFunc() {
+    if [ "$1" = "" ]
+    then
+        echo "commit message为空，请重新输入"
+        pushFunc $(readCommitMessage)
+    else
+        git add .
+        git commit -m "[$1]"
+        git push -u origin
+        git push -u os
+    fi
+}
+
+git diff
+echo "请查看本次提交差异"
+echo "确认提交请输入commit信息"
+echo "如需要取消本次提交，请按ctrl+c"
+
+pushFunc $(readCommitMessage)
+
