2642752a8f353b3f3995b1d19f25ef183adb9f43
Lix <xiao.liunit@gmail.com>
2018-01-20 21:30:57 +0800

    [change shell1 file]

--- shell/shell1.sh
+++ shell/shell1.sh
@@ -2,13 +2,18 @@
 
 # Shell 函数写法
 
+read -p "enter params :" commitString
+
 demoFunc() {
-    echo "第一个参数为 $1 !"
-    echo "第二个参数为 $2 !"
-    echo "参数总数共有 $# 个!"
-    echo "作为一个字符串输出所有的参数 $* !"
+    if [ "$commitString" = "" ]
+    then
+        echo "commit message 为空， 请重新输入"
+    else
+        echo "执行git提交过程.............."
+    fi
 }
 
 echo "-----函数执行-----"
-demoFunc 1 2 3 4 5 2150 1201
+demoFunc $commitString
 echo "-----函数结束-----"
+
