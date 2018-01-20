581aac0aab949a264cea49e916968b73bf187fb3
Lix <xiao.liunit@gmail.com>
2018-01-20 21:23:36 +0800

    [practice shell function demo]

--- shell/shell1.sh
+++ shell/shell1.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-echo Please enter your name
-read -p "Enter your name: " NAME
-echo "Hi! $NAME !"
+# Shell 函数写法
 
-exit 0
+demoFunc() {
+    echo "第一个参数为 $1 !"
+    echo "第二个参数为 $2 !"
+    echo "参数总数共有 $# 个!"
+    echo "作为一个字符串输出所有的参数 $* !"
+}
 
+echo "-----函数执行-----"
+demoFunc 1 2 3 4 5 2150 1201
+echo "-----函数结束-----"
