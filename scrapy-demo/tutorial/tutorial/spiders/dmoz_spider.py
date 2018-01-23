b1565c4607f9a2e3e6432a03fdd7554eb8a7fe99
Lix <xiao.liunit@gmail.com>
2018-01-23 21:34:57 +0800

    [xpath匹配]

--- scrapy-demo/tutorial/tutorial/spiders/dmoz_spider.py
+++ scrapy-demo/tutorial/tutorial/spiders/dmoz_spider.py
@@ -13,8 +13,10 @@ class DmozSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        filename = response.url.split("/")[-2] + '.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-    
+        for sel in response.xpath('//ul/li'):
+            title = sel.xpath('a/text()').extract()
+            link = sel.xpath('a/@href').extract()
+            desc = sel.xpath('text()').extract()
+            print title, link, desc
+
     
