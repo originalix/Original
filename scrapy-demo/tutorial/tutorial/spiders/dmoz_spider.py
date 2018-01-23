34ffa63f2f229e788d081c3b216352270cb87736
Lix <xiao.liunit@gmail.com>
2018-01-23 21:21:33 +0800

    [添加DmozSpider爬虫类]

---
+++ scrapy-demo/tutorial/tutorial/spiders/dmoz_spider.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = "Lix"
+
+import scrapy
+
+class DmozSpider(scrapy.Spider):
+    name = "dmoz"
+    allowed_domains = ['dmoztools.net']
+    start_urls = [
+        "http://dmoztools.net/Computers/Programming/Languages/Python/Books/"
+    ]
+
+    def parse(self, response):
+        filename = response.url.split("/")[-2] + '.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+    
+    
