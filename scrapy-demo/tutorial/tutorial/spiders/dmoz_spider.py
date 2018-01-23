c831f58600fffae4e447ef967b49ea0b6e19d89c
Lix <xiao.liunit@gmail.com>
2018-01-23 21:58:38 +0800

    [尝试使用Xpath]

--- scrapy-demo/tutorial/tutorial/spiders/dmoz_spider.py
+++ scrapy-demo/tutorial/tutorial/spiders/dmoz_spider.py
@@ -3,20 +3,23 @@
 __author__ = "Lix"
 
 import scrapy
+from tutorial.items import DmozItem
 
 class DmozSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ['dmoztools.net']
     start_urls = [
         "http://dmoztools.net/Computers/Programming/Languages/Python/Books/",
-        "http://dmoztools.net/Computers/Programming/Languages/Python/Resources/"
+        # "http://dmoztools.net/Computers/Programming/Languages/Python/Resources/"
     ]
 
     def parse(self, response):
-        for sel in response.xpath('//ul/li'):
-            title = sel.xpath('a/text()').extract()
-            link = sel.xpath('a/@href').extract()
-            desc = sel.xpath('text()').extract()
-            print title, link, desc
-
+        index = 0
+        for sel in response.xpath('//div[@class="site-item "]'):
+            # print sel
+            item = DmozItem()
+            item['title'] = sel.xpath('/div').extract()
+            # item['link'] = sel.xpath('a/@href').extract()
+            # item['desc'] = sel.xpath('text()').extract()
+            # yield item
     
