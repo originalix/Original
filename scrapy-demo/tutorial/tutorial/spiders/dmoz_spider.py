5077c6ba836c1fb3d58393968a65748928a7cbb4
Lix <xiao.liunit@gmail.com>
2018-01-23 21:25:55 +0800

    [创建dmoz爬虫，爬取成功]

--- scrapy-demo/tutorial/tutorial/spiders/dmoz_spider.py
+++ scrapy-demo/tutorial/tutorial/spiders/dmoz_spider.py
@@ -8,7 +8,8 @@ class DmozSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ['dmoztools.net']
     start_urls = [
-        "http://dmoztools.net/Computers/Programming/Languages/Python/Books/"
+        "http://dmoztools.net/Computers/Programming/Languages/Python/Books/",
+        "http://dmoztools.net/Computers/Programming/Languages/Python/Resources/"
     ]
 
     def parse(self, response):
