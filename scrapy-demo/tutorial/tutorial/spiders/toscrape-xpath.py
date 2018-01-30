8c4bae22161205c9304b01c58d32993aa00dbcc5
Lix <xiao.liunit@gmail.com>
2018-01-31 07:33:18 +0800

    [完成 xpath版本爬虫]

--- scrapy-demo/tutorial/tutorial/spiders/toscrape-xpath.py
+++ scrapy-demo/tutorial/tutorial/spiders/toscrape-xpath.py
@@ -9,5 +9,13 @@ class ToScrapeXpathSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        for quote in response.xpath("//div[@class='quote']"):
-            print quote
+        for quote in response.xpath('//div[@class="quote"]'):
+            yield {
+                'text' : quote.xpath('./span[@class="text"]/text()').extract_first(),
+                'author' : quote.xpath('.//small[@class="author"]/text()').extract_first(),
+                'tag' : quote.xpath('.//div[@class="tags"]/a[@class="tag"]/text()').extract()
+            }
+
+        next_page_url = quote.xpath('//li[@class="next"]/a/@href').extract_first()
+        if next_page_url is not None:
+            yield scrapy.Request(response.urljoin(next_page_url))
