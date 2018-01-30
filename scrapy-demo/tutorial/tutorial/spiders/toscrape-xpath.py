b36b9ef51a171a0425426f4f6b0005ef5d3f1332
Lix <xiao.liunit@gmail.com>
2018-01-31 07:19:31 +0800

    [test xpath]

--- scrapy-demo/tutorial/tutorial/spiders/toscrape-xpath.py
+++ scrapy-demo/tutorial/tutorial/spiders/toscrape-xpath.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import scrapy
+
+class ToScrapeXpathSpider(scrapy.Spider):
+    name = "toscrape-xpath"
+    start_urls = [
+        "http://quotes.toscrape.com/",
+    ]
+
+    def parse(self, response):
+        for quote in response.xpath("//div[@class='quote']"):
+            print quote
