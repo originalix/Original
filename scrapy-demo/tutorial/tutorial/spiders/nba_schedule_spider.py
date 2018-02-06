0c85f58be6f42561ea8ebe207a30e15722f66cb7
Lix <xiao.liunit@gmail.com>
2018-02-06 21:16:49 +0800

    [删除title，获取当前页面比赛信息]

--- scrapy-demo/tutorial/tutorial/spiders/nba_schedule_spider.py
+++ scrapy-demo/tutorial/tutorial/spiders/nba_schedule_spider.py
@@ -9,21 +9,33 @@ class NBAScheduleSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        title = response.xpath('//tr[@class="title"]//td/text()').extract()
-        
-        for t in title:
-            print t.encode('utf-8')
-        
-        data = response.xpath('//tr[contains(@class, "left")]')
+        # 暂时不需要title
+        # titleContent = response.xpath('//tr[@class="title"]//td/text()').extract()
+        # title = ""
+        # for string in titleContent:
+        #     # print t.encode('utf-8')
+        #     title = title + string.encode('utf-8')
+        # print title
 
-        for detail in data:
-            x = detail.xpath('./td[@class="left"]/text()').extract()
-            for text in x:
-                print text
-            y = detail.xpath('./td[2]/text()').extract()
-            for text in y:
-                print text            
-            a = detail.xpath('./td/a/text()').extract()
-            # print a
-            for t in a:
-                print t.encode('utf-8')
+        # data = response.xpath('//tr[contains(@class, "left")]')
+
+        # 获取当前页面比赛日期
+        dateOrigin = response.xpath('//tr[contains(@class, "linglei")]')
+        
+        for row in dateOrigin:
+            date = row.xpath('./td/text()').extract_first()
+            print date
+
+            # for text in date:
+            #     print text
+
+            # x = detail.xpath('./td[@class="left"]/text()').extract()
+            # for text in x:
+            #     print text
+            # # y = detail.xpath('./td[2]/text()').extract()
+            # # for text in y:
+            # #     print text            
+            # a = detail.xpath('./td/a/text()').extract()
+            # # print a
+            # for t in a:
+            #     print t.encode('utf-8')
