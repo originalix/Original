d7ec1134560ba2d3e09ec785097e3572aab86544
Lix <xiao.liunit@gmail.com>
2018-02-11 14:45:10 +0800

    [remove row]

--- python2.7/mongodb.py
+++ python2.7/mongodb.py
@@ -16,6 +16,7 @@ def show(collection):
     for item in collection.find():
         print (item)
 
+#  插入
 dic = {
     'name': 'wsxxxxxx',
     'age': 26,
@@ -23,5 +24,21 @@ dic = {
     'blog': 'star1201',
 }
 
-collection.insert(dic)
+# collection.insert(dic)
+# show(collection)
+
+# 插入多条记录
+many = []
+import copy
+for index in range(1, 8):
+    tempdic = copy.deepcopy(dic)
+    tempdic['age'] = index * 2
+    many.append(tempdic)
+
+collection.insert_many(many)
+# show(collection)
+
+# 删除记录
+collection.remove({ 'name' : 'wsxxxxxx' })
 show(collection)
+
