929e30fb28be182006cdbe69e75f62a0b35e01e3
Lix <xiao.liunit@gmail.com>
2018-02-11 15:12:58 +0800

    [测试mongodb 完成]

--- python2.7/mongoconnect.py
+++ python2.7/mongoconnect.py
@@ -12,3 +12,29 @@ host = MONGODB_HOST
 port = MONGODB_PORT
 dbname = MONGODB_DBNAME
 client = pymongo.MongoClient(host=host, port=port)
+
+# 定义数据库
+db = client[dbname]
+table = db[MONGODB_TABLE]
+
+print client
+print db
+print table
+
+table.remove({ 'name' : 'wsxxxxxx' })
+
+dic = {
+    'name': 'wsxxxxxx',
+    'age': 26,
+    'address': '北苑',
+    'blog': 'star1201',
+}
+
+table.insert(dic)
+
+def show(collection):
+    # 查找
+    for item in collection.find():
+        print (item)
+
+show(table)
