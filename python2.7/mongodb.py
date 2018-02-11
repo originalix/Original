108f9cc5eaf413d64a85510b832c9084c4b4a887
Lix <xiao.liunit@gmail.com>
2018-02-11 13:23:55 +0800

    [mongodb 简单插入示例]

--- python2.7/mongodb.py
+++ python2.7/mongodb.py
@@ -4,3 +4,24 @@
 import pymongo
 """Python 操作Mongodb 示例
 """
+
+conn = pymongo.MongoClient()
+db = conn.test
+collection = db.runoob
+
+print(collection)
+
+def show(collection):
+    # 查找
+    for item in collection.find():
+        print (item)
+
+dic = {
+    'name': 'wsxxxxxx',
+    'age': 26,
+    'address': '北苑',
+    'blog': 'star1201',
+}
+
+collection.insert(dic)
+show(collection)
