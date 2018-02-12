334c6a869145fad0eb44fce7401c1085fa52992f
Lix <xiao.liunit@gmail.com>
2018-02-12 20:15:18 +0800

    [获取 行列 信息]

--- python2.7/excel.py
+++ python2.7/excel.py
@@ -8,6 +8,15 @@ def read_exc():
     workbook = xlrd.open_workbook(r'/Users/Lix/Desktop/test.xlsx')
     print workbook.sheet_names()
 
+    main_sheet = workbook.sheet_by_index(0)
+    # sheet的名称，行数，列数
+    print main_sheet.name, main_sheet.nrows,main_sheet.ncols
+
+    # 获取整行和整列的值（数组）
+    rows = main_sheet.row_values(2) # 获取第三行内容
+    cols = main_sheet.col_values(2) # 获取第三列内容
+    print rows
+    print cols
 
 def main():
     read_exc()
