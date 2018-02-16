ae5cfd01dfcdf6f157064091fae2394ea4e9e1f9
Lix <xiao.liunit@gmail.com>
2018-02-17 07:21:03 +0800

    [xlutils excele]

--- python2.7/excel.py
+++ python2.7/excel.py
@@ -53,6 +53,37 @@ def xlswriter():
     #关闭保存
     workbook.close()
 
+def xlutils():
+    import xlwt #写入数据
+    import xlutils #操作excel
+    #----xlrd库
+    #打开excel文件
+    workbook = xlrd.open_workbook('myexcel.xls')
+    #获取表单
+    worksheet = workbook.sheet_by_index(0)
+    #读取数据
+    data = worksheet.cell_value(0,0)
+    #----xlwt库
+    #新建excel
+    wb = xlwt.Workbook()
+    #添加工作薄
+    sh = wb.add_sheet('Sheet1')
+    #写入数据
+    sh.write(0,0,'data')
+    #保存文件
+    wb.save('myexcel.xls')
+    #----xlutils库
+    #打开excel文件
+    book = xlrd.open_workbook('myexcel.xls')
+    #复制一份
+    new_book = xlutils.copy(book)
+    #拿到工作薄
+    worksheet = new_book.getsheet(0)
+    #写入数据
+    worksheet.write(0,0,'new data')
+    #保存
+    new_book.save()
+
 def main():
     read_exc()
 
