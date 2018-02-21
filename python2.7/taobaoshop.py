ee273b8b54fa073de7c2f96f7abc41660a5a98a9
Lix <xiao.liunit@gmail.com>
2018-02-21 18:15:52 +0800

    [增加保存htmlhtml]

--- python2.7/taobaoshop.py
+++ python2.7/taobaoshop.py
@@ -10,10 +10,29 @@ def scrapyTB():
     url = 'https://elcjstyle.taobao.com/search.htm?spm=a1z10.1-c-s.0.0.6b466fcctorM7s&search=y'
     req = urllib2.Request(url)
     response = urllib2.urlopen(req)
-    print response.read().decode('utf-8')
+    # print response.read().decode('utf-8')
+    print response.read()
+
+
+# import urllib.request 
+  
+def getHtml(url):  
+    html = urllib.request.urlopen(url).read()  
+    return html  
+  
+def saveHtml(file_name, file_content):  
+    #    注意windows文件命名的禁用符，比如 /  
+    with open(file_name.replace('/', '_') + ".html", "wb") as f:  
+        #   写文件用bytes而不是str，所以要转码  
+        f.write(file_content)  
+  
+print("下载成功")  
 
 def main():
-    scrapyTB()
+    # scrapyTB()
+    aurl = "https://elcjstyle.taobao.com/search.htm?spm=a1z10.1-c-s.0.0.6b466fcctorM7s&search=y"  
+    html = getHtml(aurl)  
+    saveHtml("sduview", html)  
 
 if __name__ == "__main__":
     main()
