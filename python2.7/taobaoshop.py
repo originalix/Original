052a5f239b98c5049bf03f2882ce62c5fa94c2f7
Lix <xiao.liunit@gmail.com>
2018-02-21 19:47:22 +0800

    [spider for taobao]

--- python2.7/taobaoshop.py
+++ python2.7/taobaoshop.py
@@ -15,6 +15,9 @@ import os
 
 class taobaoShop:
     def __init__(self):
+        """初始化构造函数
+        """
+
         self.site_url = 'https://elcjstyle.taobao.com/search.htm?spm=a1z10.1-c-s.0.0.68616fccLXsimv&search=y'
         self.driver = webdriver.Chrome()
         self.sleep_time = 1
@@ -23,26 +26,23 @@ class taobaoShop:
         self.current_page = 1
 
     def getPage(self):
+        """获取淘宝店铺页面代码
+        """
+
         self.driver.get(self.site_url)
         time.sleep(self.sleep_time)
         content = self.driver.page_source.encode('utf-8')
         print self.driver.title
-        # selector = etree.HTML(content)
-        # print selector
-        # res = selector.xpath('//div[@id="J_ShopSearchResult"]/div/div[2]')
-        # print res
-        # soup = BeautifulSoup(content, 'html.parser')
-        # self.getTotalPage(soup)
-        ele = self.driver.find_element(By.XPATH, '//*[@id="J_ShopSearchResult"]/div/div[2]/div[2]/dl[1]/dt/a/img')
-        print ele
+        
         self.saveHtml('taobaoshop', content)
-        # return soup
 
     def saveHtml(self, file_name, file_content):  
         #    注意windows文件命名的禁用符，比如 /  
         with open(file_name.replace('/', '_') + ".html", "wb") as f:
             #   写文件用bytes而不是str，所以要转码  
             f.write(file_content)
+    
+    
 
 def main():
     tb = taobaoShop()
