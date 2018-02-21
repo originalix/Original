b076e2ca42283f95cc333ff6dc2a9fe324fc46fa
Lix <xiao.liunit@gmail.com>
2018-02-22 07:20:30 +0800

    [create selenium example]

---
+++ python2.7/selenium-example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Lix'
+
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
