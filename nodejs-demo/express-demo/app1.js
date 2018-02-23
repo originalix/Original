fb48ece49affecd504a341239592f7c06ee81b9c
Lix <xiao.liunit@gmail.com>
2018-02-23 16:13:30 +0800

    [router]

--- nodejs-demo/express-demo/app1.js
+++ nodejs-demo/express-demo/app1.js
@@ -7,4 +7,11 @@ router.get('/', function(req, res) {
     res.send('<h1>Hello world</h1>');
 });
 
+// app.use('/home', router);
+app.use('/', router);
 app.use('/home', router);
+
+var port = process.env.PORT || 8080;
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
