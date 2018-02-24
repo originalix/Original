94b234bed31d65edd2841516aa2b8cadc988048f
Lix <xiao.liunit@gmail.com>
2018-02-24 10:35:11 +0800

    [second method for exports]

--- nodejs-demo/require-demo/hello.js
+++ nodejs-demo/require-demo/hello.js
@@ -1,5 +1,11 @@
-var world = function() {
-    console.log('Love wsxxxxxx');
-}
+// 创建模块 方法一
+// var world = function() {
+//     console.log('Love wsxxxxxx');
+// }
 
-exports.world = world;
+// exports.world = world;
+
+// 方法二
+exports.world = function() {
+    console.log('Hello world');
+}
