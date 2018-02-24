62e691d70304b24eba53536f2f81ccb485b7ca95
Lix <xiao.liunit@gmail.com>
2018-02-24 10:31:14 +0800

    [create require main js]

---
+++ nodejs-demo/require-demo/a.js
@@ -0,0 +1,7 @@
+console.log('module.id: ', module.id);
+console.log('module.exports: ', module.exports);
+console.log('module.parent: ', module.parent);
+console.log('module.filename: ', module.filename);
+console.log('module.loaded: ', module.loaded);
+console.log('module.children: ', module.children);
+console.log('module.paths: ', module.paths);
