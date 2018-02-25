604265387a1cd8548df9f409ad73dbe600bd47f2
Lix <xiao.liunit@gmail.com>
2018-02-26 07:24:43 +0800

    [create product category row and product row]

--- react/product-table.js
+++ react/product-table.js
@@ -1,4 +1,28 @@
+class ProductCategoryRow extends React.Component {
+    render() {
+        return (
+            <tr><th colSpan="2">{this.props.category}</th></tr>
+        );
+    }
+}
+
+class ProductRow extends React.Component {
+    render() {
+        const name = this.props.product.stocked ?
+            this.props.product.name :
+            <span style={{color: 'red'}}>
+                this.props.product.name
+            </span>
+        return (
+            <tr>
+                <td>{name}</td>
+                <td>{this.props.product.price}</td>
+            </tr>
+        );
+    }
+}
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('example')
+    <ProductCategoryRow category="nba" />,
+    document.getElementById('root')
 );
