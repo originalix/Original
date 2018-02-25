1f995f2ca78c49ab47bbe6d521676eabce5da609
Lix <xiao.liunit@gmail.com>
2018-02-26 07:36:57 +0800

    [create Product row]

--- react/product-table.js
+++ react/product-table.js
@@ -22,7 +22,32 @@ class ProductRow extends React.Component {
     }
 }
 
+class ProductTable extends React.Component {
+    render() {
+        const rows = [];
+        var lastCategory = null;
+        this.props.products.forEach(function(product) {
+            if (product.category !== lastCategory) {
+                rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+            }
+            rows.push(<ProductRow product={product} key={product.name} />);
+            lastCategory = product.category;
+        });
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>{rows}</tbody>
+            </table>
+        );
+    }
+}
+
 ReactDOM.render(
-    <ProductCategoryRow category="nba" />,
+    <ProductTable />,
     document.getElementById('root')
 );
