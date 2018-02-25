27deab041ad7bebafdd428e8dda0716d212b0a2c
Lix <xiao.liunit@gmail.com>
2018-02-26 07:40:57 +0800

    [test product table]

--- react/product-table.js
+++ react/product-table.js
@@ -25,7 +25,7 @@ class ProductRow extends React.Component {
 class ProductTable extends React.Component {
     render() {
         const rows = [];
-        var lastCategory = null;
+        let lastCategory = null;
         this.props.products.forEach(function(product) {
             if (product.category !== lastCategory) {
                 rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
@@ -47,7 +47,16 @@ class ProductTable extends React.Component {
     }
 }
 
+const PRODUCTS = [
+    {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+    {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+    {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+    {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+    {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+    {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+
 ReactDOM.render(
-    <ProductTable />,
+    <ProductTable products={PRODUCTS} />,
     document.getElementById('root')
 );
