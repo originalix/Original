067e5b227f72156b72d2818a5fbb227981c87021
Lix <xiao.liunit@gmail.com>
2018-02-26 12:59:53 +0800

    [es6 use]

--- react/proptypes.js
+++ react/proptypes.js
@@ -16,4 +16,3 @@ ReactDOM.render(
     <Greeting />,
     document.getElementById('root')
 );
-
---
+++ use-es6/es6.js
@@ -0,0 +1,12 @@
+/**
+ * 1. Let命令
+ */
+
+ // 基本用法
+{
+    let a = 10;
+    var b = 1;
+}
+
+// console.log(a);
+console.log(b);
