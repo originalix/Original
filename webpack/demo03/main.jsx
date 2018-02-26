7122b44249df0c0b74ac213f4ade914620d668f9
Lix <xiao.liunit@gmail.com>
2018-02-26 14:56:28 +0800

    [babel-loader]

---
+++ webpack/demo03/main.jsx
@@ -0,0 +1,7 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+ReactDOM.render(
+    <h1>Hello React!</h1>,
+    document.querySelector('#wrapper')
+);
---
+++ webpack/demo03/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: './main.jsx',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015', 'react']
+                    }
+                }
+            }
+        ]
+    }
+};
