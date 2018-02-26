f7a259d9c2fe696a4f7744a17dba9cdcc0c8a09c
Lix <xiao.liunit@gmail.com>
2018-02-26 14:25:16 +0800

    [webpack usage]

--- use-es6/es6.js
+++ use-es6/es6.js
@@ -2,11 +2,18 @@
  * 1. Let命令
  */
 
- // 基本用法
+ // 基本用法  let只在代码块中有效
 {
     let a = 10;
     var b = 1;
 }
 
 // console.log(a);
-console.log(b);
+// console.log(b);
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+// console.log(i);
+
