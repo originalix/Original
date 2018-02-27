3676c3caba474a142e440ba73bd13ceb03c47d2d
Lix <xiao.liunit@gmail.com>
2018-02-27 10:08:29 +0800

    [code splitting]

--- webpack/demo09/main.js
+++ webpack/demo09/main.js
@@ -3,3 +3,10 @@ document.write('<h1>Hello World</h1>');
 if (__DEV__) {
     document.write(new Date());
 }
+
+require.ensure(['./a'], function (require) {
+    var content = require('./a');
+    document.open();
+    document.write('<h1>' + content + '</h1>');
+    document.close();
+});
