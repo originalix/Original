df9066f2b8d9b28160c66aad7616fc22c774802b
Lix <xiao.liunit@gmail.com>
2018-02-28 07:34:50 +0800

    [es6 test]

--- use-es6/es6.js
+++ use-es6/es6.js
@@ -30,11 +30,44 @@
 
 
 // ES5 环境
-function f() { console.log('I am outside!'); }
+// function f() { console.log('I am outside!'); }
 
-(function () {
-  function f() { console.log('I am inside!'); }
-  if (false) {
-  }
-  f();
-}());
+// (function () {
+//   function f() { console.log('I am inside!'); }
+//   if (false) {
+//   }
+//   f();
+// }());
+
+// function log(x, y = 'javascript') {
+//     console.log(x, y);
+// }
+
+// log('Hello');
+// log('Hello', 'China');
+
+// function Point(x=0, y=0) {
+//     this.x = x;
+//     this.y = y;
+// }
+
+// const p = new Point();
+// console.log(p);
+
+// 函数 箭头函数
+const numbers = (...nums) => nums;
+console.log(numbers(1, 2, 4, 21, 50));
+
+let getTempItems = id => ({id: id, name: "temp"});
+console.log(getTempItems(2150));
+
+const full = ({first, last}) => first + ' ' + last;
+// function Person(first, last) {
+//     this.first = first;
+//     this.last = last;
+// }
+
+let Person = (first, last) => ({first: first, last: last});
+
+const person = Person('li', 'xxxxxxx');
+console.log(full(person));
