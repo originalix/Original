ab09258f105da1f32bd49a28eb9479c2b0bf0741
Lix <xiao.liunit@gmail.com>
2018-02-27 16:46:14 +0800

    [create 04-nested-routes]

---
+++ react/react-router-tutorial/04-nested-routes/modules/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello, React Router!</h1>
+                <ul role="nav">
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/repos">Repos</Link></li>
+                </ul>
+            </div>
+        );
+    }
+};
---
+++ react/react-router-tutorial/04-nested-routes/modules/Repos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Repos extends React.Component {
+    render() {
+        return (
+            <div>
+                Repos
+            </div>
+        );
+    }
+};
