251c171f4e06d397dd7fb48d55d28a215d0bc031
Lix <xiao.liunit@gmail.com>
2018-02-27 10:16:29 +0800

    [code splitting with bundle-loader]

--- webpack/demo09/main.js
+++ webpack/demo09/main.js
@@ -4,9 +4,23 @@ if (__DEV__) {
     document.write(new Date());
 }
 
-require.ensure(['./a'], function (require) {
-    var content = require('./a');
+/**
+ * Code splitting 
+ */
+
+// require.ensure(['./a'], function (require) {
+//     var content = require('./a');
+//     document.open();
+//     document.write('<h1>' + content + '</h1>');
+//     document.close();
+// });
+
+/**
+ * Code Splitting with bundle-loader
+ */
+var load = require('bundle-loader!./a.js');
+load(function(file)  {
     document.open();
-    document.write('<h1>' + content + '</h1>');
+    document.write('<h1>' + file + '</h1>');
     document.close();
 });
