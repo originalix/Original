df1007f21b5049b7ceb5aec52f72e468523e48c3
Lix <xiao.liunit@gmail.com>
2018-02-27 11:53:14 +0800

    [create demo 14]

---
+++ webpack/demo14/main.jsx
@@ -0,0 +1,8 @@
+var data = require('data');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  <h1>{data}</h1>,
+  document.getElementById('root')
+);
---
+++ webpack/demo14/webpack.config.1.js
@@ -0,0 +1,26 @@
+module.exports = {
+    entry: './main.jsx',
+    output: {
+      filename: 'bundle.js'
+    },
+    module: {
+      rules:[
+        {
+          test: /\.js[x]?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015', 'react']
+            }
+          }
+        },
+      ]
+    },
+    externals: {
+      // require('data') is external and available
+      //  on the global var data
+      'data': 'data'
+    }
+  };
+  
