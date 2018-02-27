296d209349c9ef4c2781f1ffefebb6c26bd653a2
Lix <xiao.liunit@gmail.com>
2018-02-27 12:25:04 +0800

    [create demo 15]

---
+++ webpack/demo15/webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: '/\.css$/',
+                use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015', 'react']
+                    }
+                }
+            },
+        ]
+    }
+};
