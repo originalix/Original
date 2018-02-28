ebeb96139766a655cd5fc10a14802f3c6788e013
Lix <xiao.liunit@gmail.com>
2018-02-28 15:53:16 +0800

    [config public path]

--- react/react-router-tutorial/11-productionish-server/server.js
+++ react/react-router-tutorial/11-productionish-server/server.js
@@ -3,10 +3,10 @@ var path = require('path');
 
 var app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 var PORT = process.env.PORT || 8080;
