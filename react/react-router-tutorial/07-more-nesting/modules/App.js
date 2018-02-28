c6e1a268392a2b6731fb50c0ed93e04a0b479159
Lix <xiao.liunit@gmail.com>
2018-02-28 14:33:24 +0800

    [finish index route]

--- react/react-router-tutorial/07-more-nesting/modules/App.js
+++ react/react-router-tutorial/07-more-nesting/modules/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 import NavLink from './NavLink';
+import Home from './Home';
 
 export default class App extends React.Component {
     render() {
