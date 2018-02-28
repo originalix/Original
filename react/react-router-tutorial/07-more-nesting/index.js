96b17d18bb1b18513950c138ab4b6bd8f55deb5f
Lix <xiao.liunit@gmail.com>
2018-02-28 14:49:48 +0800

    [finish clean urls]

--- react/react-router-tutorial/07-more-nesting/index.js
+++ react/react-router-tutorial/07-more-nesting/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+import { Router, Route, hashHistory, IndexRoute, browserHistory } from 'react-router'
 import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
@@ -9,7 +9,7 @@ import Home from './modules/Home'
 
 
 render((
-    <Router history={hashHistory}>
+    <Router history={browserHistory}>
         <Route path='/' component={App}>
             <IndexRoute component={Home}/>
             <Route path='/repos' component={Repos}> 
