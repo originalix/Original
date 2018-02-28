b97e7af3e9ee48f30159dc2ae1d5213943f5cfc2
Lix <xiao.liunit@gmail.com>
2018-03-01 07:19:43 +0800

    [form for navigation]

--- react/react-router-tutorial/11-productionish-server/modules/Repos.js
+++ react/react-router-tutorial/11-productionish-server/modules/Repos.js
@@ -10,6 +10,13 @@ export default class Repos extends React.Component {
                 <ul>
                     <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
                     <li><NavLink to="/repos/facebook/react">React</NavLink></li>
+                    <li>
+                        <form onSubmit={this.handleSubmit}>
+                            <input type="text" placeholder="userName"/> / {' '}
+                            <input type="text" placeholder="repo"/>{' '}
+                            <button type="submit">Go</button>
+                        </form>
+                    </li>
                 </ul>
                 {this.props.children}
             </div>
