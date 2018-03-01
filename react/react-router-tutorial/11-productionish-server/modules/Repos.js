2241cf5a0d76f90c208b1fb63d4d52ecc637480d
Lix <xiao.liunit@gmail.com>
2018-03-01 11:17:12 +0800

    [router history]

--- react/react-router-tutorial/11-productionish-server/modules/Repos.js
+++ react/react-router-tutorial/11-productionish-server/modules/Repos.js
@@ -1,46 +1,36 @@
-import React from 'react';
-import NavLink from './NavLink';
-import { browserHistory } from 'react-router';
+import React from 'react'
+import NavLink from './NavLink'
 
-const PropTypes = require('prop-types');
+export default React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object
+  },
 
-export default class Repos extends React.Component {
-    constructor(props) {
-        super(props)
-        // context.router;
-    }
-    
-    handleSubmit(event) {
-        event.preventDefault();
-        const userName = event.target.elements[0].value;
-        const repo = event.target.elements[1].value;
-        const path = `/repos/${userName}/${repo}`;
-        console.log(path);
-        // browserHistory.push(path);
-        this.context.router.push(path);
-    };
+  handleSubmit(event) {
+    event.preventDefault()
+    const userName = event.target.elements[0].value
+    const repo = event.target.elements[1].value
+    const path = `/repos/${userName}/${repo}`
+    this.context.router.push(path)
+  },
 
-    render() {
-        return (
-            <div>
-                <h2>Repos</h2>
-                <ul>
-                    <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
-                    <li><NavLink to="/repos/facebook/react">React</NavLink></li>
-                    <li>
-                        <form onSubmit={this.handleSubmit}>
-                            <input type="text" placeholder="userName"/> / {' '}
-                            <input type="text" placeholder="repo"/>{' '}
-                            <button type="submit">Go</button>
-                        </form>
-                    </li>
-                </ul>
-                {this.props.children}
-            </div>
-        );
-    }
-}
-
-Repos.contextTypes = {
-    router: PropTypes.object
-}
+  render() {
+    return (
+      <div>
+        <h2>Repos</h2>
+        <ul>
+          <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
+          <li><NavLink to="/repos/facebook/react">React</NavLink></li>
+          <li>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" placeholder="userName"/> / {' '}
+              <input type="text" placeholder="repo"/>{' '}
+              <button type="submit">Go</button>
+            </form>
+          </li>
+        </ul>
+        {this.props.children}
+      </div>
+    )
+  }
+})
