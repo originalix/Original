170fa254e02b1b1a1e7ef9dd0861ae31f65c0bf3
Lix <xiao.liunit@gmail.com>
2018-03-01 20:41:28 +0800

    [change server.js]

--- react/react-router-tutorial/13-server-rendering/index.js
+++ react/react-router-tutorial/13-server-rendering/index.js
@@ -1,21 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, hashHistory, IndexRoute, browserHistory } from 'react-router'
-import App from './modules/App'
-import About from './modules/About'
-import Repos from './modules/Repos'
-import Repo from './modules/Repo'
-import Home from './modules/Home'
+// import App from './modules/App'
+// import About from './modules/About'
+// import Repos from './modules/Repos'
+// import Repo from './modules/Repo'
+// import Home from './modules/Home'
+import routes from './modules/routes'
 
-
-render((
-    <Router history={browserHistory}>
-        <Route path='/' component={App}>
-            <IndexRoute component={Home}/>
-            <Route path='/repos' component={Repos}> 
-                <Route path='/repos/:userName/:repoName' component={Repo}/>
-            </Route>
-            <Route path='/about' component={About}/>
-        </Route>
-    </Router>
-), document.getElementById('app'));
+render(
+    <Router routes={routes} history={browserHistory}/>,
+    document.getElementById('app')
+);
--- react/react-router-tutorial/13-server-rendering/server.js
+++ react/react-router-tutorial/13-server-rendering/server.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { match, RouterContext } from 'react-router';
+import routes from './modules/routes';
+
 var express = require('express');
 var path = require('path');
 var compression = require('compression');
@@ -7,6 +12,30 @@ var app = express();
 app.use(compression())
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+    match({ routes: routes, location: req.url }, (err, redirect, props) => {
+        const appHtml = renderToString(<RouterContext {...props}/>);
+        res.send(renderPage(appHtml));
+    });
+});
+
+function renderPage(appHtml) {
+    return `
+    <!DOCTYPE html public="storage">
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+        <title>Lix's first react-router</title>
+    </head>
+    <body>
+        <div id="app">${appHtml}</div>
+        <script scr="/bundle.js"></script>
+    </body>
+    `
+}
 
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
