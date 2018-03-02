18d48da048636ec20d454b95422605684255bc05
Lix <xiao.liunit@gmail.com>
2018-03-02 15:19:05 +0800

    [counter js]

---
+++ redux/examples/counter/src/components/Counter.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Counter extends Component {
+    constructor(props) {
+        super(props)
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Counter;
