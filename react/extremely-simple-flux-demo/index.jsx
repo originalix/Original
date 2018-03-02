010de40f36394b3e8f9b7b7d5a71da31a2009de6
Lix <xiao.liunit@gmail.com>
2018-03-02 10:27:46 +0800

    [webpack test]

---
+++ react/extremely-simple-flux-demo/index.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+    <div>
+        <h1>Hello World</h1>
+    </div>,
+    document.getElementById('example')
+);
