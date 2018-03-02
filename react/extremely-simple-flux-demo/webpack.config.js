c816bf68fe04e62fbfde80dd706432bb94cd0397
Lix <xiao.liunit@gmail.com>
2018-03-02 10:28:03 +0800

    [no message]

--- react/extremely-simple-flux-demo/webpack.config.js
+++ react/extremely-simple-flux-demo/webpack.config.js
@@ -21,27 +21,3 @@ module.exports = {
         new OpenBrowserPlugin({ url: 'http://localhost:8080' })
     ]
 };
-
-// var webpack = require('webpack');
-// var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
-// var OpenBrowserPlugin = require('open-browser-webpack-plugin');
-
-// module.exports = {
-//   entry: './index.jsx',
-//   output: {
-//     filename: 'bundle.js'
-//   },
-//   resolve: {
-//     extensions: ['', '.js', '.jsx'],
-//   },
-//   module: {
-//     loaders:[
-//       { test: /\.jsx$/, exclude: /node_modules/, loader: 'jsx-loader' },
-//       { test: /\.js$/, exclude:/node_modules/, loader: 'babel-loader'},
-//     ]
-//   },
-//   plugins: [
-//     new CommonsChunkPlugin('init.js'),
-//     new OpenBrowserPlugin({ url: 'http://localhost:8080' })
-//   ]
-// };
