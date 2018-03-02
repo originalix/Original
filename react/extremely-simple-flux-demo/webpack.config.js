9cd8a26926c2f542ed1cd3bb52a2675f23d2d1fc
Lix <xiao.liunit@gmail.com>
2018-03-02 10:19:58 +0800

    [config webpack config js]

---
+++ react/extremely-simple-flux-demo/webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack');
+var CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+module.exports = {
+    entry: './index.jsx',
+    output: {
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+    },
+    module: {
+        loaders: [
+            { test: /\.jsx$/, exclude: /node_modules/, loader: 'jsx-loader' },
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+        ]
+    },
+    plugins: [
+        new CommonsChunkPlugin('init.js'),
+        new OpenBrowserPlugin({ url: 'http://loacalhost:8080' })
+    ]
+};
