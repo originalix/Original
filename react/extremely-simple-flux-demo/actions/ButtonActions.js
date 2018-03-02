75a601c5a5bc0c57a0f547b119dbe04e8661eccc
Lix <xiao.liunit@gmail.com>
2018-03-02 12:45:47 +0800

    [finish ButtonActions]

---
+++ react/extremely-simple-flux-demo/actions/ButtonActions.js
@@ -0,0 +1,12 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+
+var ButtonActions = {
+    addNewItem: function (text) {
+        AppDispatcher.dispatch({
+            actionType: 'ADD_NEW_ITEM',
+            text: text
+        });
+    },
+};
+
+module.exports = ButtonActions;
