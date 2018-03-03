cd09371a258afcc6db113f4bd1c24f7b55a71818
Lix <xiao.liunit@gmail.com>
2018-03-03 20:35:22 +0800

    [finish TodoList js]

--- redux/examples/todo-list/components/Todo.js
+++ redux/examples/todo-list/components/Todo.js
@@ -12,7 +12,7 @@ const Todo = ({onClick, completed, text}) => (
     </li>
 );
 
-Todo.PropTypes = {
+Todo.propTypes = {
     onClick: PropTypes.func.isRequired,
     completed: PropTypes.bool.isRequired,
     text: PropTypes.string.isRequired
