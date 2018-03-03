0d69d3f5b1e70650e51e2e1d11064c2eb7e597c4
Lix <xiao.liunit@gmail.com>
2018-03-03 09:54:58 +0800

    [finish action]

--- redux/examples/document-demo/action.js
+++ redux/examples/document-demo/action.js
@@ -13,3 +13,18 @@ export const VisibilityFilters = {
     SHOW_COMPLETED: 'SHOW_COMPLETED',
     SHOW_ACTIVE: 'SHOW_ACTIVE'
 }
+
+/**
+ *  action 创建函数
+ */
+export function addTodo(text) {
+    return { type: ADD_TODO, text }
+}
+
+export function toggleTodo(index) {
+    return { type: TOGGLE_TODO, index }
+}
+
+export function setVisibilityFilter(filter) {
+    return { type: SET_VISIBILITY_FILTER, filter }
+}
