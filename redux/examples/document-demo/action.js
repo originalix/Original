cf7871a0c18707f84253dc7a0a5e6f7c3b3952b2
Lix <xiao.liunit@gmail.com>
2018-03-03 09:53:02 +0800

    [action js]

---
+++ redux/examples/document-demo/action.js
@@ -0,0 +1,15 @@
+/** 
+ *  action 类型 
+ */
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+
+/**
+ *  其他的常量f
+ */
+export const VisibilityFilters = {
+    SHOW_ALL: 'SHOW_ALL',
+    SHOW_COMPLETED: 'SHOW_COMPLETED',
+    SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
