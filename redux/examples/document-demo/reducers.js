3805e567f1e2d57fa7855e6293be898fe9da7fce
Lix <xiao.liunit@gmail.com>
2018-03-03 11:16:38 +0800

    [拆分reducer]

--- redux/examples/document-demo/reducers.js
+++ redux/examples/document-demo/reducers.js
@@ -1,9 +1,38 @@
-import { VisibilityFilter } from './action'
+import { 
+    ADD_TODO,
+    TOGGLE_TODO,
+    SET_VISIBILITY_FILTER,
+    VisibilityFilter
+} from './action'
 
 const initialState = {
     visibilityFilter: VisibilityFilters.SHOW_ALL,
     todos: []
 };
+
+function todos(state = [], action) {
+    switch (action.type) {
+        case ADD_TODO:
+            return [
+                ...state,
+                {
+                    text: action.text,
+                    completed: false
+                }
+            ]
+        case TOGGLE_TODO:
+            return state.map((todo, index) => {
+                if (index === action.index) {
+                    return Object.assign({}, todo, {
+                        completed: !tode.completed
+                    })
+                }
+                return tode
+            })
+        default:
+            return state
+    }
+}
 
 function todoApp(state = initialState, action) {
     switch (action.type) {
@@ -11,6 +40,15 @@ function todoApp(state = initialState, action) {
             return Object.assign({}, state, {
                 visibilityFilter: action.filter
             });
+        
+        case ADD_TODO:
+            return Object.assign({}, state, {
+                todos: todos(state.todos, action)
+            })
+        case TOGGLE_TODO:
+            return Object.assign({}, state, {
+                todos: todos(state.todos, action)
+            })
         default:
             return state;
     }
