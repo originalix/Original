dc2fc1434363c5457e66227d9eeb72cfcf6e85f0
Lix <xiao.liunit@gmail.com>
2018-03-04 20:48:24 +0800

    [Footer components]

--- redux/examples/todo-list/src/components/Footer.js
+++ redux/examples/todo-list/src/components/Footer.js
@@ -1,1 +1,23 @@
 import React from 'react'
+import FilterLink from '../containers/FilterLink'
+import { VisibilityFilter } from '../actions'
+
+const Footer = () => {
+    <p>
+        Show:
+        {" "}
+        <FilterLink filter={ VisibilityFilters.SHOW_ALL }>
+            ALL
+        </FilterLink>
+        {", "}
+        <FilterLink filter={ VisibilityFilters.SHOW_ACTIVE }>
+            Active
+        </FilterLink>
+        {", "}
+        <FilterLink filter={ VisibilityFilters.SHOW_COMPLETED }>
+            Completed
+        </FilterLink>
+    </p>
+}
+
+export default Footer;
