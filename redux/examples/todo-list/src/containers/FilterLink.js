63b9761e581369e564a160216cf5a5ddafa2ee3e
Lix <xiao.liunit@gmail.com>
2018-03-04 20:45:05 +0800

    [finish Filter Link]

--- redux/examples/todo-list/src/containers/FilterLink.js
+++ redux/examples/todo-list/src/containers/FilterLink.js
@@ -2,3 +2,19 @@ import { connect } from 'react-redux'
 import { setVisibilityFilter } from '../actions'
 import Link from '../components/Link'
 
+const mapStateToProps = (state, ownProps) => ({
+    active: ownProps.filter === state.visibilityFilter
+})
+
+const mapDispatchToPorps = (dispatch, ownProps) => ({
+    onClick: () => {
+        dispatch(setVisibityFilter(ownProps.filter))
+    }
+})
+
+const FilterLink = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Link)
+
+export default FilterLink;
