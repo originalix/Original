efc329f30729a44181d1037093d4811a7a091500
Lix <xiao.liunit@gmail.com>
2018-03-06 07:17:47 +0800

    [finish posts]

--- redux/examples/reddit-async/src/reducers/reducers.js
+++ redux/examples/reddit-async/src/reducers/reducers.js
@@ -15,46 +15,35 @@ const selectedSubreddit = (state = 'reactjs', action) => {
     }
 }
 
-// const posts = (
-//     state = {
-//         isFetching: false,
-//         didInvalidate: false,
-//         items: []
-//     }, action) => {
-//         switch (action.type) {
-//             case INVALIDATE_SUBREDDIT:
-//                 return Object.assign({}, state, {
-//                     didInvalidate: true
-//                 })
-//             case REQUEST_POSTS:
-//                 return Object.assign({}, state, {
-//                     isFetching: true,
-//                     didInvalidate: false
-//                 })
-//             case RECEIVE_POSTS:
-//                 return Object.assign({}, state, {
-//                     isFetching: false,
-//                     didInvalidate: false,
-//                     items: action.posts,
-//                     lastUpdated: action.receivedAt
-//                 })
-//             default:
-//                 return state
-//         }
-//     }
-
-// function postsBySubreddit(state = {}, action) {
-//     switch (action.type) {
-//         case INVALIDATE_SUBREDDIT:
-//         case RECEIVE_POSTS:
-//         case REQUEST_POSTS:
-//             return Object.assign({}, state, {
-//                 [action.subreddit]: posts(state[action.subreddit], action)
-//             })
-//         default:
-//             return state
-//     }
-// }
+const posts = (state = {
+    isFetching: false,
+    didInvalidate: false,
+    items: []
+}, action) => {
+    switch (action.type) {
+        case INVALIDATE_SUBREDDIT:
+            return {
+                ...state,
+                didInvalidate: true
+            }
+        case REQUEST_POSTS:
+            return {
+                ...state,
+                isFetching: true,
+                didInvalidate: false
+            }
+        case RECEIVE_POSTS:
+            return {
+                ...state,
+                isFetching: false,
+                didInvalidate: false,
+                items: action.posts,
+                lastUpdated: action.receivedAt
+            }
+        default:
+            return state
+    }
+}
 
 const rootReducer = combineReducers({
     postsBySubreddit,
