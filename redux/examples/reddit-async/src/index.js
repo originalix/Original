722c9f13f1441380a313f07a05e8877360148021
Lix <xiao.liunit@gmail.com>
2018-03-05 14:13:11 +0800

    [finish index.js]

--- redux/examples/reddit-async/src/index.js
+++ redux/examples/reddit-async/src/index.js
@@ -4,3 +4,18 @@ import { createStore, applyMiddleware } from 'redux'
 import { selectSubreddit, fetchPosts } from './actions'
 import rootReducer from './reducers'
 
+const loggerMiddleware = createLogger()
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(
+        thunkMiddleware,
+        loggerMiddleware
+    )
+)
+
+store.dispatch(selectSubreddit('reactjs'))
+store
+    .dispatch(fetchPosts('reactjs'))
+    .then(() => console.log(store.getState())
+)
