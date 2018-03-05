53f5bc054de0efaa5066c11e49ce79a5336e1a0d
Lix <xiao.liunit@gmail.com>
2018-03-05 13:44:03 +0800

    [fetch posts]

--- redux/examples/reddit-async/src/actions/index.js
+++ redux/examples/reddit-async/src/actions/index.js
@@ -34,3 +34,24 @@ function fetchPosts(subreddit) {
             .then(json => dispatch(receivePosts(subreddit, json)))
     }
 }
+
+function shouldFetchPosts(state, subreddit) {
+    const posts = state.postsBySubreddit[subreddit]
+    if (!posts) {
+        return true
+    } else if (posts.isFetching) {
+        return false
+    } else {
+        return posts.didInvalidate
+    }
+}
+
+export function fetchPostsIfNeeded(subreddit) {
+    return (dispatch, getState) => {
+        if (shouldFetchPosts(getState(), subreddit)) {
+            return dispatch(fetchPosts(subreddit))
+        } else {
+            return Promise.resolve()
+        }
+    }
+}
