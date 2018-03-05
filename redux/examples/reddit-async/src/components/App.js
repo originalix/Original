c16f8f8636a34c6c6b9916777ec9476d0354ad92
Lix <xiao.liunit@gmail.com>
2018-03-05 20:54:28 +0800

    [subReddit]

--- redux/examples/reddit-async/src/components/App.js
+++ redux/examples/reddit-async/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { fetchPostsIfNeeded } from '../actions'
+import { selectSubreddit, fetchPostsIfNeeded } from '../actions'
 import Picker from './Picker'
 
 class App extends Component {
@@ -11,14 +11,20 @@ class App extends Component {
     
     componentDidMount() {
         const { dispatch } = this.props
-        console.log(this.props);
+        console.log(this.props.selectedSubreddit);
+        dispatch(selectSubreddit('React'))
         dispatch(fetchPostsIfNeeded('Kobe'))
     }
 
+    handleChange = nextSubreddit => {
+        this.props.dispatch(selectSubreddit(nextSubreddit))
+    }
+
     render() {
+        const { selectedSubreddit } = this.props;
         return (
             <div>
-                <Picker value='Kobe' onChange={function(){}} options={[ 'reactjs', 'Kobe' ]}/>
+                <Picker value={selectedSubreddit} onChange={this.handleChange} options={[ 'reactjs', 'Kobe' ]}/>
                 <h1>Hello world</h1>
             </div>
         );
--- redux/examples/reddit-async/src/reducers/reducers.js
+++ redux/examples/reddit-async/src/reducers/reducers.js
@@ -6,7 +6,7 @@ import {
     RECEIVE_POSTS
 } from '../actions'
 
-function selectedsubreddit(state = 'reactjs', action) {
+function selectedSubreddit(state = 'reactjs', action) {
     switch (action.type) {
         case SELECT_SUBREDDIT:
             return action.subreddit
@@ -60,7 +60,7 @@ function postsBySubreddit(state = {}, action) {
 
 const rootReducer = combineReducers({
     postsBySubreddit,
-    selectedsubreddit
+    selectedSubreddit
 })
 
 export default rootReducer;
