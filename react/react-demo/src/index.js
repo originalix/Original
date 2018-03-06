994967990bfbbd8f6fe44be0b5be68a8ae820e93
Lix <xiao.liunit@gmail.com>
2018-03-06 13:45:06 +0800

    [install antd]

--- react/react-demo/src/index.js
+++ react/react-demo/src/index.js
@@ -4,5 +4,24 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { DatePicker, version } from 'antd'
+import 'antd/dist/antd.css'
+
+{/* antd test */}
+
+ReactDOM.render(
+    <div style={{ margin: 24 }}>
+        <p style={{ marginBottom: 24 }}>
+            Current antd version: {version} <br/>
+            You can change antd version.
+        </p>
+        <DatePicker />
+    </div>,
+    document.getElementById('root')
+);
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+
