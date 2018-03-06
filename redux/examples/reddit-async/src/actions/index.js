2b4c8edbc7e59d9b66b80fa5ed8e1e6794e25632
Lix <xiao.liunit@gmail.com>
2018-03-06 09:56:42 +0800

    [no message]

--- redux/examples/reddit-async/src/actions/index.js
+++ redux/examples/reddit-async/src/actions/index.js
@@ -27,15 +27,12 @@ export const invalidateSubreddit = subreddit => ({
     subreddit
 })
 
-export function fetchPosts(subreddit) {
-    return dispatch => {
-        dispatch(requestPosts(subreddit))
-        return fetch(`http://www.reddit.com/r/${subreddit}.json`)
-            .then(response => response.json())
-            .then(json => dispatch(receivePosts(subreddit, json)))
-    }
+export const fetchPosts = subreddit => dispatch => {
+    dispatch(requestPosts(subreddit))
+    return fetch(`http://www.reddit.com/r/${subreddit}.json`)
+        .then(response => response.json())
+        .then(json => dispatch(receivePosts(subreddit, json)))
 }
-
 
 function shouldFetchPosts(state, subreddit) {
     const posts = state.postsBySubreddit[subreddit]
