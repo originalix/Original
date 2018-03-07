2261283bd51c8d62cc8b90de0bb5685e5805f2b7
Lix <xiao.liunit@gmail.com>
2018-03-07 12:03:50 +0800

    [add navigation]

---
+++ react/shadowsocks/src/components/NavBar/Navigation.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { NavBar } from 'antd-mobile';
+
+class Navigation extends Component {
+    render() {
+        return (
+            <NavBar mode="dark">
+                ShadowSocks
+            </NavBar>
+        );
+    }
+}
+
+export default Navigation;
