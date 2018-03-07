b9eadb88ea8713558eda259171eaa5636328f430
Lix <xiao.liunit@gmail.com>
2018-03-07 10:52:42 +0800

    [config redux]

--- react/shadowsocks/src/index.js
+++ react/shadowsocks/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App /> 
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
---
+++ react/shadowsocks/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import todos from './todos';
+
+const reducer = combineReducers({
+    todos
+});
+
+export default reducer;
