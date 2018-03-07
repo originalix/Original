d185c8c7259e37a2eff0ad9de4eeb171a3177984
Lix <xiao.liunit@gmail.com>
2018-03-07 13:49:44 +0800

    [finish homepage layout]

--- react/shadowsocks/src/components/Home/List.js
+++ react/shadowsocks/src/components/Home/List.js
@@ -10,10 +10,16 @@ class HomeList extends Component {
                 <List renderHeader={() => ''} className="home-list">
                     <Item 
                         thumb="/img/cloud.png"
-                        arrow="horizontal">选择服务器</Item>
+                        arrow="horizontal"
+                        extra="韩国(103.86.44.141)">
+                        选择服务器
+                    </Item>
                     <Item 
                         thumb="/img/earth.png"
-                        arrow="horizontal">代理模式</Item>
+                        arrow="horizontal"
+                        extra="自动代理模式">
+                        代理模式
+                    </Item>
                 </List>
             </div>
         );
