86e1e01b7f43f448fdeabb64be5ce38511653ac2
Lix <xiao.liunit@gmail.com>
2018-03-07 14:21:28 +0800

    [no message]

--- react/shadowsocks/src/components/NavBar/Navigation.js
+++ react/shadowsocks/src/components/NavBar/Navigation.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import { NavBar } from 'antd-mobile';
+import { NavBar, Icon } from 'antd-mobile';
+import './navigation.css';
 
 class Navigation extends Component {
     render() {
         return (
-            <NavBar mode="dark">
+            <NavBar 
+                mode="dark"
+                icon={<i className="iconfont left-item">&#xe62f;</i>}
+            >
                 ShadowSocks
             </NavBar>
         );
