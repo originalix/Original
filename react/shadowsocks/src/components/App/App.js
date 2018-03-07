f8cdd52179b3a7d1a0e8b96b9665488c1d114891
Lix <xiao.liunit@gmail.com>
2018-03-07 12:38:53 +0800

    [ConnectView]

--- react/shadowsocks/src/components/App/App.js
+++ react/shadowsocks/src/components/App/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from '../../logo.svg';
 import './App.css';
 import Navigation from '../NavBar/Navigation';
+import ConnectView from '../ConnectView/ConnectView';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
+        <ConnectView />
       </div>
     );
   }
---
+++ react/shadowsocks/src/components/ConnectView/ConnectView.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './connect-view.css';
+
+class ConnectView extends Component {
+    render() {
+        return (
+            <div className="wrap">
+                ConnectView
+            </div>
+        );
+    }
+}
+
+export default ConnectView;
