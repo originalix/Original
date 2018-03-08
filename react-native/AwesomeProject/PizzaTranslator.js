01decfacae990076c9a38a1d2c7e1c48ea9caba0
Lix <xiao.liunit@gmail.com>
2018-03-08 21:51:45 +0800

    [use ScrollView]

--- react-native/AwesomeProject/PizzaTranslator.js
+++ react-native/AwesomeProject/PizzaTranslator.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { Text, TextInput, View, Image, ScrollView } from 'react-native';
 class PizzaTranslator extends Component {
   constructor(props) {
     super(props);
@@ -17,6 +17,43 @@ class PizzaTranslator extends Component {
         <Text style={{padding: 10, fontSize: 42}}>
           {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
         </Text>
+
+        <ScrollView>
+          <Text style={{fontSize:96}}>Scroll me plz</Text>
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+
+          <Text style={{fontSize:96}}>Hello World</Text>
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          
+          <Text style={{fontSize:96}}>React</Text>
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          
+          <Text style={{fontSize:96}}>React-Native</Text>
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          
+          <Text style={{fontSize:96}}>Javascript</Text>
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+          <Image source={require('./favicon.png')} style={{height: 100, width: 100}} />
+        </ScrollView>
       </View>
     );
   }
