3683db07b8920ba910800d8a5b5e35223c384e3d
Lix <xiao.liunit@gmail.com>
2018-03-08 10:59:43 +0800

    [select country]

--- react/shadowsocks/src/components/AddProxy/SelectCountry.js
+++ react/shadowsocks/src/components/AddProxy/SelectCountry.js
@@ -11,14 +11,17 @@ class SelectCountry extends Component {
             {value: 0, label: '火星', icon: 'flag1'},
             {value: 1, label: '美国', icon: 'United States'},
             {value: 2, label: '香港', icon: 'Hong Kong'},
-            {value: 3, label: '加拿大', icon: ''},
-            {value: 3, label: '德国', icon: ''},
-            {value: 3, label: '法国', icon: ''},
-            {value: 3, label: '日本', icon: ''},
-            {value: 3, label: '韩国', icon: ''},
-            {value: 3, label: '新加坡', icon: ''},
-            {value: 3, label: '加拿大', icon: ''},
+            {value: 3, label: '加拿大', icon: 'Canada'},
+            {value: 4, label: '德国', icon: 'Germany'},
+            {value: 5, label: '法国', icon: 'France'},
+            {value: 6, label: '日本', icon: 'Japan'},
+            {value: 7, label: '韩国', icon: 'South Korea'},
+            {value: 8, label: '新加坡', icon: 'Singapore'},
+            {value: 9, label: '英国', icon: 'United Kingdom'},
+            {value: 10, label: '澳大利亚', icon: 'Australia'},
+            {value: 11, label: '荷兰', icon: 'Netherlands'},
         ];
+
         return (
             <div>
                 <NormalNavigation title="选择国家" />
