a760b9f2729b6911c2c09a47512262aa9387e6dd
Lix <xiao.liunit@gmail.com>
2018-03-08 16:40:02 +0800

    [change react-native demo]

--- react-native/AwesomeProject/App.js
+++ react-native/AwesomeProject/App.js
@@ -1,67 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, { Component } from 'react'
+import { Text, View, Image } from 'react-native'
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.welcome}>
-            Welcome to React Native!
-            </Text>
-            <Text style={styles.instructions}>
-            To get started, edit App.js
-            </Text>
-            <Text style={styles.instructions}>
-            {instructions}
-            </Text>
-
-            <Text style={styles.bigFont}>
-            This is My first react-native project
-            </Text>
-        </View>
-    );
-  }
+export default class App extends Component {
+    render() {
+        let pic = {
+            uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+        };
+        return (
+            <View style={{alignItems: 'center'}}>
+                <Text> textInComponent </Text>
+                <Image source={pic} style={{width: 193, height: 110}} />
+                <View style={{alignItems: 'center'}}>
+                    <Greeting name='Kobe' />
+                    <Greeting name='Curry' />
+                    <Greeting name='Durant' />
+                </View>
+            </View>
+        )
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  bigFont: {
-    fontSize: 40,
-    textAlign: 'center',
-    margin: 10
-  }
-});
+class Greeting extends Component {
+    render() {
+        return (
+            <Text>Hello {this.props.name}!</Text>
+        );
+    }
+}
