12f3fad0c2b9cc776fdb30bc52a168613678d344
Lix <xiao.liunit@gmail.com>
2018-03-08 22:10:37 +0800

    [finish SectionList]

--- react-native/AwesomeProject/FlatListBasics.js
+++ react-native/AwesomeProject/FlatListBasics.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, SectionList } from 'react-native';
 
 class FlatListBasics extends Component {
   render() {
@@ -15,9 +15,18 @@ class FlatListBasics extends Component {
     ];
     return (
       <View style={styles.container}>
-        <FlatList
+        {/* <FlatList
           data={data}
           renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        /> */}
+
+        <SectionList
+          sections={[
+            {title: 'D', data: ['Devin']},
+            {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']}
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
         />
       </View>
     );
@@ -35,5 +44,14 @@ const styles = StyleSheet.create({
     padding: 10,
     fontSize: 18,
     height: 44,
-  }
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
 });
