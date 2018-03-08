1268daf6f6cdb2a21696584c5a86560d6c9d74e0
Lix <xiao.liunit@gmail.com>
2018-03-08 20:48:47 +0800

    [code style]

--- react/shadowsocks/src/components/AddProxy/ManualAddProxy.js
+++ react/shadowsocks/src/components/AddProxy/ManualAddProxy.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './manualAddProxy.css';
-import { List, InputItem } from 'antd-mobile';
+import {List, InputItem} from 'antd-mobile';
 // import { createForm } from 'rc-form';
 import NormalNavigation from '../NavBar/NormalNavigation';
 
@@ -8,51 +8,39 @@ const Item = List.Item;
 
 class ManualAddProxy extends Component {
 
-    render() {
-        return (
-            <div>
-                <NormalNavigation 
-                    title="选择线路"
-                />
+  render() {
+    return (
+      <div>
+        <NormalNavigation title="选择线路"/>
 
-                <List renderHeader={() => ''}>
+        <List renderHeader={() => ''}>
 
-                    <Item extra={<img src="/img/flag/default.png" />}>国家</Item>
+          <Item extra={< img src = "/img/flag/default.png" />}>国家</Item>
 
-                    <InputItem
-                        placeholder="代理名称"
-                    >
-                        名称
-                    </InputItem>
+          <InputItem placeholder="代理名称">
+            名称
+          </InputItem>
 
-                    <InputItem
-                        placeholder="代理服务器"
-                    >
-                        服务器
-                    </InputItem>
+          <InputItem placeholder="代理服务器">
+            服务器
+          </InputItem>
 
-                    <InputItem
-                        placeholder="代理服务器端口"
-                    >
-                        端口
-                    </InputItem>
+          <InputItem placeholder="代理服务器端口">
+            端口
+          </InputItem>
 
-                    <InputItem
-                        placeholder="aes-128-cfb"
-                    >
-                        加密
-                    </InputItem>
+          <InputItem placeholder="aes-128-cfb">
+            加密
+          </InputItem>
 
-                    <InputItem
-                        placeholder="代理密码"
-                    >
-                        密码
-                    </InputItem>
+          <InputItem placeholder="代理密码">
+            密码
+          </InputItem>
 
-                </List>
-            </div>
-        );
-    }
+        </List>
+      </div>
+    );
+  }
 }
 
 export default ManualAddProxy;
--- react/shadowsocks/src/components/App/App.js
+++ react/shadowsocks/src/components/App/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Navigation from '../NavBar/Navigation';
 import HomePage from '../Home/HomePage';
 
 class App extends Component {
   render() {
-      return (
-          <div className="App">
-              <Navigation />
-              <HomePage />
-          </div>
+    return (
+      <div className="App">
+        <Navigation/>
+        <HomePage/>
+      </div>
     );
   }
 }
