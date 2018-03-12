37cbe9b858400428c80a918f9548abbb22af2eb5
Lix <xiao.liunit@gmail.com>
2018-03-12 09:45:16 +0800

    [create CalendarManager]

---
+++ react-native/AwesomeProject/ios/AwesomeProject/CalendarManager.h
@@ -0,0 +1,13 @@
+//
+//  CalendarManager.h
+//  AwesomeProject
+//
+//  Created by Lix on 2018/3/12.
+//  Copyright © 2018年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalendarManager : NSObject
+
+@end
---
+++ react-native/AwesomeProject/ios/AwesomeProject/CalendarManager.m
@@ -0,0 +1,13 @@
+//
+//  CalendarManager.m
+//  AwesomeProject
+//
+//  Created by Lix on 2018/3/12.
+//  Copyright © 2018年 Facebook. All rights reserved.
+//
+
+#import "CalendarManager.h"
+
+@implementation CalendarManager
+
+@end
