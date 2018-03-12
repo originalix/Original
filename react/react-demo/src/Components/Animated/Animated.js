3d0421e04c2795e31614d1d6bee2536c90a8ca8b
Lix <xiao.liunit@gmail.com>
2018-03-13 07:08:39 +0800

    [test css animated]

---
+++ react/react-demo/src/Components/Animated/Animated.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './animated.css';
+
+class Animated extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default Animated;
--- react/react-demo/src/index.js
+++ react/react-demo/src/index.js
@@ -1,43 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './test.css';
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Animated from './Components/Animated/Animated';
 
-import { version } from 'antd'
-// import Button from 'antd/lib/button'
-import { WingBlank, WhiteSpace, Button, DatePicker, NavBar, Icon } from 'antd-mobile';
-import 'antd/dist/antd.css'
 
 {/* antd test */}
 
 ReactDOM.render(
-    <div>
-        <NavBar
-            mode="light"
-            icon={<Icon type="left" />}
-            onLeftClick={() => console.log('onLeftClick')}
-            rightContent={[
-                <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
-                <Icon key="1" type="ellipsis" />,
-            ]}
-        >NavBar</NavBar>
-        <h1>Test Sass</h1>
-        <p style={{ marginBottom: 24 }}>
-            Current antd version: {version} <br/>
-            You can change antd version.
-        </p>
-        {/* <Button type="primary">Button</Button> */}
-        {/* <Button type="primary" size="small" inline>small</Button> */}
-        <WingBlank>
-            <Button>default</Button><WhiteSpace />
-            <Button type="warning" disabled>warning disabled</Button><WhiteSpace />
-            <Button loading>loading button</Button><WhiteSpace />
-        </WingBlank>
-    </div>,
+    <Animated/>,
     document.getElementById('root')
 );
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
