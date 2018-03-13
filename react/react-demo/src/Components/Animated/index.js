c3fd036ea64bd45b8a25a21fc07cb72954299977
Lix <xiao.liunit@gmail.com>
2018-03-14 07:55:39 +0800

    [bounce animated]

--- react/react-demo/src/Components/Animated/index.js
+++ react/react-demo/src/Components/Animated/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import './source/animate.css';
 
 class AnimatedExample extends Component {
   render() {
     return (
-      <div>
+      <div className="animated bounce">
         <h1>Hello CSS3</h1>
       </div>
     );
