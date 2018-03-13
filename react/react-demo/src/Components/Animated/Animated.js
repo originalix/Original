35c4b496ea42d2b6c0dcceb2d8d8d6638d1720e8
Lix <xiao.liunit@gmail.com>
2018-03-13 16:36:13 +0800

    [no message]

--- react/react-demo/src/Components/Animated/Animated.js
+++ react/react-demo/src/Components/Animated/Animated.js
@@ -13,6 +13,7 @@ class Animated extends Component {
           <h1>Hello CSS</h1>
         </div>
         <div className="rotate"></div>
+        <div className="step-wrap"><h1>Typing animation by Lea Verou.</h1></div>
       </div>
     );
   }
