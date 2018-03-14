d4738e1a8eef9a3b30b9c8ce665be675f6bf7726
Lix <xiao.liunit@gmail.com>
2018-03-14 13:34:21 +0800

    [finish jello animation]

--- react/react-demo/src/Components/Animated/index.js
+++ react/react-demo/src/Components/Animated/index.js
@@ -4,7 +4,7 @@ import './source/animate.css';
 class AnimatedExample extends Component {
   render() {
     return (
-      <div className="animated wobble">
+      <div className="animated jello">
         <h1>Hello CSS3</h1>
       </div>
     );
