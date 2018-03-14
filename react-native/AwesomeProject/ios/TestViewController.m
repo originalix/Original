8a23968a8ca531f7532b1b98e3a837bc23d52cea
Lix <xiao.liunit@gmail.com>
2018-03-15 07:31:28 +0800

    [native 跳转 rn]

--- react-native/AwesomeProject/ios/TestViewController.m
+++ react-native/AwesomeProject/ios/TestViewController.m
@@ -8,6 +8,8 @@
 
 #import "TestViewController.h"
 #import "RNViewController.h"
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
 
 @interface TestViewController ()
 
@@ -25,7 +27,10 @@
     self.navigationItem.title = @"原生测试页面";
     self.view.backgroundColor = [UIColor redColor];
     UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-  
+    [button addTarget:self action:@selector(buttonClick:) forControlEvents:UIControlEventTouchUpInside];
+    button.backgroundColor = [UIColor yellowColor];
+    button.frame = CGRectMake(100, 100, 100, 50);
+    [self.view addSubview:button];
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
@@ -38,8 +43,21 @@
 }
 
 - (void)buttonClick:(UIButton *)sender {
-    RNViewController *vc = [[RNViewController alloc] init];
-    [self.navigationController pushViewController:vc animated:true];
+//    RNViewController *vc = [[RNViewController alloc] init];
+//    [self.navigationController pushViewController:vc animated:true];
+  
+  NSURL *jsCodeLocation;
+  
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                      moduleName:@"AwesomeProject"
+                                               initialProperties:nil
+                                                   launchOptions:nil];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  UIViewController *vc = [UIViewController new];
+  vc.view = rootView;
+  [self.navigationController pushViewController:vc animated:true];
 }
 
 @end
