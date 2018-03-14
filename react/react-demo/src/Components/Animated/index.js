b9c5c464554d6c11ab0e1a3f6231f76b109cd602
Lix <xiao.liunit@gmail.com>
2018-03-14 10:10:51 +0800

    [finish rubberband animated]

--- react/react-demo/src/Components/Animated/index.js
+++ react/react-demo/src/Components/Animated/index.js
@@ -4,7 +4,7 @@ import './source/animate.css';
 class AnimatedExample extends Component {
   render() {
     return (
-      <div className="animated pulse">
+      <div className="animated rubberBand">
         <h1>Hello CSS3</h1>
       </div>
     );
