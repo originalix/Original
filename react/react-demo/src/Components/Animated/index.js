508830fdfecddd3b8a000d7d098762475b9e749e
Lix <xiao.liunit@gmail.com>
2018-03-14 10:30:01 +0800

    [shake css animation]

--- react/react-demo/src/Components/Animated/index.js
+++ react/react-demo/src/Components/Animated/index.js
@@ -4,7 +4,7 @@ import './source/animate.css';
 class AnimatedExample extends Component {
   render() {
     return (
-      <div className="animated rubberBand">
+      <div className="animated shake">
         <h1>Hello CSS3</h1>
       </div>
     );
