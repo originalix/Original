78f2c35fd0e8b0d88df635fcf0dfe06814a4138c
Lix <xiao.liunit@gmail.com>
2018-03-16 10:21:48 +0800

    [create pic scroll view]

---
+++ react-native/NativeProject/ios/NativeProject/Classes/Components/Controllers/PicScrollController.h
@@ -0,0 +1,13 @@
+//
+//  PicScrollController.h
+//  NativeProject
+//
+//  Created by Lix on 2018/3/16.
+//  Copyright © 2018年 Originalee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PicScrollController : UIViewController
+
+@end
---
+++ react-native/NativeProject/ios/NativeProject/Classes/Components/Controllers/PicScrollController.m
@@ -0,0 +1,25 @@
+//
+//  PicScrollController.m
+//  NativeProject
+//
+//  Created by Lix on 2018/3/16.
+//  Copyright © 2018年 Originalee. All rights reserved.
+//
+
+#import "PicScrollController.h"
+
+@interface PicScrollController ()
+
+@end
+
+@implementation PicScrollController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
