70ba84152da07a6d7420f76d1e5e8f0711b8e94f
Lix <xiao.liunit@gmail.com>
2018-03-18 10:31:17 +0800

    [touches began]

--- react-native/NativeProject/ios/NativeProject/Classes/Components/Views/LixTouchView.m
+++ react-native/NativeProject/ios/NativeProject/Classes/Components/Views/LixTouchView.m
@@ -11,7 +11,15 @@
 @implementation LixTouchView
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    // 获取当前触摸点的UITouch对象
     UITouch *touch = [touches anyObject];
+    // 触摸点 所在View
+    NSLog(@"%@", touch.view);
+    // 触摸次数
+    NSLog(@"%ld", touch.tapCount);
+    // 获取当前触摸位置
+    CGPoint touchPosition = [touch locationInView:touch.view];
+    NSLog(@"%@", NSStringFromCGPoint(touchPosition));
 }
 
 @end
