b7117b43219bc627294cbb931a19ddf2e5af38dd
Lix <xiao.liunit@gmail.com>
2018-03-19 10:51:59 +0800

    [no message]

--- react-native/NativeProject/ios/NativeProject/Classes/Components/Controllers/PicScrollController.m
+++ react-native/NativeProject/ios/NativeProject/Classes/Components/Controllers/PicScrollController.m
@@ -7,12 +7,11 @@
 //
 
 #import "PicScrollController.h"
-#import "LixTouchView.h"
+#import "LixTouchImgView.h"
 
 @interface PicScrollController ()
 
 @property (nonatomic, strong) UIScrollView *scrollView;
-@property (nonatomic, strong) LixTouchView *touchView;
 
 @end
 
@@ -21,25 +20,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
-//    [self setupViews];
-//    NSArray *imgArr = @[@"1.PNG", @"2.PNG", @"3.PNG"];
-//    NSInteger index = 0;
-//    CGFloat screenWidth = [[UIScreen mainScreen] bounds].size.width;
-//    CGFloat screenHeight = [[UIScreen mainScreen] bounds].size.height;
-//    for (NSString *imgName in imgArr) {
-//        CGFloat y = index * screenHeight;
-//        UIImage *img = [UIImage imageNamed:imgName];
-//        UIImageView *imgView = [[UIImageView alloc] initWithImage:img];
-//        imgView.frame = CGRectMake(0, y, screenWidth, screenHeight);
-//        self.scrollView.contentSize = CGSizeMake(screenWidth, y + screenHeight);
-//        [self.scrollView addSubview:imgView];
-//        index += 1;
-//    }
-//    UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapAction:)];
-//    [self.scrollView addGestureRecognizer:tapRecognizer];
-    self.touchView = [[LixTouchView alloc] initWithFrame:CGRectMake(100, 100, 100, 50)];
-    self.touchView.backgroundColor = [UIColor redColor];
-    [self.view addSubview:self.touchView];
+    [self setupViews];
+    NSArray *imgArr = @[@"1.PNG", @"2.PNG", @"3.PNG"];
+    NSInteger index = 0;
+    CGFloat screenWidth = [[UIScreen mainScreen] bounds].size.width;
+    CGFloat screenHeight = [[UIScreen mainScreen] bounds].size.height;
+    for (NSString *imgName in imgArr) {
+        CGFloat y = index * screenHeight;
+        UIImage *img = [UIImage imageNamed:imgName];
+        LixTouchImgView *imgView = [[LixTouchImgView alloc] initWithImage:img];
+        imgView.frame = CGRectMake(0, y, screenWidth, screenHeight);
+        imgView.userInteractionEnabled = true;
+        self.scrollView.contentSize = CGSizeMake(screenWidth, y + screenHeight);
+        [self.scrollView addSubview:imgView];
+        index += 1;
+    }
+    UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapAction:)];
+    [self.scrollView addGestureRecognizer:tapRecognizer];
+    
 }
 
 - (void)tapAction:(UITapGestureRecognizer *)sender {
--- react-native/NativeProject/ios/NativeProject/Classes/Components/Views/LixTouchImgView.m
+++ react-native/NativeProject/ios/NativeProject/Classes/Components/Views/LixTouchImgView.m
@@ -10,4 +10,30 @@
 
 @implementation LixTouchImgView
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    // 获取当前触摸点的UITouch对象
+    UITouch *touch = [touches anyObject];
+    // 触摸点 所在View
+    NSLog(@"%@", touch.view);
+    // 触摸次数
+    NSLog(@"%ld", touch.tapCount);
+    // 获取当前触摸位置
+    CGPoint touchPosition = [touch locationInView:touch.view];
+    NSLog(@"%@", NSStringFromCGPoint(touchPosition));
+}
+
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    CGPoint current = [touch locationInView:touch.view];
+    CGPoint previous = [touch previousLocationInView:touch.view];
+    NSLog(@"触摸前的点: %@ 当前的触摸点: %@", NSStringFromCGPoint(previous),    NSStringFromCGPoint(current));
+    
+    // 改变位置
+    CGPoint center = self.center;
+    //    center.x += current.x - previous.x;
+    center.y += current.y - previous.y;
+    
+    self.center = center;
+}
+
 @end
