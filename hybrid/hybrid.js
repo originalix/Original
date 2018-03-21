94f88565d961732c27509e324178ec0a959224dc
Lix <xiao.liunit@gmail.com>
2018-03-21 08:57:18 +0800

    [write native share]

--- hybrid/hybrid.js
+++ hybrid/hybrid.js
@@ -30,25 +30,25 @@
   var _share = function (cmd, data, callbacks) {
     callbacks = callbacks || {};
     
-    var progress = function (resp) {
-      switch (true) {
-        // 用户取消
-        case /\:cancel$/i.test(resp.err_msg) :
-          callbacks.cancel && callbacks.cancel(resp);
-        break;
-        // 发送成功
-        case /\:(confirm|ok)$/i.test(resp.err_msg):
-          callbacks.confirm && callbacks.confirm(resp);
-        break;
-        // fail　发送失败
-        case /\:fail$/i.test(resp.err_msg) :
-        default:
-          callbacks.fail && callbacks.fail(resp);
-        break;
-      }
-      // 无论成功失败都会执行的回调
-      callbacks.all && callbacks.all(resp);
-    };
+    // var progress = function (resp) {
+    //   switch (true) {
+    //     // 用户取消
+    //     case /\:cancel$/i.test(resp.err_msg) :
+    //       callbacks.cancel && callbacks.cancel(resp);
+    //     break;
+    //     // 发送成功
+    //     case /\:(confirm|ok)$/i.test(resp.err_msg):
+    //       callbacks.confirm && callbacks.confirm(resp);
+    //     break;
+    //     // fail　发送失败
+    //     case /\:fail$/i.test(resp.err_msg) :
+    //     default:
+    //       callbacks.fail && callbacks.fail(resp);
+    //     break;
+    //   }
+    //   // 无论成功失败都会执行的回调
+    //   callbacks.all && callbacks.all(resp);
+    // };
 
     var handler = function (theData, argv) {
       
@@ -66,10 +66,12 @@
           // 分享到微博
         }
       } else {
-        console.log('执行分享');
-        JSBridge.call('share.' + cmd.action, theData, function (v) {
-          alert(v);
-        });
+        // console.log('执行分享');
+        // JSBridge.call('share.' + cmd.action, theData, function (v) {
+        //   alert(v);
+        // });
+
+        _nativeShare(cmd.action, theData, callbacks);
       }
     };
 
@@ -77,7 +79,19 @@
   }
 
   var _nativeShare = function (action, data, callbacks) {
-    
+    JSBridge.call('share.' + action, data, function (res) {
+      switch (res.msg) {
+        case "confirm":
+          callbacks.confirm;
+          break;
+        case "cancel":
+          callbacks.cancel;
+        case "fail":
+        default:
+          callbacks.fail;
+      }
+      callbacks.all;
+    });
   }
 
   HYBridApi.shareToTimeline = function (data, callbacks) {
@@ -85,6 +99,7 @@
       menu: 'menu:share:timeline',
       action: 'shareTimeline'
     }, {
+      "state": 1,
       "appid": data.appId ? data.appId : '',
       "img_url": data.imgUrl,
       "link": data.link,
