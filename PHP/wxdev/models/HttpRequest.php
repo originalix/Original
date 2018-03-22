4ec88864c26e675fd401eb12da22a8c8bc175e9f
Lix <xiao.liunit@gmail.com>
2018-03-23 07:03:54 +0800

    [create HttpRequest Model]

---
+++ PHP/wxdev/models/HttpRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+class HttpRequest extends Model
+{
+    /**
+     * 发送返回格式为JSON的HTTP GET请求
+     *
+     * @param [String] $url
+     * @param [Array] $params
+     * @return JSON-Data
+     */
+    public function get($url, $params)
+    {
+        $url = $this->generateUrl($url, $params);
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $data = curl_exec($curl);
+        curl_close($curl);
+
+        return json_decode($data);
+    }
+
+    /**
+     * 根据参数数组，生成get请求的url
+     *
+     * @param [String] $url
+     * @param [Array] $params
+     * @return String
+     */
+    protected function generateUrl($url, $params)
+    {
+        foreach ($params as $key => $value) {
+            $index = array_search($key, array_keys($params));
+            if ($index == 0) {
+                $url = $url . '?' . $key . '=' . $value;
+            } else {
+                $url = $url . '&' . $key . '=' . $value;
+            }
+        }
+
+        return $url;
+    }
+}
