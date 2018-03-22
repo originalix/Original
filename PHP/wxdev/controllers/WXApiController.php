740576351716754e159c6eee48672d4d18905429
Lix <xiao.liunit@gmail.com>
2018-03-22 21:29:08 +0800

    [curl get request]

--- PHP/wxdev/controllers/WXApiController.php
+++ PHP/wxdev/controllers/WXApiController.php
@@ -14,6 +14,19 @@ class WXApiController extends BaseController
 
     public function actionGetAccessToken()
     {
-        
+        $grant_type = 'client_credential';
+        $appid = 'wx543ed3903a242eb6';
+        $secret = '972d0295533d95069c14338296e1bff7';
+
+        $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=' . $grant_type . '&appid=' . $appid . '&secret=' . $secret;
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HEADER, 1);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        $data = curl_exec($curl);
+        curl_close($curl);
+        return $data;
     }
 }
