3cd8f8ba1a9ff62db39608366efa9b8c4ab5ba65
Lix <xiao.liunit@gmail.com>
2018-03-25 21:47:50 +0800

    [no message]

--- PHP/wxdev/web/server.php
+++ PHP/wxdev/web/server.php
@@ -1,6 +1,42 @@
 <?php
 
-include __DIR__ . '/vendor/autoload.php';
+// include __DIR__ . '/vendor/autoload.php';
+
+// echo "Hello world";
+
+// use EasyWeChat\Foundation\Application;
+
+// $options = [
+//     'debug' => true,
+//     'app_id' => 'your-app-id',
+//     'secret' => 'you-secret',
+//     'token' => 'easywechat',
+
+//     'log' => [
+//         'level' => 'debug',
+//         'file' => '/tmp/easywechat.log',
+//     ],
+
+//     // ...
+// ];
+
+// $app = new Application($options);
+
+// $response = $app->server->serve();
+// $response->send();
+
+
+// comment out the following two lines when deployed to production
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+
+// $config = require __DIR__ . '/../config/web.php';
+
+// (new yii\web\Application($config))->run();
+
 
 use EasyWeChat\Foundation\Application;
 
@@ -20,5 +56,5 @@ $options = [
 
 $app = new Application($options);
 
-$response = $app->server->serve();
-$response->send();
+// $response = $app->server->serve();
+// $response->send();
