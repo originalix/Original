5a0fbc92ca6ca10fdb776462d6240adadd3478d8
Lix <xiao.liunit@gmail.com>
2018-03-25 21:49:35 +0800

    [create EasyController]

---
+++ PHP/wxdev/controllers/EasyController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use app\controllers\BaseController;
+use app\models\HttpRequest;
+
+use EasyWeChat\Foundation\Application;
+
+class EasyController extends BaseController
+{
+    public function actionResponse()
+    {
+        $options = [
+            'debug'  => true,
+            'app_id' => 'your-app-id',
+            'secret' => 'you-secret',
+            'token'  => 'easywechat',
+        
+            // 'aes_key' => null, // 可选
+        
+            'log' => [
+                'level' => 'debug',
+                'file'  => '/tmp/easywechat.log', // XXX: 绝对路径！！！！
+            ],
+        
+            //...
+        ];
+        
+        $app = new Application($options);
+        
+        $response = $app->server->serve();
+        
+        // 将响应输出
+        $response->send(); // Laravel 里请使用：return $response;
+    }
+}
