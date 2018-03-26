9e911b71dbaadba95d85742b42512ddf7ac7a305
Lix <xiao.liunit@gmail.com>
2018-03-26 11:54:37 +0800

    [set recive message]

--- PHP/wxdev/controllers/EasyController.php
+++ PHP/wxdev/controllers/EasyController.php
@@ -43,19 +43,38 @@ class EasyController extends \yii\web\Controller
         $server = Yii::$app->wechat->server;
 
         $server->setMessageHandler(function($message) {
-            return "您好！欢迎关注我!";
+            switch ($message->MsgType) {
+                case 'event':
+                    return '收到事件消息';
+                    break;
+                case 'text':
+                    return '收到文字消息';
+                    break;
+                case 'image':
+                    return '收到图片消息';
+                    break;
+                case 'voice':
+                    return '收到语音消息';
+                    break;
+                case 'video':
+                    return '收到视频消息';
+                    break;
+                case 'location':
+                    return '收到坐标消息';
+                    break;
+                case 'link':
+                    return '收到链接消息';
+                    break;
+                // ... 其它消息
+                default:
+                    return '收到其它消息';
+                    break;
+            }
         });
 
         $response = $server->serve();
-
-        // var_dump($response);
-        // $response->send();
-        Yii::$app->wechat->server->serve()->sendContent();
-        // \Yii::$app->response->data = $response->send();
-        // echo ($response->send());
-
+        $response->sendContent();
         Yii::error('=======Request end=======');
-        // return $response;
     }
 
     public function actionLog()
