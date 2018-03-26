f87c26a22bb5d5579c56e359d8d380ea18661b5b
Lix <xiao.liunit@gmail.com>
2018-03-27 07:42:56 +0800

    [admin store]

---
+++ PHP/fecshop/appadmin/config/fecshop_local_services/Store.php
@@ -0,0 +1,35 @@
+<?php
+   return [
+   'store' => [
+        'class'  => 'fecshop\services\Store',
+        'stores' => [
+            'appadmin.fecshoptest.com' => [
+                'language'         => 'zh_CN',
+                'languageName'     => '中文',
+                'localThemeDir'    => '@apphtml5/theme/terry/theme01',
+                'thirdThemeDir'    => [],
+                'currency'         => 'CNY',
+                'mobile'           => [
+                    'enable'            => true,
+                    'condition'         => ['phone', 'tablet'],
+                    'redirectDomain'    => 'apphtml5.fecshoptest.com',
+                ],
+                // 第三方账号登录配置
+                'thirdLogin' => [
+                    // facebook账号登录
+                    'thirdLogin' => [
+                        'facebook' =>[                       #fb api配置 ，fb可以一个app设置pc和手机两个域名 
+                            'facebook_app_id'     => '184963',
+                            'facebook_app_secret' => '2e097dd7139',
+                        ],
+                        "google" => [                       #谷歌api visit https://code.google.com/apis/console to generate your google api
+                            'CLIENT_ID'  	 => '38037grhccontent.com',
+                            'CLIENT_SECRET'  => 'ei8RaoCHYm0rrwO',
+                        ],
+                    ]
+                ],
+                // 'sitemapDir' => '@apphtml5/web/cn/sitemap.xml',
+            ],
+        ],
+    ],
+];
--- PHP/fecshop/appserver/config/fecshop_local_services/Store.php
+++ PHP/fecshop/appserver/config/fecshop_local_services/Store.php
@@ -51,6 +51,54 @@
                 ],
                 
             ],
+
+            // store key：域名去掉http部分，作为key，这个必须这样定义。
+            'appserver.fecshoptest.com' => [
+                'language'         => 'zh_CN',        // 语言简码需要在@common/config/fecshop_local_services/FecshopLang.php 中定义。
+                'languageName'     => '中文',    // 语言简码对应的文字名称，将会出现在语言切换列表中显示。
+                'currency'         => 'CNY', // 当前store的默认货币,这个货币简码，必须在货币配置中配置
+                
+                // 用于sitemap生成中域名。
+                'https'            => false,
+                
+                'serverLangs'   => [
+                    
+                    [
+                        'code'             => 'fr',
+                        'language'         => 'fr_FR',        // 语言简码需要在@common/config/fecshop_local_services/FecshopLang.php 中定义。
+                        'languageName'     => 'Français',
+                    ],
+                    [
+                        'code'             => 'en',
+                        'language'         => 'en_US',        // 语言简码需要在@common/config/fecshop_local_services/FecshopLang.php 中定义。
+                        'languageName'     => 'English',
+                    ],
+                    [
+                        'code'             => 'es',
+                        'language'         => 'es_ES',        // 语言简码需要在@common/config/fecshop_local_services/FecshopLang.php 中定义。
+                        'languageName'     => 'Español',
+                    ],
+                    [
+                        'code'             => 'zh',
+                        'language'         => 'zh_CN',        // 语言简码需要在@common/config/fecshop_local_services/FecshopLang.php 中定义。
+                        'languageName'     => '中文',
+                    ],
+                ],
+                
+                // 第三方账号登录配置
+                'thirdLogin' => [
+                    // facebook账号登录
+                    'facebook' =>[                       #fb api配置 ，fb可以一个app设置pc和手机两个域名 
+                        'facebook_app_id'     => '184963',
+                        'facebook_app_secret' => '2e097dd7139',
+                    ],
+                    "google" => [                       #谷歌api visit https://code.google.com/apis/console to generate your google api
+                        'CLIENT_ID'  	 => '38037grhccontent.com',
+                        'CLIENT_SECRET'  => 'ei8RaoCHYm0rrwO',
+                    ],
+                ],
+                
+            ],
             
         ],
 
