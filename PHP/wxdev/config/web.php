53a24a10295960016b2508b992f98656ce7d4a42
Lix <xiao.liunit@gmail.com>
2018-03-26 12:13:54 +0800

    [response for wx]

--- PHP/wxdev/config/web.php
+++ PHP/wxdev/config/web.php
@@ -15,6 +15,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'f1DSj_7j0cZHffvIxGJJhnfrDZkfr_Qn',
+            'enableCsrfValidation' => false,
             'csrfParam' => '_csrf-api',
             'parsers' => [
                 'application/json' => 'yii\web\JsonParser',
--- PHP/wxdev/controllers/EasyController.php
+++ PHP/wxdev/controllers/EasyController.php
@@ -10,34 +10,6 @@ class EasyController extends \yii\web\Controller
 {
     public function actionResponse()
     {
-        $options = [
-            'debug'  => true,
-            'app_id' => 'your-app-id',
-            'secret' => 'you-secret',
-            'token'  => 'easywechat',
-        
-            // 'aes_key' => null, // 可选
-        
-            'log' => [
-                'level' => 'debug',
-                'file'  => '/tmp/easywechat.log', // XXX: 绝对路径！！！！
-            ],
-        
-            //...
-        ];
-
-        return $options;
-
-        // $app = new Application($options);
-        
-        // $response = $app->server->serve();
-        
-        // // 将响应输出
-        // $response->send(); // Laravel 里请使用：return $response;
-    }
-
-    public function actionSend()
-    {
         Yii::error('=======Request start=======');
 
         $server = Yii::$app->wechat->server;
@@ -75,10 +47,5 @@ class EasyController extends \yii\web\Controller
         $response = $server->serve();
         $response->sendContent();
         Yii::error('=======Request end=======');
-    }
-
-    public function actionLog()
-    {
-        Yii::error('========error=======');
     }
 }
