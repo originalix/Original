9b158334059825fac832e933c6efd3397295cc5f
Lix <xiao.liunit@gmail.com>
2018-03-30 16:05:02 +0800

    [validate password for login]

--- PHP/wxdev/models/AdminUser/AdminUserLogin.php
+++ PHP/wxdev/models/AdminUser/AdminUserLogin.php
@@ -2,11 +2,18 @@
 
 namespace app\models\AdminUser;
 use app\models\AdminUser;
+use fecshop\services\AdminUser;
 
 class AdminUserLogin extends AdminUser
 {
     public $password;
+    public $_admin_user;
 
+    /**
+     * 登录验证规则
+     *
+     * @return Array
+     */
     public function rules()
     {
         $parent_rules = parent::rules();
@@ -17,11 +24,19 @@ class AdminUserLogin extends AdminUser
             ['username', 'string', 'min' => 3, 'max' => 12, 'message' => '用户名长度应在4-12字符之间'],
             ['username','match','pattern'=>'/^[a-zA-Z0-9_]+$/','message'=>'{attribute}只能由英文字母、数字、下划线组成'],
             ['password', 'validatePasswordFormat'],
+            ['password', 'validatePassword'],
         ];
 
         return array_merge($parent_rules, $current_rules);
     }
 
+    /**
+     * 密码验证规则
+     *
+     * @param [type] $attribute
+     * @param [type] $params
+     * @return void
+     */
     public function validatePasswordFormat($attribute, $params)
     {
         if ($this->id) {
@@ -35,6 +50,26 @@ class AdminUserLogin extends AdminUser
                 $this->addError($attribute, "密码长度必须大于等于六位");                
             }
         }
+    }
+
+    public function validatePassword()
+    {
+        if (!$this->hasErrors()) {
+            $AdminUser = $this->getAdminUser();
+            if (!$AdminUser) {
+                $this->addError('用户名', '用户名不存在');
+            } else if (!$AdminUser->validatePassword($this->password)) {
+                $this->addError('用户名或密码', '不正确');
+            }
+        }
+    }
+
+    public function getAdminUser()
+    {
+        if ($this->_admin_user === null) {
+            $this->_admin_user = AdminUser::findByUsername($this->username);
+        }
+        return $this->_admin_user;
     }
 
 }
