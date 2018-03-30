787e333eec4306a62d80e6d5d994719a5f256af0
Lix <xiao.liunit@gmail.com>
2018-03-30 10:32:15 +0800

    [新增广场控制器]

---
+++ PHP/wxdev/controllers/SquareController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use app\controllers\BaseController;
+use app\models\HttpRequest;
+
+class SquareController extends BaseController
+{
+    public function actionOrigin
+    {
+        return ['code' => 200];
+    }
+}
