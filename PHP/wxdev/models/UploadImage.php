c9267d1ec5b23981d9353cfb0da4107c611a6f2e
Lix <xiao.liunit@gmail.com>
2018-04-02 14:30:47 +0800

    [upload function]

--- PHP/wxdev/models/UploadImage.php
+++ PHP/wxdev/models/UploadImage.php
@@ -7,5 +7,20 @@ use yii\web\UploadedFile;
 
 class UploadImage extends Model
 {
-    
+    public function rules()
+    {
+        return [
+            [['image'], 'file', 'skipOnEmpty' => false, 'extensions' => 'png, jpg, jpeg, gif, bmp'],
+        ];
+    }
+
+    public function upload()
+    {
+        if ($this->validate()) {
+            $this->image->saveAs('uploads/' . $this->image->baseName . '.' . $this->image->extension);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
