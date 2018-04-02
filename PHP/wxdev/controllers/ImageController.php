28ca59bb09414cc3623337f49bd7fce4a2b4efd8
Lix <xiao.liunit@gmail.com>
2018-04-02 14:41:05 +0800

    [upload image function]

--- PHP/wxdev/controllers/ImageController.php
+++ PHP/wxdev/controllers/ImageController.php
@@ -9,5 +9,17 @@ use yii\web\UploadedFile;
 
 class ImageController extends Controller
 {
+    public function actionUpload()
+    {
+        $model = new UploadImage;
 
+        if (Yii::$app->request->isPost) {
+            $model->image = UploadedFile::getInstance($model, 'image');
+            if ($model->upload()) {
+                return ['msg' => '图片上传成功'];
+            }
+        }
+
+        return $this->render('upload', ['model' => $model]);
+    }
 }
