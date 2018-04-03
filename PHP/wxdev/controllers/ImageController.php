51f167862ec4cfeea0765155a78e4691a019c26c
Lix <xiao.liunit@gmail.com>
2018-04-03 12:11:47 +0800

    [文件大小转换函数]

--- PHP/wxdev/controllers/ImageController.php
+++ PHP/wxdev/controllers/ImageController.php
@@ -1,20 +1,20 @@
 <?php
 /*
- * @Author: Lix 
- * @Date: 2018-04-03 06:58:05 
- * @Last Modified by:   Lix 
- * @Last Modified time: 2018-04-03 06:58:05 
+ * @Author: Lix
+ * @Date: 2018-04-03 06:58:05
+ * @Last Modified by:   Lix
+ * @Last Modified time: 2018-04-03 06:58:05
  */
 
 namespace app\controllers;
 
-use Yii;
-// use yii\web\Controller;
 use app\controllers\BaseController;
+// use yii\web\Controller;
+use app\helpers\FileHelper;
 // use app\models\UploadImage;
 // use yii\web\UploadedFile;
-use app\helpers\FileHelper;
 use app\helpers\ImageCompress;
+use Yii;
 
 class ImageController extends BaseController
 {
@@ -27,12 +27,12 @@ class ImageController extends BaseController
 
     // public function actionUpload()
     // {
-        
+
     //     $model = new UploadImage();
-        
+
     //     if (Yii::$app->request->isPost) {
     //         $model->imageFile = UploadedFile::getInstanceByName('image');
-            
+
     //         if ($model->imageFile && $model->validate()) {
     //             if ($model->upload()) {
     //                 return ['msg' => '上传成功'];
@@ -62,9 +62,50 @@ class ImageController extends BaseController
     public function actionCompress()
     {
         $source = '/Users/Lix/Documents/Sites/code-repo/PHP/wxdev/web/uploads/attachments/default/201804/03/400.jpeg';
-        $dst_img = '/Users/Lix/Documents/Sites/code-repo/PHP/wxdev/web/uploads/attachments/default/201804/03/400_compress3.jpeg';
-        $percent = 0.1;
-        $image = (new ImageCompress($source, $percent))->compressImg($dst_img);
-        print_r($image);
+        // $dst_img = '/Users/Lix/Documents/Sites/code-repo/PHP/wxdev/web/uploads/attachments/default/201804/03/400_compress3.jpeg';
+        // $percent = 0.1;
+        // $image = (new ImageCompress($source, $percent))->compressImg($dst_img);
+
+        $size = filesize($source)  / 1024;
+        // $sizeArr = $this->FileSizeConvert($size);
+
+        return ['msg' => $size];
     }
+
+    public function FileSizeConvert($bytes)
+    {
+        $bytes = floatval($bytes);
+        $arBytes = array(
+            0 => array(
+                "UNIT" => "TB",
+                "VALUE" => pow(1024, 4),
+            ),
+            1 => array(
+                "UNIT" => "GB",
+                "VALUE" => pow(1024, 3),
+            ),
+            2 => array(
+                "UNIT" => "MB",
+                "VALUE" => pow(1024, 2),
+            ),
+            3 => array(
+                "UNIT" => "KB",
+                "VALUE" => 1024,
+            ),
+            4 => array(
+                "UNIT" => "B",
+                "VALUE" => 1,
+            ),
+        );
+
+        foreach ($arBytes as $arItem) {
+            if ($bytes >= $arItem["VALUE"]) {
+                $result = $bytes / $arItem["VALUE"];
+                $result = str_replace(".", ",", strval(round($result, 2))) . " " . $arItem["UNIT"];
+                break;
+            }
+        }
+        return $result;
+    }
+
 }
