a0485bbcdf57541c740c795fe97ef5bf47151c02
Lix <xiao.liunit@gmail.com>
2018-04-03 16:09:28 +0800

    [add Header]

---
+++ react/api-test/src/components/Layout/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Menu } from 'antd';
+
+class HeaderView extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default HeaderView;
