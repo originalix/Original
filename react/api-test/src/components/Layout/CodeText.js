2191e09a1abd3170b63677a8fa8043c04bd589e6
Lix <xiao.liunit@gmail.com>
2018-04-04 12:50:25 +0800

    [no message]

--- react/api-test/src/components/Layout/CodeText.js
+++ react/api-test/src/components/Layout/CodeText.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
+import jsonFormat from '../../Tools';
+import 'highlight.js/styles/github.css'
 
 class CodeText extends Component {
+
+  highlight(code, lang) {
+    const hljs = require('highlight.js');
+    if (typeof lang === 'undefined') {
+      return hljs.highlightAuto(code).value;
+    } else if (lang === 'nohighlight') {
+      return code;
+    } else {
+      // let a = document.getElementById('codeText');
+      var b = document.querySelectorAll("pre");
+      console.log(b);
+      if (b.length > 0) {
+        // alert('有了B');
+        b.each(function (i, block) {
+          hljs.highlightBlock(block);
+        })
+      }
+      // hljs.highlightBlock(code);
+      return hljs.highlight(lang, code).value;
+      // return hljs.highlightBlock(code);
+    }
+  }
+
   render() {
     const { code } = this.props;
+    // const formatCode = jsonFormat(code);
+    // console.log(formatCode);
+    const highlightCode = this.highlight(code, 'javascript');
     return (
-      <div>
+      <div id="codeText">
         <pre>
-          <code class="json">
-            {code}
+          <code dangerouslySetInnerHTML={{ __html: highlightCode}}>
+            {/* {highlightCode} */}
+
           </code>
         </pre>
       </div>
