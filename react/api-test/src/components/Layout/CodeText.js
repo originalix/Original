80ac38d9195cc8fc8f48351bd3ccaa474413e0f4
Lix <xiao.liunit@gmail.com>
2018-04-04 13:00:53 +0800

    [usage react-highlight]

--- react/api-test/src/components/Layout/CodeText.js
+++ react/api-test/src/components/Layout/CodeText.js
@@ -1,44 +1,17 @@
 import React, { Component } from 'react';
 import jsonFormat from '../../Tools';
-import 'highlight.js/styles/github.css'
+import Highlight from 'react-highlight';
+import 'highlight.js/styles/gruvbox-dark.css';
 
 class CodeText extends Component {
 
-  highlight(code, lang) {
-    const hljs = require('highlight.js');
-    if (typeof lang === 'undefined') {
-      return hljs.highlightAuto(code).value;
-    } else if (lang === 'nohighlight') {
-      return code;
-    } else {
-      // let a = document.getElementById('codeText');
-      var b = document.querySelectorAll("pre");
-      console.log(b);
-      if (b.length > 0) {
-        // alert('有了B');
-        b.each(function (i, block) {
-          hljs.highlightBlock(block);
-        })
-      }
-      // hljs.highlightBlock(code);
-      return hljs.highlight(lang, code).value;
-      // return hljs.highlightBlock(code);
-    }
-  }
-
   render() {
     const { code } = this.props;
-    // const formatCode = jsonFormat(code);
-    // console.log(formatCode);
-    const highlightCode = this.highlight(code, 'javascript');
     return (
       <div id="codeText">
-        <pre>
-          <code dangerouslySetInnerHTML={{ __html: highlightCode}}>
-            {/* {highlightCode} */}
-
-          </code>
-        </pre>
+        <Highlight className="javascript">
+          {code}
+        </Highlight>
       </div>
     );
   }
