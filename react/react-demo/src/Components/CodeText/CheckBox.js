6f1cf0fca027f152f7f4a60f23bf97a2b7d26ae8
Lix <xiao.liunit@gmail.com>
2018-04-05 07:11:46 +0800

    [create Checkbos]

---
+++ react/react-demo/src/Components/CodeText/CheckBox.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class CheckBox extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default CheckBox;
+
