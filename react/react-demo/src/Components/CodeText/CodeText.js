09dcede0e91ddd4ae85cb7f119a3d6567651ec43
Lix <xiao.liunit@gmail.com>
2018-04-08 07:05:42 +0800

    [create Code Text]

---
+++ react/react-demo/src/Components/CodeText/CodeText.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import jsonFormat from '../../Tools';
+import Highlight from 'react-highlight';
+import 'highlight.js/styles/monokai.css';
+import './CodeTest.css';
+
+class CodeText extends Component {
+
+  render() {
+    const { code } = this.props;
+    return (
+      <div id="codeText">
+        <Highlight className="javascript">
+          {jsonFormat(code)}
+        </Highlight>
+      </div>
+    );
+  }
+}
+
+export default CodeText;
