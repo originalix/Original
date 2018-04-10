6ac260e9315a10c3406809297d8fad758530d6d5
Lix <xiao.liunit@gmail.com>
2018-04-10 16:40:02 +0800

    [finish customer table]

--- PHP/lixshop/console/migrations/m180410_082918_create_customer_table.php
+++ PHP/lixshop/console/migrations/m180410_082918_create_customer_table.php
@@ -21,7 +21,19 @@ class m180410_082918_create_customer_table extends Migration
             'access_token_created_at' => $this->timestamp()
         ]);
 
-        
+        $this->createTable('{{%customer_address}}', [
+            'id' => $this->primaryKey(),
+            'customer_id' => $this->integer()->notNull()->comment('顾客id'),
+            'name' => $this->string(100)->notNull()->comment('姓名'),
+            'telephone' => $this->string(11)->notNull()->comment('电话'),
+            'province' => $this->string(100)->notNull()->comment('省份'),
+            'city' => $this->string(255)->notNull()->comment('城市'),
+            'district' => $this->string(255)->notNull()->comment('区'),
+            'street' => $this->string(255)->notNull()->comment('街道地址'),
+            'is_default' => $this->boolean()->defaultValue(false)->comment('是否为默认地址'),
+            'created_at' => $this->timestamp(),
+            'updated_at' => $this->timestamp()->defaultValue(null),
+        ]);
     }
 
     public function down()
