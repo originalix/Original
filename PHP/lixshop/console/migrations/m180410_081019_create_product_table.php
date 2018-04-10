6892c672da66aa52e0920603cdccf4d689adc0c6
Lix <xiao.liunit@gmail.com>
2018-04-10 16:11:32 +0800

    [create product table]

---
+++ PHP/lixshop/console/migrations/m180410_081019_create_product_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\db\Migration;
+use fecshop\services\Page;
+
+/**
+ * Handles the creation of table `product`.
+ */
+class m180410_081019_create_product_table extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        
+    }
+}
