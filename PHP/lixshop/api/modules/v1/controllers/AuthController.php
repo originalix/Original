539e16d4e2c067ab56ae8860998d204a96e3955a
Lix <xiao.liunit@gmail.com>
2018-04-18 17:58:28 +0800

    [no message]

--- PHP/lixshop/api/modules/v1/controllers/AuthController.php
+++ PHP/lixshop/api/modules/v1/controllers/AuthController.php
@@ -4,18 +4,26 @@ namespace api\modules\v1\controllers;
 
 use Yii;
 use api\components\BaseController;
+use api\models\customer\AddCustomer;
 
 class AuthController extends BaseController
 {
     public function actionCreate()
     {
-        $request = Yii::$app->request;
-        $openId = $request->post('openId');
-        $mobile = $request->post('mobile');
+        $addCustomer = new AddCustomer();
+        // $addCustomer->openId = $request->post('openId');
+        // $addCustomer->mobile = $request->post('mobile');
 
+        if ($addCustomer->load(Yii::$app->request->post())) {
+            return ['msg' => '认证成功'];
+            if ($customer = $addCustomer->signup()) {
+                return $customer;
+            }
+        }
 
-        return [
-            
-        ];
+        print_r($addCustomer);
+        exit();
+
+        return ['msg' => $addCustomer->getFirstErrors()];
     }
 }
