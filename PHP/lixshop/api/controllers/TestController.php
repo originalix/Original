0eae106268bf2f63e0df94d834217ee7bdcb7fc8
Lix <xiao.liunit@gmail.com>
2018-04-18 08:00:57 +0800

    [no message]

--- PHP/lixshop/api/controllers/TestController.php
+++ PHP/lixshop/api/controllers/TestController.php
@@ -1,11 +1,22 @@
 <?php
 
+namespace api\controllers;
+
 use Yii;
 
 class TestController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        return ['code' => 200];
+        throw new \yii\web\HttpException(418, '请求有毒');
+    }
+
+    public function actionView()
+    {
+        return [
+            'name' => 'lix',
+            'age' => 25,
+            'car' => 'crv',
+        ];
     }
 }
