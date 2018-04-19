dcc985c533eb927f5c42410a3a33820854080864
Lix <xiao.liunit@gmail.com>
2018-04-19 20:50:54 +0800

    [create address model controller]

---
+++ PHP/lixshop/api/modules/v1/controllers/AddressController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace api\modules\v1\controllers;
+
+use Yii;
+use api\components\BaseController;
+
+class AddressController extends BaseController
+{
+    public function actionIndex()
+    {
+
+    }
+
+    public function actionCreate()
+    {
+
+    }
+
+    public function actionView()
+    {
+
+    }
+
+    public function actionUpdate()
+    {
+
+    }
+
+    public function actionDelete()
+    {
+
+    }
+}
