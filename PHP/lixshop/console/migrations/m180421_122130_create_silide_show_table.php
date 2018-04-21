b4ee13ee7a40521e7a5e68e8d48a9763e382a0a4
Lix <xiao.liunit@gmail.com>
2018-04-21 20:25:47 +0800

    [no message]

--- PHP/lixshop/console/migrations/m180421_122130_create_silide_show_table.php
+++ PHP/lixshop/console/migrations/m180421_122130_create_silide_show_table.php
@@ -7,21 +7,21 @@ use yii\db\Migration;
  */
 class m180421_122130_create_silide_show_table extends Migration
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function safeUp()
+    public function up()
     {
-        $this->createTable('silide_show', [
+        $this->createTable('{{%slide_show}}', [
             'id' => $this->primaryKey(),
+            'title' => $this->string()->defaultValue(null)->comment('轮播图标题'),
+            'path' => $this->string(255)->defaultValue(null)->comment('存储路径'),
+            'filename' => $this->string(255)->defaultValue(null)->comment('文件名'),
+            'is_usage' => $this->boolean()->comment('是否使用'),
+            'created_at' => $this->timestamp(),
+            'updated_at' => $this->timestamp()->defaultValue(null)
         ]);
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function safeDown()
+    public function down()
     {
-        $this->dropTable('silide_show');
+        $this->dropTable('{{%slide_show}}');
     }
 }
