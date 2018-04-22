0baf570742cb20dded8c10b2e239d5f15faf368e
Lix <xiao.liunit@gmail.com>
2018-04-22 20:31:18 +0800

    [create sale promotion]

---
+++ PHP/lixshop/console/migrations/m180422_122637_create_sale_promotion_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `sale_promotion`.
+ */
+class m180422_122637_create_sale_promotion_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%sale_promotion}}', [
+            'id' => $this->primaryKey(),
+            'product_id' => $this->integer()->defaultValue(NULL)->comment('产品id'),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('sale_promotion');
+    }
+}
