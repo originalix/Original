eb960a4498a79929098591ae30392e306a4d6f11
Lix <xiao.liunit@gmail.com>
2018-04-23 13:22:13 +0800

    [profile api]

---
+++ PHP/lixshop/common/models/CouponUsage.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%sales_coupon_usage}}".
+ *
+ * @property int $id
+ * @property int $coupon_id 优惠券id
+ * @property int $customer_id 顾客用户id
+ * @property int $times_used 使用次数
+ */
+class CouponUsage extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%sales_coupon_usage}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['coupon_id', 'customer_id', 'times_used'], 'required'],
+            [['coupon_id', 'customer_id', 'times_used'], 'integer'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'coupon_id' => '优惠券id',
+            'customer_id' => '顾客用户id',
+            'times_used' => '使用次数',
+        ];
+    }
+}
