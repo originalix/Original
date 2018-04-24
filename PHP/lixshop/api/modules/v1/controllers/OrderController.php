3d743222d60c7ab65b938a1c630337b0e46aa899
Lix <xiao.liunit@gmail.com>
2018-04-24 08:09:06 +0800

    [no message]

--- PHP/lixshop/api/modules/v1/controllers/OrderController.php
+++ PHP/lixshop/api/modules/v1/controllers/OrderController.php
@@ -15,4 +15,26 @@ class OrderController extends BaseController
         $order->setAttributes(Yii::$app->request->post());
         return $order;
     }
+
+    /*
+     * items_count
+     * total_amount
+     * discount_amount
+     * real_amount
+     * coupon_code
+     * payment_method
+     * address_id
+     * order_remark
+     * txn_type
+     * 
+     * orderItem 
+     * {
+     *      [
+     *      'product_id',
+     *      'count'
+     *      'price'
+     *      'row_total'
+     *      ]
+     * }
+     */
 }
