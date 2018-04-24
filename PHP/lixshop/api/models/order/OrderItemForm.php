8746e4be10dce631248a018d5eea197db57348cc
Lix <xiao.liunit@gmail.com>
2018-04-24 21:31:30 +0800

    [create order item form]

---
+++ PHP/lixshop/api/models/order/OrderItemForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace api\models\order;
+
+use Yii;
+use yii\base\Model;
+use yii\web\HttpException;
+use api\models\order\OrderItem;
+
+class OrderItemForm extends Model
+{   
+    public function rules()
+    {
+        
+    }
+}
