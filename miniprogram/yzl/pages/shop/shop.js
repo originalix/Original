69149beba39435d94353489d0e805f464a5fbdca
Lix <xiao.liunit@gmail.com>
2018-04-28 13:59:35 +0800

    [block wxfor]

--- miniprogram/yzl/pages/shop/shop.js
+++ miniprogram/yzl/pages/shop/shop.js
@@ -3,5 +3,14 @@ Page({
     message: 'Hello world!',
     array: [1, 2, 3, 4, 5],
     view: 'MINA',
+    staffA: {firstName: 'Hulk', lastName: 'Hu'},
+    staffB: {firstName: 'Shang', lastName: 'You'},
+    staffC: {firstName: 'Gideon', lastName: 'Lin'},
+    count: 1
+  },
+  add: function (e) {
+    this.setData({
+      count: this.data.count + 1,
+    })
   }
 })
