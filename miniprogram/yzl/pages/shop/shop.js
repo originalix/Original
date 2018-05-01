c8f8ecd78133b7efcb7d83d0ef5809d63a763ebb
Lix <xiao.liunit@gmail.com>
2018-05-02 07:21:23 +0800

    [no message]

--- miniprogram/yzl/pages/shop/shop.js
+++ miniprogram/yzl/pages/shop/shop.js
@@ -1,24 +1,4 @@
 Page({
   data: {
-    message: 'Hello world!',
-    array: [1, 2, 3, 4, 5],
-    view: 'MINA',
-    staffA: {firstName: 'Hulk', lastName: 'Hu'},
-    staffB: {firstName: 'Shang', lastName: 'You'},
-    staffC: {firstName: 'Gideon', lastName: 'Lin'},
-    count: 1,
-    array1: [1, 2, 3, 4, 5, 1, 2, 3, 4]
   },
-  add: function (e) {
-    this.setData({
-      count: this.data.count + 1,
-    })
-  },
-  tapName: function(event) {
-    console.log(event)
-  },
-  bindViewTap:function(event){
-    event.currentTarget.dataset.alphaBeta === 1 // - 会转为驼峰写法
-    event.currentTarget.dataset.alphabeta === 2 // 大写会转为小写
-  }
 })
