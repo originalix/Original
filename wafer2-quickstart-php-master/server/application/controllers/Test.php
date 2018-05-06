f0e268ea6599c8448e1dd73e5ce47cdecde641ae
Lix <xiao.liunit@gmail.com>
2018-05-07 07:29:39 +0800

    [test 转发]

--- wafer2-quickstart-php-master/server/application/controllers/Test.php
+++ wafer2-quickstart-php-master/server/application/controllers/Test.php
@@ -4,9 +4,13 @@ class Test extends CI_Controller
 {
     public function index()
     {
-        return $this->json([
-            'code' => 200,
-            'message' => 'lix'
-        ]);
+        $this->load->model('HttpRequest', 'http');
+        
+        // return $this->json([
+        //     'code' => 200,
+        //     'message' => $this->http->get('http://localhost/code-repo/PHP/lixshop/api/web/test/index')
+        // ]);
+
+        return $this->json($this->http->get('http://localhost/code-repo/PHP/lixshop/api/web/test/index'));
     }
 }
