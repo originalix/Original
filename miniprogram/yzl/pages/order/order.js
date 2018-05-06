aa7ffd39fe58ef82ab99822bcc429afbdcd4f95d
Lix <xiao.liunit@gmail.com>
2018-05-06 11:30:56 +0800

    [zan tab]

--- miniprogram/yzl/pages/order/order.js
+++ miniprogram/yzl/pages/order/order.js
@@ -1,5 +1,14 @@
 Page({
   data: {
-
+    noOrder: false,
+    list: [{
+      id: '1',
+      title: 'title'
+    },
+    {
+      id: '2',
+      title: '你好'
+    }],
+    selectedId: '1',
   }
 })
