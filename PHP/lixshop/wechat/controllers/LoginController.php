33b1769d2fa8d971889130a4b146e6cabc68a630
Lix <xiao.liunit@gmail.com>
2018-05-07 20:07:29 +0800

    [login function]

--- PHP/lixshop/wechat/controllers/LoginController.php
+++ PHP/lixshop/wechat/controllers/LoginController.php
@@ -5,14 +5,18 @@ namespace wechat\controllers;
 use Yii;
 use wechat\components\BaseController;
 use QCloud_WeApp_SDK\Auth\LoginService as LoginService;
-use QCloud_WeApp_SDK as Constants;
+use QCloud_WeApp_SDK\Constants as Constants;
 
 class LoginController extends BaseController
 {
     public function actionIndex()
     {
-        return [
-            'code' => 200
-        ];
+        $result = LoginService::login();
+        
+        if ($result['loginState'] === Constants::S_AUTH) {
+            return ['code' => 0, 'data' => $result['userinfo']];
+        } else {
+            return ['code' => -1, 'error' => $result['error']];
+        }
     }
 }
