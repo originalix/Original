9f0af11b72ce240617523ba64608428d8d00e533
Lix <xiao.liunit@gmail.com>
2018-05-08 21:04:19 +0800

    [获取请求头]

--- PHP/lixshop/wechat/controllers/LoginController.php
+++ PHP/lixshop/wechat/controllers/LoginController.php
@@ -23,6 +23,32 @@ class LoginController extends BaseController
     public function actionTest()
     {
         $headers = apache_request_headers();
+        $diyHeaders = $this->getAllHeaders();
+        if (is_array($diyHeaders)) {
+            $headers = array_merge($headers, $diyHeaders);
+        }
         return ['code' => 418, 'data' => $headers];
+    }
+
+    protected function getAllHeaders()
+    {
+        $headers = array(); 
+        foreach ($_SERVER as $key => $value) { 
+            if ('HTTP_' == substr($key, 0, 5)) { 
+                $headers[str_replace('_', '-', substr($key, 5))] = $value; 
+            } 
+        }
+
+        if (isset($_SERVER['PHP_AUTH_DIGEST'])) { 
+            $header['AUTHORIZATION'] = $_SERVER['PHP_AUTH_DIGEST'];
+        } elseif (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])) { 
+            $header['AUTHORIZATION'] = base64_encode($_SERVER['PHP_AUTH_USER'] . ':' . $_SERVER['PHP_AUTH_PW']); 
+        } 
+        if (isset($_SERVER['CONTENT_LENGTH'])) { 
+            $header['CONTENT-LENGTH'] = $_SERVER['CONTENT_LENGTH']; 
+        } 
+        if (isset($_SERVER['CONTENT_TYPE'])) { 
+            $header['CONTENT-TYPE'] = $_SERVER['CONTENT_TYPE']; 
+        }
     }
 }
--- wafer2-quickstart-php-master/server/application/controllers/Login.php
+++ wafer2-quickstart-php-master/server/application/controllers/Login.php
@@ -6,7 +6,11 @@ use QCloud_WeApp_SDK\Constants as Constants;
 
 class Login extends CI_Controller {
     public function index() {
-        $headers = apache_request_headers();
+        $headers = $this->apache_request_headers();
+        $diyHeaders = $this->getAllHeaders();
+        if (is_array($diyHeaders)) {
+            $headers = array_merge($headers, $diyHeaders);
+        }
         
         $result = LoginService::login();
         
@@ -29,5 +33,48 @@ class Login extends CI_Controller {
         $this->json([
             'code' => 2150
         ]);
-    } 
+    }
+
+    protected function apache_request_headers() 
+    {
+        $arh = array();
+        $rx_http = '/\AHTTP_/';
+        foreach($_SERVER as $key => $val) {
+            if( preg_match($rx_http, $key) ) {
+            $arh_key = preg_replace($rx_http, '', $key);
+            $rx_matches = array();
+            // do some nasty string manipulations to restore the original letter case
+            // this should work in most cases
+            $rx_matches = explode('_', $arh_key);
+            if( count($rx_matches) > 0 and strlen($arh_key) > 2 ) {
+                foreach($rx_matches as $ak_key => $ak_val) $rx_matches[$ak_key] = ucfirst($ak_val);
+                $arh_key = implode('-', $rx_matches);
+            }
+            $arh[$arh_key] = $val;
+            }
+        }
+        return( $arh );
+    }
+
+    protected function getAllHeaders()
+    {
+        $headers = array(); 
+        foreach ($_SERVER as $key => $value) { 
+            if ('HTTP_' == substr($key, 0, 5)) { 
+                $headers[str_replace('_', '-', substr($key, 5))] = $value; 
+            } 
+        }
+
+        if (isset($_SERVER['PHP_AUTH_DIGEST'])) { 
+            $header['AUTHORIZATION'] = $_SERVER['PHP_AUTH_DIGEST'];
+        } elseif (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])) { 
+            $header['AUTHORIZATION'] = base64_encode($_SERVER['PHP_AUTH_USER'] . ':' . $_SERVER['PHP_AUTH_PW']); 
+        } 
+        if (isset($_SERVER['CONTENT_LENGTH'])) { 
+            $header['CONTENT-LENGTH'] = $_SERVER['CONTENT_LENGTH']; 
+        } 
+        if (isset($_SERVER['CONTENT_TYPE'])) { 
+            $header['CONTENT-TYPE'] = $_SERVER['CONTENT_TYPE']; 
+        }
+    }
 }
