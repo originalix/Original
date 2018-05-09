28eadcb93cd13c976be11c4cb7199ef92fad0228
Lix <xiao.liunit@gmail.com>
2018-05-09 08:04:47 +0800

    [no message]

--- wafer2-quickstart-php-master/server/application/config/config.php
+++ wafer2-quickstart-php-master/server/application/config/config.php
@@ -129,14 +129,14 @@ $config['subclass_prefix'] = 'MY_';
 | Or if you have your vendor/ directory located somewhere else, you
 | can opt to set a specific path as well:
 |
-|	$config['composer_autoload'] = '/path/to/vendor/autoload.php';
 |
 | For more information about Composer, please visit http://getcomposer.org/
-|
-| Note: This will NOT disable or override the CodeIgniter-specific
-|	autoloading (application/config/autoload.php)
-*/
-$config['composer_autoload'] = FALSE;
+    |
+    | Note: This will NOT disable or override the CodeIgniter-specific
+    |	autoloading (application/config/autoload.php)
+    */
+    $config['composer_autoload'] = TRUE;
+    $config['composer_autoload'] = '../../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
