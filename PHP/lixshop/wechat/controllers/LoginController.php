013c0846f19f382882ea7a20c08c31279f588fad
Lix <xiao.liunit@gmail.com>
2018-05-10 18:17:25 +0800

    [contruction mini programer request]

--- PHP/lixshop/wechat/controllers/LoginController.php
+++ PHP/lixshop/wechat/controllers/LoginController.php
@@ -10,8 +10,18 @@ class LoginController extends BaseController
 {
     public function actionIndex()
     {
+        // https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code
+        $js_code = Yii::$app->request->post('js_code');
+        $js_code = 'xxxxxxx';
         $client = new Client();
-		$res = $client->request('GET', 'http://140.143.8.19/code-repo/PHP/lixshop/api/web/test/index');
+		$res = $client->request('GET', 'https://api.weixin.qq.com/sns/jscode2session', [
+            'query' => [
+                'appid' => 'wx3321acae41983595',
+                'secret' => 'xxxxxx',
+                'js_code' => $js_code,
+                'grant_type' => 'authorization_code'
+            ],
+        ]);
 		$data = json_decode($res->getBody()->getContents());
         return ['data' => $data];
     }
