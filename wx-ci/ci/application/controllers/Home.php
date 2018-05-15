931b0054ba4ed6ab26f745258ef20ca3b3e3d12d
Lix <xiao.liunit@gmail.com>
2018-05-15 09:54:21 +0800

    [add ci config api]

--- wx-ci/ci/application/controllers/Home.php
+++ wx-ci/ci/application/controllers/Home.php
@@ -12,6 +12,19 @@ class Home extends CI_Controller
 		$data = json_decode($res->getBody()->getContents());
 		return $this->json($data);
 	}
+	
+    public function config()
+    {
+		$access_token = $this->input->get_request_header('Authorization', TRUE);
+		$client = new Client();
+		$res = $client->request('GET', 'http://140.143.8.19/code-repo/PHP/lixshop/api/web/v1/home/index', [
+			'headers' => [
+				'Authorization' => 'Bearer 5ljzJarPzVS2Uzyt4KdwxlHSVfCk5_4s'
+			]
+		]);
+		$data = json_decode($res->getBody()->getContents());
+		return $this->json($data);
+	}
 
 	public function test()
 	{
