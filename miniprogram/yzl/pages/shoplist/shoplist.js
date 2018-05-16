c9aa3d640d70f63177651692d8598fd698840514
Lix <xiao.liunit@gmail.com>
2018-05-17 07:07:29 +0800

    [test load tabs]

--- miniprogram/yzl/pages/shoplist/shoplist.js
+++ miniprogram/yzl/pages/shoplist/shoplist.js
@@ -3,15 +3,26 @@ var appInstance = getApp()
 
 Page({
   data: {
-    noOrder: false,
     tabs: [
+
+    ]
+  },
+  onLoad() {
+    setTimeout(this.getTab, 3000)
+  },
+  getTab() {
+    var tlist = [
       {title: '选项一', content: '内容一'},
-      {title: '选项二', content: '内容二'},
+      {title: '选项二', content: 'jjwwwwww'},
       {title: '选项三', content: '内容三'},
       {title: '选项四', content: '内容四'},
       {title: '选项五', content: '内容五'},
       {title: '选项六', content: '内容六'}
     ]
+    console.log(tlist);
+    this.setData({
+      tabs: tlist,
+    }, function () {})
   },
   handleTabChange(selectedId) {
     console.log(selectedId);
