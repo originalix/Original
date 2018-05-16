4d23e0adfeac2beb8e4d12f4b2b3156023194cb3
Lix <xiao.liunit@gmail.com>
2018-05-17 06:49:27 +0800

    [text color]

--- miniprogram/yzl/pages/shoplist/shoplist.js
+++ miniprogram/yzl/pages/shoplist/shoplist.js
@@ -5,8 +5,12 @@ Page({
   data: {
     noOrder: false,
     tabs: [
-      {title: '已完成', content: '内容一'},
-      {title: '未完成', content: '内容二'},
+      {title: '选项一', content: '内容一'},
+      {title: '选项二', content: '内容二'},
+      {title: '选项三', content: '内容三'},
+      {title: '选项四', content: '内容四'},
+      {title: '选项五', content: '内容五'},
+      {title: '选项六', content: '内容六'}
     ]
   },
   handleTabChange(selectedId) {
