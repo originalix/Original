67c4a7a05bce562fa39e1946f35c02dbec40e886
Lix <xiao.liunit@gmail.com>
2018-05-16 21:36:05 +0800

    [shoplist wxml]

--- miniprogram/yzl/pages/shoplist/shoplist.js
+++ miniprogram/yzl/pages/shoplist/shoplist.js
@@ -2,5 +2,17 @@ var config = require('../../config.js');
 var appInstance = getApp()
 
 Page({
-
+  data: {
+    noOrder: false,
+    tabs: [
+      {title: '已完成', content: '内容一'},
+      {title: '未完成', content: '内容二'},
+    ]
+  },
+  handleTabChange(selectedId) {
+    console.log(selectedId);
+  },
+  onClick: function(e) {
+    console.log(`ComponentId:${e.detail.componentId},you selected:${e.detail.key}`);
+  }
 })
