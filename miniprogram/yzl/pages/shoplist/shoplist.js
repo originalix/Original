1cbae0a10e1f03c6abd404ed4658bb319b72bc0e
Lix <xiao.liunit@gmail.com>
2018-05-18 07:03:02 +0800

    [change ptli]

--- miniprogram/yzl/pages/shoplist/shoplist.js
+++ miniprogram/yzl/pages/shoplist/shoplist.js
@@ -25,17 +25,15 @@ Page({
     }, function () {})
   },
   mockProduct() {
-    var pt = {
-      'id' : 0,
-      'image': 'http://140.143.8.19/code-repo/PHP/lixshop/backend/web/uploads/temp/78145dcf5ea44b0b89afc2f3392445a3.jpg',
-      'title': '衬衫哦哦哦',
-      'price': '16.00',
-      'badge': 0
-    }
     var ptli = []
     for(var i = 0; i < 40; i++) {
-      pt.id = i;
-      console.log(pt.id)
+      var pt = {
+        'id' : i,
+        'image': 'http://140.143.8.19/code-repo/PHP/lixshop/backend/web/uploads/temp/78145dcf5ea44b0b89afc2f3392445a3.jpg',
+        'title': '衬衫哦哦哦',
+        'price': '16.00',
+        'badge': 0
+      }
       ptli.push(pt)
     }
     this.setData({
