fc1946e45556314fd4fefddd8a27e9172a4c0696
Lix <xiao.liunit@gmail.com>
2018-05-19 20:44:59 +0800

    [aadd filename_bad_chars]

--- wafer2-quickstart-php-master/server/application/controllers/Security.php
+++ wafer2-quickstart-php-master/server/application/controllers/Security.php
@@ -3,5 +3,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Security extends CI_Controller
 {
-    
+    public $filename_bad_chars =	array(
+		'../', '<!--', '-->', '<', '>',
+		"'", '"', '&', '$', '#',
+		'{', '}', '[', ']', '=',
+		';', '?', '%20', '%22',
+		'%3c',		// <
+		'%253c',	// <
+		'%3e',		// >
+		'%0e',		// >
+		'%28',		// (
+		'%29',		// )
+		'%2528',	// (
+		'%26',		// &
+		'%24',		// $
+		'%3f',		// ?
+		'%3b',		// ;
+		'%3d'		// =
+	);
 }
