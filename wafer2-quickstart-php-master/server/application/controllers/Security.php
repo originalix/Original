d8472e0be77873e2f893fbf68b1f93f5cf14404a
Lix <xiao.liunit@gmail.com>
2018-05-20 20:57:00 +0800

    [add allowed-request]

--- wafer2-quickstart-php-master/server/application/controllers/Security.php
+++ wafer2-quickstart-php-master/server/application/controllers/Security.php
@@ -20,5 +20,35 @@ class Security extends CI_Controller
 		'%3f',		// ?
 		'%3b',		// ;
 		'%3d'		// =
+    );
+    
+    protected $_never_allowed_str =	array(
+		'document.cookie'	=> '[removed]',
+		'document.write'	=> '[removed]',
+		'.parentNode'		=> '[removed]',
+		'.innerHTML'		=> '[removed]',
+		'-moz-binding'		=> '[removed]',
+		'<!--'				=> '&lt;!--',
+		'-->'				=> '--&gt;',
+		'<![CDATA['			=> '&lt;![CDATA[',
+		'<comment>'			=> '&lt;comment&gt;'
 	);
+
+	/**
+	 * List of never allowed regex replacements
+	 *
+	 * @var	array
+	 */
+	protected $_never_allowed_regex = array(
+		'javascript\s*:',
+		'(document|(document\.)?window)\.(location|on\w*)',
+		'expression\s*(\(|&\#40;)', // CSS and IE
+		'vbscript\s*:', // IE, surprise!
+		'wscript\s*:', // IE
+		'jscript\s*:', // IE
+		'vbs\s*:', // IE
+		'Redirect\s+30\d',
+		"([\"'])?data\s*:[^\\1]*?base64[^\\1]*?,[^\\1]*?\\1?"
+	);
+
 }
