5308ed1356cbec91aed046608fe0197a83f35685
Lix <xiao.liunit@gmail.com>
2018-05-21 16:41:09 +0800

    [test https for wx]

--- miniprogram/yzl/app.js
+++ miniprogram/yzl/app.js
@@ -7,6 +7,19 @@ App({
     const authInfo = auth.getAuthInfo()
     console.log(authInfo)
     auth.login()
+    // 测试请求地址
+    wx.request({
+      url: 'https://api.yzl1030.com/test/index',
+      data: {
+      },
+      method: 'GET',
+      success: function(res) {
+        console.log(res)
+      },
+      fail: function(error) {
+        console.log(error)
+      }
+    });
   },
   onError: function (msg) {
     console.log(msg);
