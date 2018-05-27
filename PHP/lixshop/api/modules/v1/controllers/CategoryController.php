5c9f2b8b6caf875f050f0eba030b0523bccf51b2
Lix <xiao.liunit@gmail.com>
2018-05-27 11:29:40 +0800

    [create categoryController]

---
+++ PHP/lixshop/api/modules/v1/controllers/CategoryController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace api\modules\v1\controllers;
+
+use Yii;
+use api\components\BaseController;
+
+class CategoryController extends BaseController
+{
+    /**
+     *  根据category-id获取商品列表
+     */
+    public function actionList()
+    {
+
+    }
+}
