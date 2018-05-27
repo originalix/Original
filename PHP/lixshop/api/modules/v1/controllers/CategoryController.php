6eb3fdf69477cf489491a5cd66a7609a2bf17c7c
Lix <xiao.liunit@gmail.com>
2018-05-27 21:27:31 +0800

    [add action for search]

--- PHP/lixshop/api/modules/v1/controllers/CategoryController.php
+++ PHP/lixshop/api/modules/v1/controllers/CategoryController.php
@@ -4,14 +4,16 @@ namespace api\modules\v1\controllers;
 
 use Yii;
 use api\components\BaseController;
+use api\modules\v1\models\CategoryListSearch;
 
 class CategoryController extends BaseController
 {
     /**
      *  根据category-id获取商品列表
      */
-    public function actionList()
+    public function actionList($id)
     {
-
+        $model = new CategoryListSearch();
+        return $model->search($id);
     }
 }
--- PHP/lixshop/api/modules/v1/models/CategoryListSearch.php
+++ PHP/lixshop/api/modules/v1/models/CategoryListSearch.php
@@ -22,3 +22,4 @@ class CategoryListSearch extends ProductCategory
         return $dataProvider;
     }
 }
+
