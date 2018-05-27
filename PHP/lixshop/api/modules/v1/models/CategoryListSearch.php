e22fb98e931e5cf4e3072409983dd84f4e4a769a
Lix <xiao.liunit@gmail.com>
2018-05-27 21:23:42 +0800

    [add category list search]

---
+++ PHP/lixshop/api/modules/v1/models/CategoryListSearch.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace api\modules\v1\models;
+
+use Yii;
+use common\models\ProductCategory;
+use yii\data\ActiveDataProvider;
+
+class CategoryListSearch extends ProductCategory
+{
+    public function search($id)
+    {
+        $query = static::find()->where([
+            'category_id' => $id 
+        ]); 
+        $query->with('product');
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        return $dataProvider;
+    }
+}
