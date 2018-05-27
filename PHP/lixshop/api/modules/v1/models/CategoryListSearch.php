216482fe24e0f081b5aae6860429d2940be59d54
Lix <xiao.liunit@gmail.com>
2018-05-27 21:54:14 +0800

    [change return fields for category list]

--- PHP/lixshop/api/modules/v1/models/CategoryListSearch.php
+++ PHP/lixshop/api/modules/v1/models/CategoryListSearch.php
@@ -8,23 +8,25 @@ use yii\data\ActiveDataProvider;
 
 class CategoryListSearch extends ProductCategory
 {
+    public function fields()
+    {
+        return [
+            'product'
+        ];
+    }
+
     public function search($id)
     {
-        // $query = static::find()->where([
-            // 'category_id' => $id 
-        // ]); 
-        // $query->with('product');
+        $query = static::find()->where([
+            'category_id' => $id 
+        ]);
+        $query->with('product');
 
-        // $dataProvider = new ActiveDataProvider([
-            // 'query' => $query,
-        // ]);
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
 
-        // return $dataProvider;
-        // $a = static::find()->where([
-            // 'product_id' => $id
-        // ])->one();
-        $a = static::findOne($id);
-        return $a->product;
+        return $dataProvider;
     }
 }
 
