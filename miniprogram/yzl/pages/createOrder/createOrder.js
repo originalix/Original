855f253515264f127671f41bb602c197ad814afe
Lix <xiao.liunit@gmail.com>
2018-05-30 11:00:43 +0800

    [增加时间日期选择器]

--- miniprogram/yzl/pages/createOrder/createOrder.js
+++ miniprogram/yzl/pages/createOrder/createOrder.js
@@ -1,3 +1,18 @@
 Page({
-
+	data: {
+		time: '12:01',
+		date: '2018-05-30'
+	},
+	bindTimeChange: function(e) {
+		console.log('picker发送选择改变，携带值为', e.detail.value)
+		this.setData({
+			time: e.detail.value
+		})
+	},
+	bindDateChange: function(e) {
+  	console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
 })
