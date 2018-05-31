20da46080f42c5e364795e52fa102d0ac789c62f
Lix <xiao.liunit@gmail.com>
2018-05-31 09:40:34 +0800

    [get cart data]

--- miniprogram/yzl/pages/createOrder/createOrder.js
+++ miniprogram/yzl/pages/createOrder/createOrder.js
@@ -1,9 +1,28 @@
+var config = require('../../config.js');
+var appInstance = getApp()
+
 Page({
 	data: {
 		time: '12:01',
 		date: '2018-05-30',
 		address: {},
 		isChooseAddress: false,
+		productList: [],
+	},
+	onLoad () {
+		var that = this
+		try {
+			var cartData = wx.getStorageSync('CART_LIST_DATA')
+			if (cartData) {
+				that.setData({
+					productList: cartData
+				}, function () {
+					console.log(that.data.productList)
+				})
+			}
+		} catch (e) {
+			console.log(e)
+		}
 	},
 	bindTimeChange: function(e) {
 		console.log('picker发送选择改变，携带值为', e.detail.value)
