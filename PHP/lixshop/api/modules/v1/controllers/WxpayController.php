dd56ca311db128a81945fa35eaa7b9f3a2bfa85a
Lix <xiao.liunit@gmail.com>
2018-06-01 21:16:19 +0800

    [create WxpayController]

---
+++ PHP/lixshop/api/modules/v1/controllers/WxpayController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace api\modules\v1\controllers;
+
+use Yii;
+use api\components\BaseController;
+
+class WxpayController extends BaseController
+{
+    public function create()
+    {
+        
+    }
+}
