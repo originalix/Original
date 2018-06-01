86e2368e035127ad3e744eb132628cffe3103d83
Lix <xiao.liunit@gmail.com>
2018-06-01 20:33:01 +0800

    [添加微信构造函数]

--- PHP/lixshop/api/utils/WeEncryption.php
+++ PHP/lixshop/api/utils/WeEncryption.php
@@ -16,6 +16,12 @@ class WeEncryption
     private $trade_type = 'APP';
     private static $details;
 
+    /**
+     * 构造函数，初始化成员变量
+     * @param String $appid 小程序id
+     * @param Int $mch_id 商户号
+     * @prams String $key 秘钥
+     */
     private function __construct()
     {
         if (is_string($appid) && is_string($mch_id)) {
@@ -23,6 +29,18 @@ class WeEncryption
             $this->mch_id = Yii::$app->params['MCH_ID'];
             $this->key = Yii::$app->params['APP_SECRET'];
         }
+    }
+
+    /**
+     * 获取当前实例
+     * @return WeEncryption 实例
+     */
+    public static function getInstance()
+    {
+        if (self::$instance == null) {
+            self::$instance == new Self(); 
+        }
+        return self::$instance;
     }
 }
 
