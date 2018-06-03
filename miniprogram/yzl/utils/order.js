6405800c4cadcab0e54dda2fd1a0a4482bee78b7
Lix <xiao.liunit@gmail.com>
2018-06-03 11:41:06 +0800

    [get pay params function]

--- miniprogram/yzl/utils/order.js
+++ miniprogram/yzl/utils/order.js
@@ -1,5 +1,22 @@
-const getPayParams = () => {
+var config = require('../config.js')
 
+const getPayParams = (trade_no, total_fee) => {
+	wx.request({
+		url: config.service.createWxPayUrl,
+		data: {
+			'body': '衣之恋-干洗',
+			'out_trade_no': trade_no,
+			'total_fee': total_fee
+		},
+		method: 'POST',
+		success: function (res) {
+			console.log(res)
+		},
+		fail: function (error) {
+			console.log('error')
+			console.log(res)
+		}
+	})
 }
 
 module.exports{ getPayParams }
