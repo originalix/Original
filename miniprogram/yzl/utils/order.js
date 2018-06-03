6b4d8d6a043efa83cbd757b66c026992a4a15114
Lix <xiao.liunit@gmail.com>
2018-06-03 15:06:19 +0800

    [add comment]

--- miniprogram/yzl/utils/order.js
+++ miniprogram/yzl/utils/order.js
@@ -1,8 +1,16 @@
 var config = require('../config.js')
+var appInstance = getApp()
 
+/**
+ *  根据后台订单号，商品价格，获取创建小程序支付的参数
+ *  @param String trade_no 后台订单号
+ *  @param Int total_fee 商品价格
+ *
+ */
 const getPayParams = (trade_no, total_fee) => {
 	wx.request({
 		url: config.service.createWxPayUrl,
+		header: appInstance.requestToken,
 		data: {
 			'body': '衣之恋-干洗',
 			'out_trade_no': trade_no,
@@ -19,5 +27,5 @@ const getPayParams = (trade_no, total_fee) => {
 	})
 }
 
-module.exports{ getPayParams }
+module.exports = { getPayParams }
 
