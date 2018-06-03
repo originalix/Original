6221ba270a4841a60fd3924685cdfd9f69e3ff33
Lix <xiao.liunit@gmail.com>
2018-06-03 08:03:24 +0800

    [call back function]

--- PHP/lixshop/api/models/WxPay.php
+++ PHP/lixshop/api/models/WxPay.php
@@ -71,5 +71,41 @@ class WxPay extends Model
             echo json_encode($resignData);
         }
     }
+
+    public function callBack()
+    {
+        $obj = $this->encpt->getNotifyData();
+        if ($obj == false) {
+            exit;
+        } 
+
+        if ($obj) {
+            $data = array(
+                'appid'				=>	$obj->appid,
+                'mch_id'			=>	$obj->mch_id,
+                'nonce_str'			=>	$obj->nonce_str,
+                'result_code'		=>	$obj->result_code,
+                'openid'			=>	$obj->openid,
+                'trade_type'		=>	$obj->trade_type,
+                'bank_type'			=>	$obj->bank_type,
+                'total_fee'			=>	$obj->total_fee,
+                'cash_fee'			=>	$obj->cash_fee,
+                'transaction_id'	=>	$obj->transaction_id,
+                'out_trade_no'		=>	$obj->out_trade_no,
+                'time_end'			=>	$obj->time_end
+            );
+        }
+
+        $sign = $encpt->getSign($data);
+
+        if ($sign == $obj->sign) {
+            $reply = "<xml>
+					<return_code><![CDATA[SUCCESS]]></return_code>
+					<return_msg><![CDATA[OK]]></return_msg>
+				</xml>";
+		    echo $reply;
+		    exit;
+        }
+    }
 }
 
