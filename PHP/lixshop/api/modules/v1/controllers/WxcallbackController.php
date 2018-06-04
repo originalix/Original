124891f08eca1275a1c2f108055b72ed9fcb86ce
Lix <xiao.liunit@gmail.com>
2018-06-04 08:56:10 +0800

    [no message]

--- PHP/lixshop/api/modules/v1/controllers/WxcallbackController.php
+++ PHP/lixshop/api/modules/v1/controllers/WxcallbackController.php
@@ -20,7 +20,8 @@ class WxcallbackController extends \yii\web\Controller
     public function actionIndex()
     {
         Yii::warning('接收数据 ============= start ==============', 'order');
-        $postXML = $GLOBALS["HTTP_RAW_POST_DATA"];
+        // $postXML = $GLOBALS["HTTP_RAW_POST_DATA"];
+        $postXML = isset($GLOBALS['HTTP_RAW_POST_DATA']) ? $GLOBALS['HTTP_RAW_POST_DATA'] : file_get_contents("php://input");
         Yii::warning($postXML, 'order');
         Yii::warning('接收数据 ============= end ==============', 'order');
         Yii::warning('订单回调', 'order');
