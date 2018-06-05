6a8aabd7812d5db70618a7abbe6a74fbac24124d
Lix <xiao.liunit@gmail.com>
2018-06-05 20:58:46 +0800

    merge

--- PHP/lixshop/common/config/bootstrap.php
+++ PHP/lixshop/common/config/bootstrap.php
@@ -5,4 +5,4 @@ Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@wechat', dirname(dirname(__DIR__)) . '/wechat');
 Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
-Yii::setAlias('@baseurl', 'http://localhost/code-repo/PHP/lixshop');
+Yii::setAlias('@baseurl', 'http://140.143.8.19/code-repo/PHP/lixshop');
