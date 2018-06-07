81cbcddcaec70611e45207ddf2c44c6f80144756
Lix <xiao.liunit@gmail.com>
2018-06-07 20:38:41 +0800

    [test steps components]

--- miniprogram/yzl/pages/orderDetail/orderDetail.js
+++ miniprogram/yzl/pages/orderDetail/orderDetail.js
@@ -5,7 +5,31 @@ var appInstance = getApp()
 
 Page({
 	data: {
-
+		steps: [
+			{
+				// 此步骤是否当前完成状态
+				current: false,
+				// 此步骤是否已经完成
+				done: true,
+				// 此步骤显示文案
+				text: '步骤一',
+			},
+			{
+				done: true,
+				current: true,
+				text: '步骤二',
+			},
+			{
+				done: false,
+				current: false,
+				text: '步骤三',
+			}
+		]
+	},
+	onLoad () {
+		wx.setNavigationBarTitle({
+      title: '待付款的订单' 
+		})
 	}
 })
 
