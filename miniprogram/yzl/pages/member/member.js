43b5778984e7c5870b1d8d7f8608084e91f1efbc
Lix <xiao.liunit@gmail.com>
2018-06-08 15:50:45 +0800

    [更新会员中心 css]

--- miniprogram/yzl/pages/member/member.js
+++ miniprogram/yzl/pages/member/member.js
@@ -1,18 +1,22 @@
 Page({
   data:{
-    list: [{
-      title: '常用地址',
-      slot: false,
-      src: '/resource/img/location.png'
-    }, {
-      title: '推荐有奖',
-      slot: false,
-      src: '/resource/img/love.png'
-    }, {
-      title: '积分商城',
-      slot: false,
-      src: '/resource/img/gift.png'
-    }]
+    list: [
+			// {
+				// title: '常用地址',
+				// slot: false,
+				// src: '/resource/img/location.png'
+			// },
+			{
+				title: '推荐有奖',
+				slot: false,
+				src: '/resource/img/love.png'
+			},
+			{
+				title: '积分商城',
+				slot: false,
+				src: '/resource/img/gift.png'
+			}
+		]
   },
   methods: { }
 })
