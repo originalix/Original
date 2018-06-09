722280cb93d9c47933615e11fc32ddb044cc60e0
Lix <xiao.liunit@gmail.com>
2018-06-09 17:54:39 +0800

    [mock list data]

--- miniprogram/yzl/pages/charge/charge.js
+++ miniprogram/yzl/pages/charge/charge.js
@@ -5,10 +5,39 @@ var appInstance = getApp()
 
 Page({
 	data: {
-
+		list: [],
+		currentIdx: 9999
 	},
 	onLoad () {
 
 	},
+	getChargeList () {
+		let list = [
+			{
+				value: 2000,
+				gift: 500
+			},
+			{
+				value: 500,
+				gift: 80
+			},
+			{
+				value: 1000,
+				gift: 230
+			},
+			{
+				value: 300,
+				gift: 40
+			},
+			{
+				value: 5000,
+				gift: 1400
+			},
+			{
+				value: 10000,
+				gift: 3000
+			},
+		]
+	}
 })
 
