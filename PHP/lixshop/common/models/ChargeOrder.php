a88da38b47fbab216c76db7dc34a70412b20fc1e
Lix <xiao.liunit@gmail.com>
2018-06-10 19:57:30 +0800

    [no message]

--- PHP/lixshop/common/models/ChargeOrder.php
+++ PHP/lixshop/common/models/ChargeOrder.php
@@ -3,6 +3,7 @@
 namespace common\models;
 
 use Yii;
+use yii\behaviors\TimestampBehavior;
 
 /**
  * This is the model class for table "{{%charge_order}}".
@@ -23,6 +24,21 @@ use Yii;
  */
 class ChargeOrder extends \yii\db\ActiveRecord
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {        
+        return [
+            [
+                'class' => TimestampBehavior::className(),
+                'createdAtAttribute' => 'created_at',// 自己根据数据库字段修改
+                'updatedAtAttribute' => 'updated_at', // 自己根据数据库字段修改
+                'value' => date('Y-m-d H:i:s'), // 自己根据数据库字段修改
+            ],
+        ];
+    }
+
     /**
      * @inheritdoc
      */
