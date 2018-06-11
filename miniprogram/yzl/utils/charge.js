46731e06a572c5c12f417dc13e38f2215bf611b3
Lix <xiao.liunit@gmail.com>
2018-06-11 08:31:40 +0800

    [create charge order api]

--- miniprogram/yzl/utils/charge.js
+++ miniprogram/yzl/utils/charge.js
@@ -27,6 +27,32 @@ const getChargeList = (data) => {
 	})
 }
 
+const createChargeOrder = (data) => {
+	wx.request({
+		url:config.service.createChargeOrderUrl,
+		header: appInstance.requestToken,
+		data: data.params,
+		method: 'POST',
+		success: function (res) {
+			console.log(res)
+			const code = res.data.code
+			if (data.success !== undefined && typeof(data.success) === 'function') {
+				data.success(res.data.data)
+			} else {
+				if (data.fail !== undefined && typeof(data.fail) === 'function') {
+					data.fail(res.data.msg)
+				}
+			}
+		},
+		fail: function (error) {
+			if (data.fail !== undefined && typeof(data.fail) === 'function') {
+				data.fail('发起支付失败，请重试')
+			}
+			console.log(error)
+		}
+	})
+}
+
 module.exports = {
 	getChargeList
 }
