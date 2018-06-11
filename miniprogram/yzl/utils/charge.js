9ba3c168ed637296e5a7b824c7d1ac29ce0c789a
Lix <xiao.liunit@gmail.com>
2018-06-11 08:27:58 +0800

    [get charge list]

---
+++ miniprogram/yzl/utils/charge.js
@@ -0,0 +1,33 @@
+var config = require('../config.js')
+var appInstance = getApp()
+
+const getChargeList = (data) => {
+	wx.request({
+		url: config.service.getChargeListUrl,
+		header: appInstance.requestToken,
+		data: {},
+		method: 'GET',
+		success: function (res) {
+			console.log(res)
+			const code = res.data.code
+			if (data.success !== undefined && typeof(data.success) === 'function') {
+				data.success(res.data.data)
+			} else {
+				if (data.fail !== undefined && typeof(data.fail) === 'function') {
+					data.fail(res.data.msg)
+				}
+			}
+		},
+		fail: function (error) {
+			if (data.fail !== undefined && typeof(data.fail) === 'function') {
+				data.fail('发起支付失败，请重试')
+			}
+			console.log(error)
+		}
+	})
+}
+
+module.exports = {
+	getChargeList
+}
+
