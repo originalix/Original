a7d8598c01db2f2b0244af7eda04e7f563aaf8d0
Lix <xiao.liunit@gmail.com>
2018-06-11 08:33:38 +0800

    [get user me api]

--- miniprogram/yzl/utils/charge.js
+++ miniprogram/yzl/utils/charge.js
@@ -20,7 +20,7 @@ const getChargeList = (data) => {
 		},
 		fail: function (error) {
 			if (data.fail !== undefined && typeof(data.fail) === 'function') {
-				data.fail('发起支付失败，请重试')
+				data.fail('获取充值列表失败，请重试')
 			}
 			console.log(error)
 		}
@@ -46,7 +46,33 @@ const createChargeOrder = (data) => {
 		},
 		fail: function (error) {
 			if (data.fail !== undefined && typeof(data.fail) === 'function') {
-				data.fail('发起支付失败，请重试')
+				data.fail('创建充值订单失败 请重试')
+			}
+			console.log(error)
+		}
+	})
+}
+
+const getUserMe = (data) => {
+	wx.request({
+		url: config.service.getUserMe,
+		header: appInstance.requestToken,
+		data: {},
+		method: 'GET',
+		success: function (res) {
+			console.log(res)
+			const code = res.data.code
+			if (data.success !== undefined && typeof(data.success) === 'function') {
+				data.success(res.data.data)
+			} else {
+				if (data.fail !== undefined && typeof(data.fail) === 'function') {
+					data.fail(res.data.msg)
+				}
+			}
+		},
+		fail: function (error) {
+			if (data.fail !== undefined && typeof(data.fail) === 'function') {
+				data.fail('创建充值订单失败 请重试')
 			}
 			console.log(error)
 		}
