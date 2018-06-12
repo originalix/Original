ddbeceb973b41d19466042524dcb0a75d591e354
Lix <xiao.liunit@gmail.com>
2018-06-12 16:56:53 +0800

    [create balance controller]

---
+++ PHP/lixshop/api/modules/v1/controllers/BalanceController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace api\modules\v1\controllers;
+
+use Yii;
+use api\components\BaseController;
+use common\models\BalanceLog;
+
+class BalanceController extends BaseController
+{
+    public function actionLog()
+    {
+
+    }
+}
+
