b8b219275567c7bced7e599d2df21866b51be125
Lix <xiao.liunit@gmail.com>
2018-06-12 17:00:33 +0800

    [finish balance log api]

--- PHP/lixshop/api/modules/v1/controllers/BalanceController.php
+++ PHP/lixshop/api/modules/v1/controllers/BalanceController.php
@@ -5,12 +5,18 @@ namespace api\modules\v1\controllers;
 use Yii;
 use api\components\BaseController;
 use common\models\BalanceLog;
+use yii\data\ActiveDataProvider;
 
 class BalanceController extends BaseController
 {
     public function actionLog()
     {
-
+        $query = BalanceLog::find()
+           ->where(['customer_id' => Yii::$app->user->identity->id]); 
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+        return $dataProvider;
     }
 }
 
