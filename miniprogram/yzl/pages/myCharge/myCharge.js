88aea5624e25bd8daf8886d1755484a11282e438
Lix <xiao.liunit@gmail.com>
2018-06-12 15:50:33 +0800

    [set list]

--- miniprogram/yzl/pages/myCharge/myCharge.js
+++ miniprogram/yzl/pages/myCharge/myCharge.js
@@ -2,6 +2,26 @@ var config = require('../../config.js');
 var appInstance = getApp()
 
 Page({
-	data: {}
+	data: {
+		listData: [],
+	},
+	onLoad() {
+		this.setListData()
+	},
+	setListData () {
+		var list = [];
+		for (var i=0; i<100; i++) {
+			let data = {
+				'title': '充值',
+				'type': 1,
+				'value': 200
+			}	
+			list.push(data)
+		}
+
+		this.setData({
+			listData: list
+		})
+	}
 })
 
