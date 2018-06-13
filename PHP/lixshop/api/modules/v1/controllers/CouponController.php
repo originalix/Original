286dff979567b708fb2541d9589224a3ca72353d
Lix <xiao.liunit@gmail.com>
2018-06-13 19:20:23 +0800

    [领券 控制器方法]

--- PHP/lixshop/api/modules/v1/controllers/CouponController.php
+++ PHP/lixshop/api/modules/v1/controllers/CouponController.php
@@ -4,6 +4,8 @@ namespace api\modules\v1\controllers;
 
 use Yii;
 use api\components\BaseController;
+use api\models\coupon\CouponLogic;
+use yii\web\HttpException;
 
 class CouponController extends BaseController
 {
@@ -14,7 +16,13 @@ class CouponController extends BaseController
 
     public function actionExchange()
     {
+        $code = Yii::$app->request->post('code');
+        if (is_null($code)) {
+            throw new HttpException(417, '请输入优惠券码');
+        }
 
+        $model = new CouponLogic();
+        return $model->exchange($code);
     }
 }
 
