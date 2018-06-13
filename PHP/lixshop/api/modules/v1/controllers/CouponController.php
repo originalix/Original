83d49642bb8980948566f1ddfd538ee9f5542133
Lix <xiao.liunit@gmail.com>
2018-06-13 18:15:36 +0800

    [create Coupon controller]

---
+++ PHP/lixshop/api/modules/v1/controllers/CouponController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace api\modules\v1\controllers;
+
+use Yii;
+use api\components\BaseController;
+
+class CouponController extends BaseController
+{
+    public function actionIndex()
+    {
+
+    }
+}
+
