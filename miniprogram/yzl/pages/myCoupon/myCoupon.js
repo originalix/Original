32dd42ebbb74719c18d7af28ae3ea14d556ca308
Lix <xiao.liunit@gmail.com>
2018-06-13 08:46:51 +0800

    [layout coupon]

--- miniprogram/yzl/pages/myCoupon/myCoupon.js
+++ miniprogram/yzl/pages/myCoupon/myCoupon.js
@@ -3,6 +3,15 @@ var appInstance = getApp()
 var chargeUtils = require('../../utils/charge.js');
 
 Page({
-	data: {}
+	data: {
+		tabs: [
+			{title: '未使用', content: 1},
+			{title: '已使用', content: 2},
+			{title: '已过期', content: 3}
+		],
+		noOrder: true,
+		loadmore: false,
+		loadText: '正在努力加载...'
+	}
 })
 
