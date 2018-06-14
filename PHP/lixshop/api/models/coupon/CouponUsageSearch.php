567d0795153a166eea698e3d1ae7c6724d23c955
Lix <xiao.liunit@gmail.com>
2018-06-14 23:01:45 +0800

    [no message]

--- PHP/lixshop/api/models/coupon/CouponUsageSearch.php
+++ PHP/lixshop/api/models/coupon/CouponUsageSearch.php
@@ -6,6 +6,8 @@ use Yii;
 use api\models\coupon\CouponUsage as CommonCouponUsage;
 use yii\web\HttpException;
 use yii\data\ActiveDataProvider;
+// use api\models\coupon\Coupon;
+use common\models\Coupon;
 
 class CouponUsageSearch extends CommonCouponUsage
 {
@@ -17,16 +19,19 @@ class CouponUsageSearch extends CommonCouponUsage
 
     public function search()
     {
-        $query = static::find()
-            ->where([
-                'customer_id' => Yii::$app->user->identity->id,
-                'is_used' => 1
-            ])->joinWith([
-                'coupon' => function ($query) {
-                    $query->andWhere('times_used = 0');
-                },
-            ]);
+        // $query = static::find()
+        //     ->where([
+        //         'customer_id' => Yii::$app->user->identity->id,
+        //         'is_used' => 1
+        //     ])->joinWith([
+        //         'coupon' => function ($query) {
+        //             $query->AndWhere('DATEDIFF(DAY, expiration_date, getdate()) <= 15');
+        //         },
+        //     ]);
+        $query = Coupon::find()
+            ->where('DATEDIFF(expiration_date, NOW()) > 1');
         
+        //DATEDIFF(DAY,dateadd(m,AgentDate,UpdateTime),getdate())<=15
         // start
         // $query = static::find()
             // ->where([
