8d9fe8e992cbcb971b27850cf557ed7315148f7d
Lix <xiao.liunit@gmail.com>
2018-06-14 21:30:04 +0800

    [search]

--- PHP/lixshop/api/models/coupon/CouponUsageSearch.php
+++ PHP/lixshop/api/models/coupon/CouponUsageSearch.php
@@ -8,9 +8,19 @@ use yii\web\HttpException;
 
 class CouponUsageSearch extends CommonCouponUsage
 {
+    const COUPON_NOT_USED = 1;
+    const COUPON_IS_USED = 2;
+    const COUPON_EXPIRED = 3;
+
+    public $type;
+
     public function search()
     {
-
+        $query = static::find()
+            ->where([
+                'customer_id' => Yii::$app->user->identity->id,
+                'is_used' => 1
+            ])->all();
     }
 }
 
