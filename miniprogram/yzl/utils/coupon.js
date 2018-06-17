0f410a7625d30a66ed0e66090366f9c83d97f399
Lix <xiao.liunit@gmail.com>
2018-06-17 20:49:57 +0800

    [补全优惠券列表api]

--- miniprogram/yzl/utils/coupon.js
+++ miniprogram/yzl/utils/coupon.js
@@ -9,6 +9,24 @@ const getCouponList = (data) => {
 			'type': data.type,
 		},
 		method: 'GET',
-	
+		success: function (res) {
+			const code = res.data.code
+			if (code === 200) {
+				const response = res.data.data
+				if (data.success !== undefined && typeof(data.success) === 'function') {
+					data.success(response)
+				}
+			} else {
+				if (data.fail !== undefined && typeof(data.fail) === 'function') {
+					data.fail(res.data.msg)
+				}
+			}
+		},
+		fail: function (error) {
+			if (data.fail !== undefined && typeof(data.fail) === 'function') {
+				data.fail('获取优惠券列表失败，请重试')
+			}
+			console.log(error)
+		}
 	})
 }
