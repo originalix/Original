5b6447d140de91436bb906511c024a52ae6d3987
Lix <xiao.liunit@gmail.com>
2018-06-23 18:51:55 +0800

    [migration for customer]

---
+++ PHP/lixshop/console/migrations/m180623_104830_add_discount_to_customer.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180623_104830_add_discount_to_customer
+ */
+class m180623_104830_add_discount_to_customer extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('{{%customer}}', 'discount', $this->decimal(5,2)->defaultValue(10.0)->comment('用户享受的会员折扣');
+        $this->addColumn('{{%customer}}', 'name', $this->string(255)->defaultValue(NULL)->comment('顾客姓名'));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m180623_104830_add_discount_to_customer cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m180623_104830_add_discount_to_customer cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
