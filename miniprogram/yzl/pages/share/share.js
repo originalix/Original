af76d90acfc0b25094d930551879352b358f7080
Lix <xiao.liunit@gmail.com>
2018-06-26 17:24:57 +0800

    [share app message]

--- miniprogram/yzl/pages/share/share.js
+++ miniprogram/yzl/pages/share/share.js
@@ -4,6 +4,16 @@ var orderUtils = require('../../utils/order.js');
 var appInstance = getApp()
 
 Page({
-	data: {}
+	data: {},
+	onShareAppMessage: function (res) {
+		console.log(res)
+		if (res.from === 'button') {
+			console.log('来自转发按钮')
+		}
+		return {
+			title: '衣之恋小程序分享',
+			path: '/page/myCharge/myCharge'
+		}
+	},
 })
 
