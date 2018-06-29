9bb6cab3096d056b97937837db83557e949ddc9b
Lix <xiao.liunit@gmail.com>
2018-06-29 10:20:14 +0800

    [测试时间]

--- PHP/lixshop/api/modules/v1/controllers/ShareController.php
+++ PHP/lixshop/api/modules/v1/controllers/ShareController.php
@@ -21,10 +21,23 @@ class ShareController extends BaseController
 
         $uid = Yii::$app->user->identity->id;
         $now_time = date("y-m-d");
-        $test_time = date("y-m-d", strtotime(Yii::$app->user->identity->created_at));
+        $test_time = date("y-m-d", strtotime(Yii::$app->user->identity->access_token_created_at));
+        if ($now_time == $test_time) {
+            $t = '同一天';
+        } else if ($now_time > $test_time) {
+            $t = '现在时间比创建时间大';
+        } else if ($now_time < $test_time) {
+            $t = '现在时间比创建时间小';
+        }
+        $a = strtotime($now_time);
+        $b = strtotime($test_time);
+
         return [
             'a' => $now_time,
             'b' => $test_time,
+            't' => $t,
+            'x' => $a,
+            'y' => $b,
         ];
     }
 }
