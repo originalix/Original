2d1ab6890e2ab00e47a963795500216422ac889b
Lix <xiao.liunit@gmail.com>
2018-07-02 12:20:56 +0800

    [write log for charge pay]

--- PHP/lixshop/api/models/charge/ChargePay.php
+++ PHP/lixshop/api/models/charge/ChargePay.php
@@ -14,6 +14,9 @@ class ChargePay extends Model
     public $trade_no;
     public $total_fee;
 
+    /**
+     *  使用余额支付的main函数
+     */
     public function main()
     {
         $order = Order::find()
@@ -43,18 +46,65 @@ class ChargePay extends Model
         $transaction = Yii::$app->db->beginTransaction();
 
         try {
-          if (! $order->save()) {
-              throw new HttpException(421, array_values($order->getFirstErrors())[0]);
-          } 
+            if (! $order->save()) {
+                throw new HttpException(421, array_values($order->getFirstErrors())[0]);
+            } 
 
-          if (! $customer->save()) {
-              throw new HttpException(421, array_values($customer->getFirstErrors())[0]);
-          }
+            if (! $customer->save()) {
+                throw new HttpException(421, array_values($customer->getFirstErrors())[0]);
+            }
 
-          $transaction->commit();
+            $this->writeLog($customer, $order->real_amount);
+
+            $transaction->commit();
         } catch (Exception $e) {
-        
+            $transaction->rollBack();
+            throw new HttpException(421, $e->getMessage()); 
         }
 
+        return $order->attributes;
+    }
+
+    /**
+     *  余额消费之后，记录消费日志
+     */
+    function writeLog($customer, $real_amount)
+    {
+        // $customer = Customer::findOne($customer_id);
+        $customer = $this->calculateCredit($customer, $real_amount);
+        $customer->save();
+
+        $log = new BalanceLog();
+        $log->customer_id = $customer->id;
+        $log->type = 1;
+        $log->amount = $real_amount;
+        $log->mark = '消费'.$real_amount.'元';
+        $log->balance = $customer->charge;
+        $log->save();
+        
+        if (intval($real_amount) < 1) {
+            return;
+        }
+
+        $credit_log = new CreditLog();
+        $credit_log->customer_id = $customer->id;
+        $credit_log->type = 1;
+        $credit_log->amount = intval($real_amount);
+        $credit_log->mark = '获取'.$credit_log->amount.'元';
+        $credit_log->balance = $customer->credit;
+        $credit_log->save();
+    }
+
+    /**
+     * 计算用户积分
+     */
+    protected function calculateCredit($customer, $real_amount)
+    {
+        $credit = intval($real_amount);
+        if ($credit < 1) {
+            return $customer;
+        }
+        $customer->credit += $credit;
+        return $customer;
     }
 }
