cf368285cf3874a0442cbff53fe9485de1d24307
Lix <xiao.liunit@gmail.com>
2018-07-02 21:38:19 +0800

    [action detail for promotion control]

--- PHP/lixshop/api/modules/v1/controllers/PromotionController.php
+++ PHP/lixshop/api/modules/v1/controllers/PromotionController.php
@@ -5,8 +5,16 @@ namespace api\modules\v1\controllers;
 use Yii;
 use api\components\BaseController;
 use api\models\product\SalePromotion;
+use yii\web\HttpException;
 
 class PromotionController extends BaseController
 {
+    public function actionDetail($id)
+    {
+        if (is_null($id)) {
+            throw new HttpException(418, '请求参数缺失');
+        }
 
+        return SalePromotion::findOne($id);
+    }
 }
