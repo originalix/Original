0a463c22ad59b7b4a18739e23562648d3014008d
Lix <xiao.liunit@gmail.com>
2018-07-02 22:00:06 +0800

    [get promotion detail api]

--- miniprogram/yzl/config.js
+++ miniprogram/yzl/config.js
@@ -58,6 +58,8 @@ var config = {
     shareAndGetCouponAPI: `${base_host}/v1/share/index`,
     // 发起余额支付
     chargePayAPI: `${base_host}/v1/charge/pay`,
+    // 获取促销商品详情
+    promotionDetailAPI: `${base_host}/v1/promotion/detail`
   }
 };
 
--- miniprogram/yzl/pages/promotion/promotion.js
+++ miniprogram/yzl/pages/promotion/promotion.js
@@ -7,11 +7,41 @@ var appInstance = getApp()
 const Dialog = require('../../bower_components/zanui-weapp/dist/dialog/dialog')
 
 Page({
-  data: {},
+  data: {
+    product: {}
+  },
   onLoad(option) {
     console.log(option)
+    this.getPromotionDetail(option.id)
   },
   getPromotionDetail (id) {
+    var that = this
+    wx.showLoading({
+      title: '加载中',
+    })
+    wx.request({
+      url: config.service.promotionDetailAPI,
+      header: appInstance.requestToken,
+      data: {
+        'id': id
+      },
+      method: 'GET',
+      success: function (res) {
+        console.log(res)
+        wx.hideLoading()
+        const code = res.data.code
+        if (code === 200) {
+          const response = res.data
+          that.setData({
+            product: response.data.product
+          })
+        }
+      },
+      fail: function (error) {
+        wx.hideLoading()
+        console.log(error)
+      }
+    })
 
   }
 })
