7b473d8d64d1731ae9aba37ca3863de5305a9146
Lix <xiao.liunit@gmail.com>
2018-07-12 13:10:06 +0800

    [no message]

--- use-es6/promise.js
+++ use-es6/promise.js
@@ -20,3 +20,50 @@ promise.then(function() {
 });
 
 console.log('Hi');
+
+// 异步加载图片示例
+function loadImageAsync(url) {
+  return new Promise(function(resolve, reject) {
+    const image = new Image();
+
+    image.onload = function() {
+      resolve(image);
+    };
+
+    image.onerror = function() {
+      reject(new Error('Could not load image at ' + url));
+    };
+
+    image.src = url;
+  });
+}
+
+// 用promise对象实现Ajax操作的示例
+const getJSON = function (url) {
+  const promise = new Promise(function(resolve, reject) {
+    const handler = function() {
+      if (this.readyState !== 4) {
+        return;
+      }
+      if (this.status === 200) {
+        resolve(this.response);
+      } else {
+        reject(new Error(this.statusText));
+      }
+    };
+    const client = new XMLHttpRequest();
+    client.open('GET', url);
+    client.onreadystatechange = handler;
+    client.responseType = 'json';
+    client.setRequestHeader('Accept', 'application/json');
+    client.send();
+  });
+
+  return promise;
+};
+
+getJSON('https://api.yzl1030.com/test/ip').then(function(json) {
+  console.log('Contents: ', json);
+}, function(error) {
+  console.error('出错了', error);
+});
