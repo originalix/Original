c20c105084359a822994791d12ecac033abbbb28
Lix <xiao.liunit@gmail.com>
2018-07-15 21:42:22 +0800

    [bug product with vip]

--- miniprogram/yzl/pages/index/index.js
+++ miniprogram/yzl/pages/index/index.js
@@ -2,6 +2,9 @@ var config = require('../../config.js');
 var appInstance = getApp()
 var util = require('../../utils/util.js')
 var auth = require('../../utils/auth.js')
+var chargeUtils = require('../../utils/charge.js');
+
+const Dialog = require('../../bower_components/zanui-weapp/dist/dialog/dialog')
 
 Page({
   data: {
@@ -12,7 +15,8 @@ Page({
     duration: 1000,
     categories: [],
     promotions: [],
-    tips: ''
+    tips: '',
+		userInfo: {},
   },
   onLoad: function (option) {
     if (appInstance.accessToken.length < 1) {
@@ -38,6 +42,7 @@ Page({
   },
   initialiaze(option) {
     this.getIndexConfig()
+    this.getUserInfo()
     console.log(`option query is : `)
     console.log(option)
     if (option.shareid !== undefined) {
@@ -85,6 +90,22 @@ Page({
       }
     })
   },
+	// 获取用户信息
+	getUserInfo () {
+		let that = this
+		chargeUtils.getUserMe({
+			'success': function (res) {
+				that.setData({
+					userInfo: res
+				}, function () {
+					console.log(this.data.userInfo)
+				})
+			},
+			'fail': function (error) {
+				console.log('用户信息获取失败')
+			}
+		})
+	},
   pushToShopList(e) {
     const categoryId = e.currentTarget.dataset.id
     console.log('categoryId is : ' + categoryId)
@@ -119,9 +140,40 @@ Page({
       }
     })
   },
-  go2PromotionPage(e) {
+  promotionClicked(e) {
+    var that = this
     console.log(e)
     let promotionId = e.currentTarget.dataset.promotionid
+    let needVip = e.currentTarget.dataset.needvip
+    console.log(needVip)
+    if (needVip === 0) {
+      this.go2PromotionPage(promotionId)
+      return
+    }
+    if (this.data.userInfo.card_id !== null) {
+      this.go2PromotionPage(promotionId)
+      return
+    }
+    Dialog({
+      title: '温馨提醒',
+      message: '该商品为会员专享，请充值后再购买',
+      selector: '#zan-dialog-charge',
+      buttons: [{
+        text: '去充值',
+        color: 'red',
+        type: 'charge'
+      }, {
+        text: '确认',
+        type: 'confirm'
+      }]
+    }).then(({type}) => {
+      console.log(type)
+      if (type === 'charge') {
+        that.go2ChargePage()
+      }
+    })
+  },
+  go2PromotionPage(promotionId) {
     wx.navigateTo({
       url: '/pages/promotion/promotion?id=' + promotionId
     })
