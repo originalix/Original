ef24d0d33bd9daa94584b7884f5258c2cfa3e449
Lix <xiao.liunit@gmail.com>
2018-08-14 12:28:39 +0800

    :construction:set response type

--- js/node-mock/koa-app.js
+++ js/node-mock/koa-app.js
@@ -1,8 +1,40 @@
 const Koa = require('koa');
-const app = new Koa();
+const Router = require('koa-router');
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+const app = new Koa();
+const router = new Router();
+
+// logger
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
 });
+
+// X-response-time
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// set response type
+app.use(async (ctx, next) => {
+  await next();
+  ctx.response.type = 'json';
+})
+
+router.get('/', (ctx, next) => {
+  ctx.body = { data: 'Hello World' };
+});
+
+router.get('/test', (ctx, next) => {
+  ctx.body = { data: 'Test' };  
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(3000);
