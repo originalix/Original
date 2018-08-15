1dcbcf5c68880598f3de546871fb59b7b38456a9
Lix <xiao.liunit@gmail.com>
2018-08-15 13:33:21 +0800

    :construction:mock 小米商城分类数据

--- js/mock-app/routes/mock.js
+++ js/mock-app/routes/mock.js
@@ -14,4 +14,35 @@ router.get('/koa', (ctx) => {
   });
 });
 
+const category = Mock.mock({
+  'data|5-10': [{
+    'id|+1': 1,
+    'name': Random.first(),
+    'spread': {
+      'title': Random.csentence(5, 10),
+      'description': Random.csentence(15, 20),
+      'image': Random.image('264x84'),
+      'link': Random.url('http')
+    },
+    'items|5-10': [{
+      'id|+1': 1,
+      'name': Random.first(),
+      'items|5-10': [{
+        'id|+1': 1,
+        'name': Random.first(),
+        'image': Random.image('60x50'),
+        'link': Random.url('http')
+      }]
+    }]
+  }]
+});
+
+router.get('/category', (ctx) => {
+  ctx.body = {
+    'code': 200,
+    'msg': 'success',
+    category
+  }
+}); 
+
 module.exports = router;
