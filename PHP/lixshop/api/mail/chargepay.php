f7add40c1c7a30244982a2f0b99a7c2b9196f670
Lix <xiao.liunit@gmail.com>
2018-09-20 14:16:51 +0800

    :construction:no message

--- PHP/lixshop/api/mail/chargepay.php
+++ PHP/lixshop/api/mail/chargepay.php
@@ -1,6 +1,6 @@
 <p>
     <p>亲爱的老板你好，您有新的余额补差价支付：</p>
-    <p>支付金额: <?= $real_amount ?></p>
+    <p>支付金额: <?= $real_amount ?>元</p>
     <p>用户ID: <?= $customer->id ?></p>
     <p>顾客姓名: <?= $customer->name ?></p>
     <p>联系电话： <?= $customer->mobile ?></p>
