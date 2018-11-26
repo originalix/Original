ec67851fa1436d1c0894730ea63138855e88c6b5
Lix <xiao.liunit@gmail.com>
2018-11-26 16:24:15 +0800

    :construction:multiply面试题解答

--- js/interview/multiply.js
+++ js/interview/multiply.js
@@ -6,14 +6,16 @@
  * console.log(a); // [1, 2, 3, 4, 5, 1, 4, 9, 16, 25]
  */
 Array.prototype.multiply = function() {
-  var newArray = this.map(function(curr, idx) {
-    return curr * curr;
-  });
-  console.log(this);
-  console.log(newArray);
-  this.shift();
-  this.shift();
-  return this.concat(newArray);
+  // var newArray = this.map(function(curr, idx) {
+  //   return curr * curr;
+  // });
+  // 直接使用concat生成数组最方便 但是不能直接修改数组 而是返回新的值
+  // return this.concat(newArray);
+  
+  // 如果需要直接修改结果 建议这样修改
+  this.forEach(function(curr, idx) {
+    this.push(curr * curr);
+  }.bind(this));
 }
 
 const a = [1, 2, 3, 4, 5];
