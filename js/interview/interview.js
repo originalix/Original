788575229fdc728d0ebeabc8f607e8af292b778e
Lix <xiao.liunit@gmail.com>
2018-11-27 09:56:01 +0800

    :construction:proxy 测试

--- js/interview/interview.js
+++ js/interview/interview.js
@@ -4,3 +4,17 @@
 // 分为基础数据类型和引用数据类型
 // 基础数据类型分为 String, Number, Boolean, Undefined, Null, Object, Symbol(ES6新加)
 // 引用数据类型分为 Object、Array、Function、RegExp、Date、Math
+
+var obj = new Proxy({}, {
+  get: function(target, key, receiver) {
+    console.log(`getting ${key}!`);
+    return Reflect.get(target, key, receiver);
+  },
+  set: function(target, key, value, receiver) {
+    console.log(`setting ${key}!`);
+    return Reflect.set(target, key, value, receiver);
+  }
+});
+
+obj.count = 1;
+obj.count++;
