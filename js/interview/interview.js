f79e016d19a9b8c6e5a3511f8af4b32a83146510
Lix <xiao.liunit@gmail.com>
2018-11-27 09:33:29 +0800

    :construction:interview

---
+++ js/interview/interview.js
@@ -0,0 +1,6 @@
+/**
+ * JavaScript 中有哪些不同的数据类型？
+ */
+// 分为基础数据类型和引用数据类型
+// 基础数据类型分为 String, Number, Boolean, Undefined, Null, Object, Symbol(ES6新加)
+// 引用数据类型分为 Object、Array、Function、RegExp、Date、Math
